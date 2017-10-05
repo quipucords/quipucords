1a501fb30ad4deb167fb0235cac4bf8aee9f18f3
Chris Hambridge <chambrid@redhat.com>
2017-10-05 16:07:03 -0400

    Add a results endpoint for scans . Closes #41.
    *Returns scan job info
    *Returns results which are essentially basic row x column data
    *Can augment in the future

---
+++ quipucords/api/scanresults_model.py
@@ -0,0 +1,43 @@
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+#
+"""Defines the models used with the API application.
+   These models are used in the REST definitions
+"""
+
+from django.db import models
+from api.scanjob_model import ScanJob
+
+
+class ResultKeyValue(models.Model):
+    """A key value pair of captured data"""
+    key = models.CharField(max_length=64, unique=True)
+    value = models.CharField(max_length=1024)
+
+    class Meta:
+        verbose_name_plural = 'Result Key Values'
+
+
+class Results(models.Model):
+    """The captured results from a scan"""
+    row = models.CharField(max_length=64, unique=True)
+    columns = models.ManyToManyField(ResultKeyValue)
+
+    class Meta:
+        verbose_name_plural = 'Results'
+
+
+class ScanJobResults(models.Model):
+    """The results of a scan job"""
+    scan_job = models.ForeignKey(ScanJob, on_delete=models.CASCADE)
+    results = models.ManyToManyField(Results)
+
+    class Meta:
+        verbose_name_plural = 'Scan Results'
---
+++ quipucords/api/tests_scanresults.py
@@ -0,0 +1,89 @@
+#
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+#
+"""Test the API application"""
+
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from api.hostcredential_model import HostCredential
+from api.networkprofile_model import NetworkProfile
+from api.scanjob_model import ScanJob
+from api.scanresults_model import (ScanJobResults, Results, ResultKeyValue)
+
+
+class ScanJobResultsTest(TestCase):
+    """Test the basic ScanJobResults infrastructure."""
+
+    def setUp(self):
+        self.cred = HostCredential.objects.create(
+            name='cred1',
+            username='username',
+            password='password',
+            sudo_password=None,
+            ssh_keyfile=None)
+        self.cred_for_upload = self.cred.id
+
+        self.network_profile = NetworkProfile(
+            name='profile1',
+            ssh_port=22)
+        self.network_profile.save()
+        self.network_profile.credentials.add(self.cred)
+        self.scanjob = ScanJob(profile_id=self.network_profile.id,
+                               scan_type=ScanJob.DISCOVERY)
+        self.scanjob.save()
+
+    def test_get_results_not_present(self):
+        """Get results on a specific ScanJob by primary key."""
+
+        url = reverse('scanjob-detail', args=(self.scanjob.id,))
+        url = url + 'results/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_results_empty(self):
+        """Get empty results on a specific ScanJob by primary key."""
+
+        results = ScanJobResults(scan_job=self.scanjob)
+        results.save()
+
+        url = reverse('scanjob-detail', args=(self.scanjob.id,))
+        url = url + 'results/'
+        response = self.client.get(url)
+        expected = {'id': 1, 'scan_job': 1, 'results': []}
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), expected)
+
+    def test_get_results(self):
+        """Get results on a specific ScanJob by primary key."""
+
+        results = ScanJobResults(scan_job=self.scanjob)
+        results.save()
+
+        row1 = Results(row='row1')
+        row1.save()
+
+        rkv1 = ResultKeyValue(key='key1', value='value1')
+        rkv1.save()
+        row1.columns.add(rkv1)
+        row1.save()
+        results.results.add(row1)
+        results.save()
+
+        url = reverse('scanjob-detail', args=(self.scanjob.id,))
+        url = url + 'results/'
+        response = self.client.get(url)
+        expected = {'id': 1,
+                    'results': [{'columns': [{'key': 'key1',
+                                              'value': 'value1'}],
+                                 'row': 'row1'}],
+                    'scan_job': 1}
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), expected)
