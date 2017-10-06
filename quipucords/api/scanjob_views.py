12aa7c0e2059d9c4b517cc6f9fbf8b865b61abb6
Chris Hambridge <chambrid@redhat.com>
2017-10-06 09:47:11 -0400

    Add a results endpoint for scans . Closes #41. (#90)
    * Add a results endpoint for scans . Closes #41.
    
    *Returns scan job info
    *Returns results which are essentially basic row x column data
    *Can augment in the future
    
    * delete debug print statement.

--- quipucords/api/scanjob_views.py
+++ quipucords/api/scanjob_views.py
@@ -12,13 +12,21 @@
 
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
+from rest_framework.decorators import detail_route
 from django.shortcuts import get_object_or_404
 from api.scanjob_model import ScanJob
 from api.scanjob_serializer import ScanJobSerializer
 from api.networkprofile_model import NetworkProfile
+from api.scanresults_model import ScanJobResults, Results, ResultKeyValue
+from api.scanresults_serializer import (ScanJobResultsSerializer,
+                                        ResultsSerializer,
+                                        ResultKeyValueSerializer)
 
 
 PROFILE_KEY = 'profile'
+ROW_KEY = 'row'
+COLUMN_KEY = 'columns'
+RESULTS_KEY = 'results'
 
 
 def expand_network_profile(scan):
@@ -32,6 +40,38 @@ def expand_network_profile(scan):
         slim_profile = {'id': profile_id, 'name': profile.name}
         scan[PROFILE_KEY] = slim_profile
     return scan
+
+
+def expand_key_values(result_columns):
+    """Take collection of result key value ids and pull objects from db.
+    create slim dictionary version of key/value to return to user.
+    """
+    if result_columns is not None and result_columns[COLUMN_KEY]:
+        fields_list = []
+        for field_id in result_columns[COLUMN_KEY]:
+            field_data = ResultKeyValue.objects.get(pk=field_id)
+            serializer = ResultKeyValueSerializer(field_data)
+            field_out = {'key': serializer.data['key'],
+                         'value': serializer.data['value']}
+            fields_list.append(field_out)
+    return fields_list
+
+
+def expand_scan_results(results):
+    """Take collection of results ids and pull objects from db.
+    create slim dictionary version of rows of key/value to return to user.
+    """
+    if results is not None and results[RESULTS_KEY]:
+        results_list = []
+        for results_id in results[RESULTS_KEY]:
+            results_data = Results.objects.get(pk=results_id)
+            serializer = ResultsSerializer(results_data)
+            columns_out = expand_key_values(serializer.data)
+            results_out = {ROW_KEY: serializer.data[ROW_KEY],
+                           COLUMN_KEY: columns_out}
+            results_list.append(results_out)
+        results[RESULTS_KEY] = results_list
+    return results
 
 
 # pylint: disable=too-many-ancestors
@@ -57,3 +97,14 @@ class ScanJobViewSet(mixins.RetrieveModelMixin,
         serializer = ScanJobSerializer(scan)
         scan_out = expand_network_profile(serializer.data)
         return Response(scan_out)
+
+    # pylint: disable=unused-argument,invalid-name
+    @detail_route(methods=['get'])
+    def results(self, request, pk=None):
+        """Get the results of a scan job"""
+        get_object_or_404(self.queryset, pk=pk)
+        results_queryset = ScanJobResults.objects.all()
+        results = get_object_or_404(results_queryset, scan_job=pk)
+        serializer = ScanJobResultsSerializer(results)
+        results_out = expand_scan_results(serializer.data)
+        return Response(results_out)
