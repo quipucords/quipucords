927fb0238de12d79d29f2314ba22a90097133751
Jeffrey Phillips <jephilli@redhat.com>
2017-12-04 16:13:25 -0500

    Initial UI navigation layout

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'patternfly/dist/css/patternfly.css';
+import 'patternfly/dist/css/patternfly-additions.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
