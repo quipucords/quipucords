55fbd54708f5690eccd9b78195749138fe25dfe5
CD Cabrera <ccabrera@redhat.com>
2017-12-05 12:04:54 -0500

    Django serve static file settings
    Serve settings for static files from client directory.

--- quipucords/quipucords/settings.py
+++ quipucords/quipucords/settings.py
@@ -143,7 +143,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/client/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'client'),
+]
 
 LOGGING = {
     'version': 1,
