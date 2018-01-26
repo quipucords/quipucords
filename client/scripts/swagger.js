cd9496e62a04b506feef425ad0bc6dc3363458c9
CD Cabrera <cabrera.code@gmail.com>
2018-01-26 10:09:31 -0500

    UI Build updates for Swagger & UI (#508)
    * Build updates for Swagger
    * Removing package-lock until release

--- client/scripts/swagger.js
+++ client/scripts/swagger.js
@@ -1,16 +1,22 @@
 const express = require('express');
-const app = express();
 const swaggerUi = require('swagger-ui-express');
+const swaggerParser = require('swagger-parser');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load(`${process.cwd()}/../docs/swagger.yml`);
+const yamlFile = `${process.cwd()}/../docs/swagger.yml`;
+const app = express();
 const port = process.env.PORT || 4040;
+const swaggerDocument = YAML.load(yamlFile);
 
 app.use('/docs/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, function() {
-  console.log(`
-You can now view API docs in the browser.
+  console.log(
+    `\nYou can now view API docs in the browser.\n  Open: http://localhost:${port}/docs/api\n`
+  );
 
-  Open: http://localhost:${port}/docs/api
-`);
+  swaggerParser.validate(yamlFile, err => {
+    if (err) {
+      console.error(`  \x1b[31m${err.name}\n  \x1b[31m${err.message}\x1b[0m\n`);
+    }
+  });
 });
