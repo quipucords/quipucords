a1c2661748f9045df98d36a14e1b4be6120ec872
Chris Hambridge <chambrid@redhat.com>
2018-02-01 11:04:00 -0500

    Updates to the docker image and documentation to make server container beta ready. Closes #429.

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-source ~/venv/bin/activate;~/venv/bin/gunicorn quipucords.wsgi -c /deploy/gunicorn.conf.py
+source ~/venv/bin/activate;make server-migrate server-set-superuser -C /app;~/venv/bin/gunicorn quipucords.wsgi -c /deploy/gunicorn.conf.py
