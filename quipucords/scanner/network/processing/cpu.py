8f24dcee2dbb669137575f785a6924f6597783d9
abaiken <aaiken@redhat.com>
2018-02-02 14:49:04 -0500

    add post processing for cpu/date facts

---
+++ quipucords/scanner/network/processing/cpu.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2018 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+
+"""Initial processing of the shell output from the cpu role."""
+
+import logging
+from scanner.network.processing import process
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+# pylint: disable=too-few-public-methods
+
+# #### Processors ####
+class ProcessCpuModelVer(process.Processor):
+    """Process the model version of the cpu."""
+
+    KEY = 'cpu_model_ver'
+
+    @staticmethod
+    def process(output):
+        """Pass the output back through."""
+        return output['stdout_lines']
+
+
+class ProcessCpuCpuFamily(process.Processor):
+    """Process the cpu family."""
+
+    KEY = 'cpu_cpu_family'
+
+    @staticmethod
+    def process(output):
+        """Pass the output back through."""
+        return output['stdout_lines']
+
+
+class ProcessCpuVendorId(process.Processor):
+    """Process the vendor id of the cpu."""
+
+    KEY = 'cpu_vendor_id'
+
+    @staticmethod
+    def process(output):
+        """Pass the output back through."""
+        return output['stdout_lines']
+
+
+class ProcessCpuModelName(process.Processor):
+    """Process the model name of the cpu."""
+
+    KEY = 'cpu_model_name'
+
+    @staticmethod
+    def process(output):
+        """Pass the output back through."""
+        return output['stdout_lines']
+
+
+class ProcessCpuBogomips(process.Processor):
+    """Process the bogomips of the cpu."""
+
+    KEY = 'cpu_bogomips'
+
+    @staticmethod
+    def process(output):
+        """Pass the output back through."""
+        return output['stdout_lines']
+
+
