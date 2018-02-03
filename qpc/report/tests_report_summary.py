925e28f3aaec8ba8c440510138ce5fc9b1265c8f
Kevan Holdaway <lahatlihim@gmail.com>
2018-02-03 11:36:25 -0500

    Add a few netagive tests

--- qpc/report/tests_report_summary.py
+++ qpc/report/tests_report_summary.py
@@ -18,6 +18,7 @@ import json
 from io import StringIO
 from argparse import ArgumentParser, Namespace
 import requests_mock
+from qpc.cli import CLI
 from qpc.tests_utilities import HushUpStderr, redirect_stdout, DEFAULT_CONFIG
 from qpc.report import REPORT_URI
 from qpc.report.summary import ReportSummaryCommand
@@ -38,7 +39,7 @@ class ReportSummaryTests(unittest.TestCase):
         # nosetests command.
         self.orig_stderr = sys.stderr
         self.test_json_filename = 'test_%d.json' % time.time()
-        self.test_csv_filename = 'test_%d.json' % time.time()
+        self.test_csv_filename = 'test_%d.csv' % time.time()
         sys.stderr = HushUpStderr()
 
     def tearDown(self):
@@ -55,7 +56,7 @@ class ReportSummaryTests(unittest.TestCase):
             pass
 
     def test_summary_report_as_json(self):
-        """Testing the add network source command successfully."""
+        """Testing retreiving summary report as json."""
         report_out = StringIO()
         get_report_url = get_server_location() + \
             REPORT_URI + '?fact_collection_id=1'
@@ -68,10 +69,59 @@ class ReportSummaryTests(unittest.TestCase):
                              path=self.test_json_filename)
             with redirect_stdout(report_out):
                 nac.main(args)
-                self.assertEqual(report_out.getvalue(),
+                self.assertEqual(report_out.getvalue().strip(),
                                  messages.FILE_SUCCESSFULLY_WRITTEN %
-                                 self.test_json_filename + '\n')
+                                 self.test_json_filename)
                 with open(self.test_json_filename, 'r') as json_file:
                     data = json_file.read()
                     file_content_dict = json.loads(data)
                 self.assertDictEqual(get_report_json_data, file_content_dict)
+
+    def test_summary_report_as_csv(self):
+        """Testing retreiving summary report as csv."""
+        report_out = StringIO()
+        get_report_url = get_server_location() + \
+            REPORT_URI + '?fact_collection_id=1'
+        get_report_csv_data = 'Fact Collection\n'
+        get_report_csv_data += '1\n\n\n'
+        get_report_csv_data += 'key\n'
+        get_report_csv_data += 'value\n'
+
+        get_report_csv_data = {'id': 1, 'report': [{'key': 'value'}]}
+        with requests_mock.Mocker() as mocker:
+            mocker.get(get_report_url, status_code=200,
+                       json=get_report_csv_data)
+            nac = ReportSummaryCommand(SUBPARSER)
+            args = Namespace(id='1', output_json=False, output_csv=True,
+                             path=self.test_csv_filename)
+            with redirect_stdout(report_out):
+                nac.main(args)
+                self.assertEqual(report_out.getvalue().strip(),
+                                 messages.FILE_SUCCESSFULLY_WRITTEN %
+                                 self.test_csv_filename)
+                with open(self.test_csv_filename, 'r') as json_file:
+                    data = json_file.read()
+                    file_content_dict = json.loads(data)
+                    print(file_content_dict)
+                self.assertDictEqual(get_report_csv_data, file_content_dict)
+
+    def test_summary_report_output_directory(self):
+        """Testing fail because output directory."""
+        with self.assertRaises(SystemExit):
+            sys.argv = ['/bin/qpc', 'report', 'summary',
+                        '--json', '--output-file', '/']
+            CLI().main()
+
+    def test_summary_report_output_directory_not_exist(self):
+        """Testing fail because output directory does not exist."""
+        with self.assertRaises(SystemExit):
+            sys.argv = ['/bin/qpc', 'report', 'summary',
+                        '--json', '--output-file', '/foo/bar/']
+            CLI().main()
+
+    def test_summary_report_output_file_empty(self):
+        """Testing fail because output file empty."""
+        with self.assertRaises(SystemExit):
+            sys.argv = ['/bin/qpc', 'report', 'summary',
+                        '--json', '--output-file', '']
+            CLI().main()
