2a490680211c6941b1984a764287440d5ebb07c9
Kevan Holdaway <lahatlihim@gmail.com>
2018-02-05 09:57:20 -0500

    More updates to copyright dates

--- qpc/utils.py
+++ qpc/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2017 Red Hat, Inc.
+# Copyright (c) 2017-2018 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public License,
 # version 3 (GPLv3). There is NO WARRANTY for this software, express or
