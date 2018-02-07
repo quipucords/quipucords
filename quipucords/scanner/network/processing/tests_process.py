77366de3d58b743040fbfc99252ce9bbd4f2292f
Kevan Holdaway <32331547+kholdaway@users.noreply.github.com>
2018-02-07 18:37:12 -0500

    Process facts inline instead of host_done (#660)

--- quipucords/scanner/network/processing/tests_process.py
+++ quipucords/scanner/network/processing/tests_process.py
@@ -149,76 +149,77 @@ class TestProcess(TestCase):
     def test_not_result_no_processing(self):
         """Test a value that is not a task result, and needs no processing."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {NOT_TASK_RESULT_KEY: 'foo'},
-                            HOST), {NOT_TASK_RESULT_KEY: 'foo'})
+            process.process(self.scan_task, {},
+                            NOT_TASK_RESULT_KEY, 'foo',
+                            HOST), 'foo')
 
     def test_not_result_sudo_error(self):
         """Test a value that is not a task result, but is a sudo error."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {NOT_TASK_RESULT_KEY: process.SUDO_ERROR},
-                            HOST), {NOT_TASK_RESULT_KEY: process.NO_DATA})
+            process.process(self.scan_task, {},
+                            NOT_TASK_RESULT_KEY, process.SUDO_ERROR,
+                            HOST), process.NO_DATA)
 
     def test_processing_bad_input(self):
         """Test a key that is not a task result, but needs processing."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {TEST_KEY: 'foo'}, HOST),
-            {TEST_KEY: process.NO_DATA})
+            process.process(self.scan_task, {},
+                            TEST_KEY, 'foo', HOST),
+            process.NO_DATA)
 
     def test_no_processing(self):
         """Test a key that doesn't need to be processed."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {NOT_A_KEY: ansible_result('')},
-                            HOST), {NOT_A_KEY: ansible_result('')})
+            process.process(self.scan_task, {},
+                            NOT_A_KEY, ansible_result(process.NO_DATA),
+                            HOST),
+            ansible_result(process.NO_DATA))
 
     def test_simple_processor(self):
         """Test a key whose processor succeeds."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {TEST_KEY: ansible_result('')},
-                            HOST), {TEST_KEY: '1'})
+            process.process(self.scan_task, {},
+                            TEST_KEY, ansible_result(process.NO_DATA),
+                            HOST), '1')
 
     def test_missing_dependency(self):
         """Test a key whose processor is missing a dependency."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {DEPENDENT_KEY: ansible_result('')},
-                            HOST), {DEPENDENT_KEY: process.NO_DATA})
+            process.process(self.scan_task, {},
+                            DEPENDENT_KEY, ansible_result(process.NO_DATA),
+                            HOST), process.NO_DATA)
 
     def test_satisfied_dependency(self):
         """Test a key whose processor has a dependency, which is present."""
         self.assertEqual(
             process.process(self.scan_task,
-                            {NO_PROCESSOR_KEY: 'result',
-                             DEPENDENT_KEY: ansible_result('')},
+                            {DEPENDENT_KEY: ansible_result(
+                                '')}, NO_PROCESSOR_KEY, 'result',
                             HOST),
-            {NO_PROCESSOR_KEY: 'result', DEPENDENT_KEY: '2'})
+            'result')
 
     def test_skipped_task(self):
         """Test a task that Ansible skipped."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {TEST_KEY: {'skipped': True}},
-                            HOST), {TEST_KEY: process.NO_DATA})
+            process.process(self.scan_task, {},
+                            TEST_KEY, {'skipped': True},
+                            HOST), process.NO_DATA)
 
     def test_task_errored(self):
         """Test a task that errored on the remote machine."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {TEST_KEY: ansible_result('error!', rc=1)},
+            process.process(self.scan_task, {},
+                            TEST_KEY, ansible_result('error!', rc=1),
                             HOST),
-            {TEST_KEY: process.NO_DATA})
+            process.NO_DATA)
 
     def test_processor_errored(self):
         """Test a task where the processor itself errors."""
         self.assertEqual(
-            process.process(self.scan_task,
-                            {PROCESSOR_ERROR_KEY: ansible_result('')},
+            process.process(self.scan_task, {},
+                            PROCESSOR_ERROR_KEY, ansible_result(''),
                             HOST),
-            {PROCESSOR_ERROR_KEY: process.NO_DATA})
+            process.NO_DATA)
 
 
 class TestProcessorMeta(TestCase):
