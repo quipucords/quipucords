a2c7f09f2d576f8b08c31e839ce88a5d6a832df1
Elijah DeLee <kdelee@redhat.com>
2018-02-07 11:36:54 -0500

    Fix typo in messages and update test to match

--- quipucords/api/messages.py
+++ quipucords/api/messages.py
@@ -49,7 +49,7 @@ SOURCE_CRED_WRONG_TYPE = 'Credentials must have the same type as source.'
 SOURCE_TYPE_REQ = 'A value for source_type must be provided to ' \
                   'create a source.'
 SOURCE_TYPE_INV = 'A source_type must not be provided when updating a source.'
-SOURCE_CRED_IDS_INV = 'Credential identitiers must be integer values.'
+SOURCE_CRED_IDS_INV = 'Credential identifiers must be integer values.'
 SOURCE_MIN_CREDS = 'Source must have at least one set of credentials.'
 
 NET_HOST_AS_STRING = 'A host range must be a string.'
@@ -80,7 +80,7 @@ SAT_ONE_CRED = 'Source of type satellite must have a single credential.'
 # scan jobs messages
 PLURAL_SCAN_JOBS_MSG = 'Scan Jobs'
 SJ_REQ_SOURCES = 'Scan job must have one or more sources.'
-SJ_SOURCE_IDS_INV = 'Source identitiers must be integer values.'
+SJ_SOURCE_IDS_INV = 'Source identifiers must be integer values.'
 SJ_SOURCE_DO_NOT_EXIST = 'Source with id=%d could not be'\
     ' found in database.'
 SJ_STATUS_MSG_CREATED = 'Job is created.'
