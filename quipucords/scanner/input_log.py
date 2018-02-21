536c2a0e3e7f1b7f34e2e1b6100a4a6f04aa8bb0
Noah Lavine <nlavine@redhat.com>
2018-02-21 12:27:13 -0500

    Use Python RotatingFileHandler instead of custom one

--- quipucords/scanner/input_log.py
+++ quipucords/scanner/input_log.py
@@ -11,9 +11,8 @@
 
 import json
 import logging
+from logging import handlers
 import multiprocessing as mp
-import os
-import time
 import uuid
 from django.db import transaction
 from api import log_state
@@ -25,8 +24,7 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 UUID_CACHE = None
 
-DEFAULT_MAX_SIZE = 1 << 30  # 1 GiB
-DEFAULT_MAX_AGE = 365 * 24 * 60 * 60  # 1 year, except on leap years
+NUM_LOG_FILES = 10  # Divide the log into 10 different files
 
 
 @transaction.atomic()
@@ -65,194 +63,46 @@ def next_sequence_number():
     return number
 
 
-def find_prefix_scan_stat(prefix, dirname):
-    """Find files in a directory matching a prefix, and stat them.
+# We use the standard Python loggers when possible, but with two main
+# differences:
 
-    This function exists because os.scandir isn't in Python 3.4, which
-    we support.
+# 1. We have a custom log formatter. The standard Python log format is
+#    nice for humans to read, but not nice for machines to parse. So
+#    we have JSONFormatter, which writes log records as plain JSON
+#    dicts that are easy to parse.
 
-    :param prefix: the name prefix to search for.
-    :param dirname: the directory to scan.
+# 2. We don't use LogRecord objects. Almost none of their attributes
+#    apply to what we're doing, and there doesn't seem to be any
+#    advantage to using them, so we just pass plain dictionaries
+#    around.
 
-    :returns: a list of (name, stat) pairs for matching files.
-    """
-    result = []
+class JSONFormatter(logging.Formatter):
+    """Write input log messages as plain JSON dicts."""
 
-    for name in os.listdir(dirname):
-        if name.startswith(prefix):
-            result.append((name, os.stat(os.path.join(dirname, name))))
-
-    return result
-
-
-# pylint: disable=too-many-instance-attributes
-class RotatingLogFile(object):
-    """A log file that automatically rotates itself when full."""
-
-    # This class maintains a set of log files with names 'basename-0',
-    # 'basename-1', .... It automatically switches to a new one when
-    # the last log file is either too big or too old, and removes
-    # earlier ones when they exceed the size or age limits. A few
-    # flaws:
-    #   - It only re-evaluates its log file list on writes. If you
-    #     never write anything, it will never rotate the logs.
-    #   - It is not safe to use more than one instance of this class
-    #     with the same basename. It makes no attempt at locking, and
-    #     it doesn't look for notifications of new log files after
-    #     it's gathered its initial set.
-    #   - It's not thread safe.
-
-    # pylint: disable=too-many-arguments
-    def __init__(self, max_size, max_age,
-                 step_size=None, step_age=None, dry_run=False):
-        """Initialize the RotatingLogFile."""
-        # Log files will be named 'basename-0', 'basename-1', etc.
-        self.basename = None
-        self.dirname = None
-        self.max_size = max_size
-        self.max_age = max_age
-        self.dry_run = dry_run
-        self.total_bytes = 0
-
-        # The size and age limits where we start a new log file.
-        self.step_size = step_size or (max_size + 9) // 10
-        self.step_age = step_age or (max_age + 9) // 10
-
-        # self.log_files will be a list of dicts with entries 'name',
-        # 'created', and 'size'. The list will be sorted with the
-        # newest entries in the front.
-        self.log_files = []
-        self.file_counter = 0  # The number of the newest existing log file.
-
-        # The log file we send writes to. This should always be an
-        # open File object for the file named by
-        # self.log_files[0]['name'], if that exists.
-        self.open_log_file = None
-
-        # Whether we've scanned the log directory for old log files.
-        self.directory_scanned = False
-
-        self.write_lock = mp.Lock()
-
-    def read_files_from_basename(self, basename):
-        """Initialize log_files and file_counter from a basename."""
-        self.dirname, self.basename = os.path.split(basename)
-        if self.dry_run:
-            self.log_files = []
-        else:
-            self.read_files_from_stats(
-                find_prefix_scan_stat(self.basename, self.dirname))
-
-    def read_files_from_stats(self, names_and_stats):
-        """Initialize log_files and file_counter from (name, stat) pairs.
-
-        This method is separate to simplify testing.
-        """
-        for name, stat in names_and_stats:
-            if name.startswith(self.basename):
-                logger.debug('Found log file %s', name)
-                base, _, counter = name.partition('-')
-                if base != self.basename:
-                    logger.error('Bad log file name: %s', name)
-                    continue
-                if not counter.isdigit():
-                    logger.error('Bad log file name: %s', name)
-                    continue
-
-                self.file_counter = max(self.file_counter, int(counter))
-
-                self.log_files.append(
-                    {'name': os.path.join(self.dirname, name),
-                     'created': stat.st_ctime,
-                     'size': stat.st_size})
-                self.total_bytes += stat.st_size
-        logger.debug('Presorted log file set is %s', self.log_files)
-
-        self.log_files = sorted(self.log_files,
-                                key=lambda x: x['created'], reverse=True)
-        logger.debug('Initial log file set is %s', self.log_files)
-
-    def remove_oldest_log_file(self):
-        """Remove the oldest log file."""
-        self.total_bytes -= self.log_files[-1]['size']
-        if not self.dry_run:
-            os.remove(self.log_files[-1]['name'])
-        del self.log_files[-1]
-
-    def new_log_file(self, now=None):
-        """Make a new log file."""
-        self.file_counter += 1
-        new_name = os.path.join(self.dirname,
-                                '{0}-{1}'.format(self.basename,
-                                                 self.file_counter))
-        if self.dry_run:
-            size = 0
-            created = now
-        else:
-            self.open_log_file = open(new_name, 'ab')
-            stat_result = os.stat(new_name)
-            size = stat_result.st_size
-            created = stat_result.st_ctime
-
-        # If we were rotating log files a lot, this insert would be a
-        # performance issue, but it seems very unlikely that we would
-        # rotate that much.
-        self.log_files.insert(0, {'name': new_name,
-                                  'created': created,
-                                  'size': size})
-
-    def rotate_log_files(self, bytes_ahead=0, now=None):
-        """Add and remove log files if necessary.
-
-        This function ensures that the log files listed in
-        self.log_files are under the max_size and max_age limits and
-        that self.open_log_file points to an open log file with enough
-        space left before self.step_size to write bytes_ahead bytes.
-
-        :param bytes_ahead: number of bytes to make available in the
-            current log file.
-        :param now: the current time as number of seconds from epoch,
-            or None. Defaults to time.time().
-        """
-        now = now or time.time()
-
-        # Remove old log files
-        while (self.log_files and
-               now - self.log_files[-1]['created'] > self.max_age):
-            self.remove_oldest_log_file()
-
-        while (self.log_files and
-               self.total_bytes + bytes_ahead > self.max_size):
-            self.remove_oldest_log_file()
-
-        # Maybe create new log file
-        if not self.log_files or \
-           self.log_files[0]['size'] + bytes_ahead > self.step_size or \
-           now - self.log_files[0]['created'] > self.step_age:
-            self.new_log_file(now)
-
-        # Maybe open the log file
-        if not self.open_log_file and not self.dry_run:
-            self.open_log_file = open(self.log_files[0]['name'], 'ab')
-
-    def write_record(self, record, now=None):
-        """Write to the rotating log."""
-        binary_record = (json.dumps(record) + '\n').encode(encoding='utf-8')
-        self.rotate_log_files(bytes_ahead=len(binary_record), now=now)
-
-        # A single record is always written to exactly one log file,
-        # even if the record is larger than self.step_size. (If this
-        # happens with any frequency, then self.step_size is way too
-        # small.)
-        if not self.dry_run:
-            with self.write_lock:
-                self.open_log_file.write(binary_record)
-                self.open_log_file.flush()
-        self.log_files[0]['size'] += len(binary_record)
-        self.total_bytes += len(binary_record)
+    def format(self, record):
+        """Format a JSON dict as a JSON string."""
+        # StreamHandler will add a '\n' after every record.
+        return json.dumps(record)
 
 
-INPUT_LOG = None
+# We use multiprocessing and not threads, so we need to use a
+# multiprocessing lock.
+class MultiprocessRotatingFileHandler(handlers.RotatingFileHandler):
+    """A RotatingFileHandler that uses a multiprocessing lock."""
+
+    def createLock(self):
+        """Create a lock."""
+        self.lock = mp.RLock()
+
+
+_INPUT_LOG = MultiprocessRotatingFileHandler(
+    filename=settings.INPUT_LOG_BASENAME,
+    encoding='utf-8',
+    maxBytes=settings.INPUT_LOG_MAX_SIZE // NUM_LOG_FILES,
+    backupCount=NUM_LOG_FILES)
+_INPUT_LOG.setFormatter(JSONFormatter())
+
+_DRY_RUN = False
 
 
 def log_record(record):
@@ -260,16 +110,11 @@ def log_record(record):
 
     :param record: the record. a Python dict, with JSON-compatible values.
     """
-    global INPUT_LOG  # pylint: disable=global-statement
-    if INPUT_LOG is None:
-        INPUT_LOG = RotatingLogFile(max_size=DEFAULT_MAX_SIZE,
-                                    max_age=DEFAULT_MAX_AGE)
-        INPUT_LOG.read_files_from_basename(settings.INPUT_LOG_BASENAME)
-
     record['database_uuid'] = str(get_database_uuid())
     record['sequence_number'] = next_sequence_number()
 
-    INPUT_LOG.write_record(record)
+    if not _DRY_RUN:
+        _INPUT_LOG.emit(record)
 
 
 # pylint: disable=too-many-arguments
@@ -293,11 +138,8 @@ def log_fact(host, fact, value, scan_job, scan_task, source):
 
 def disable_log_for_test():
     """Disable the input log for testing purposes."""
-    global INPUT_LOG  # pylint: disable=global-statement
-    INPUT_LOG = RotatingLogFile(max_size=DEFAULT_MAX_SIZE,
-                                max_age=DEFAULT_MAX_AGE,
-                                dry_run=True)
-    INPUT_LOG.read_files_from_basename(settings.INPUT_LOG_BASENAME)
+    global _DRY_RUN  # pylint: disable=global-statement
+    _DRY_RUN = True
 
 
 RAW_PARAMS = '_raw_params'
--- quipucords/scanner/test_input_log.py
+++ quipucords/scanner/test_input_log.py
@@ -10,8 +10,6 @@
 """Test the input logger."""
 
 import uuid
-import unittest
-from unittest import mock
 
 from django.test import TestCase
 
@@ -43,38 +41,3 @@ class TestNextSequenceNumber(TestCase):
         # Next retrievals count up
         self.assertEqual(input_log.next_sequence_number(), 1)
         self.assertEqual(input_log.next_sequence_number(), 2)
-
-
-class TestRotatingLogFile(unittest.TestCase):
-    """Test RotatingLogFile."""
-
-    def test_rotating_log_file(self):
-        """Make a RotatingLogFile and walk through basic usage."""
-        rlf = input_log.RotatingLogFile(100, 100, dry_run=True)
-
-        self.assertEqual(rlf.step_size, 10)
-        self.assertEqual(rlf.step_age, 10)
-
-        base_2 = ('base-2', mock.Mock(st_ctime=1, st_size=1))
-        base_3 = ('base-3', mock.Mock(st_ctime=2, st_size=1))
-        base_4 = ('base-4', mock.Mock(st_ctime=3, st_size=1))
-
-        rlf.basename = 'base'
-        rlf.dirname = ''
-        rlf.read_files_from_stats([base_2, base_3, base_4])
-
-        self.assertEqual(rlf.log_files,
-                         [{'name': 'base-4', 'created': 3, 'size': 1},
-                          {'name': 'base-3', 'created': 2, 'size': 1},
-                          {'name': 'base-2', 'created': 1, 'size': 1}])
-        self.assertEqual(rlf.file_counter, 4)
-
-        # 'foo' will fit in log file 'base-4'
-        rlf.write_record('foo', now=4)
-        self.assertEqual(rlf.file_counter, 4)
-        self.assertEqual(rlf.log_files[0]['name'], 'base-4')
-
-        # 'thisisalongrecord' will trigger creation of 'base-5'
-        rlf.write_record('thisisalongrecord', now=5)
-        self.assertEqual(rlf.file_counter, 5)
-        self.assertEqual(rlf.log_files[0]['name'], 'base-5')
