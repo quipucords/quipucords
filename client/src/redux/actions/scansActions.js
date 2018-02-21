6ce0052f33f0f2ae1a1450c7386aa10dc23c0d11
Jeffrey Phillips <jephilli@redhat.com>
2018-02-21 09:44:30 -0500

    UI: Add scan action and reducer tests

--- client/src/redux/actions/scansActions.js
+++ client/src/redux/actions/scansActions.js
@@ -1,77 +1,53 @@
 import { scansTypes } from '../constants';
 import scansService from '../../services/scansService';
 
-const dispatchObjects = {
-  addScan: data => {
-    return {
-      type: scansTypes.ADD_SCAN,
-      payload: scansService.addScan(data)
-    };
-  },
-  cancelScan: id => {
-    return {
-      type: scansTypes.CANCEL_SCAN,
-      payload: scansService.cancelScan(id)
-    };
-  },
-  getScan: id => {
-    return {
-      type: scansTypes.GET_SCAN,
-      payload: scansService.getScan(id)
-    };
-  },
-  getScans: (query = {}) => {
-    return {
-      type: scansTypes.GET_SCANS,
-      payload: scansService.getScans('', query)
-    };
-  },
-  getScanResults: id => {
-    return {
-      type: scansTypes.GET_SCAN_RESULTS,
-      payload: scansService.getScanResults(id)
-    };
-  },
-  pauseScan: id => {
-    return {
-      type: scansTypes.PAUSE_SCAN,
-      payload: scansService.pauseScan(id)
-    };
-  },
-  restartScan: id => {
-    return {
-      type: scansTypes.RESTART_SCAN,
-      payload: scansService.restartScan(id)
-    };
-  }
-};
-
-const addScan = data => dispatch => {
-  return dispatch(dispatchObjects.addScan(data));
-};
-
-const cancelScan = id => dispatch => {
-  return dispatch(dispatchObjects.cancelScan(id));
-};
-
 const getScan = id => dispatch => {
-  return dispatch(dispatchObjects.getScan(id));
+  return dispatch({
+    type: scansTypes.GET_SCAN,
+    payload: scansService.getScan(id)
+  });
 };
 
 const getScans = (query = {}) => dispatch => {
-  return dispatch(dispatchObjects.getScans(query));
+  return dispatch({
+    type: scansTypes.GET_SCANS,
+    payload: scansService.getScans('', query)
+  });
 };
 
 const getScanResults = id => dispatch => {
-  return dispatch(dispatchObjects.getScanResults(id));
+  return dispatch({
+    type: scansTypes.GET_SCAN_RESULTS,
+    payload: scansService.getScanResults(id)
+  });
+};
+
+const addScan = data => dispatch => {
+  return dispatch({
+    type: scansTypes.ADD_SCAN,
+    payload: scansService.addScan(data)
+  });
+};
+
+const cancelScan = id => dispatch => {
+  return dispatch({
+    type: scansTypes.CANCEL_SCAN,
+    payload: scansService.cancelScan(id)
+  });
 };
 
 const pauseScan = id => dispatch => {
-  return dispatch(dispatchObjects.pauseScan(id));
+  return dispatch({
+    type: scansTypes.PAUSE_SCAN,
+    payload: scansService.pauseScan(id)
+  });
 };
 
 const restartScan = id => dispatch => {
-  return dispatch(dispatchObjects.restartScan(id));
+  return dispatch({
+    type: scansTypes.RESTART_SCAN,
+    payload: scansService.restartScan(id)
+  });
 };
 
-export { dispatchObjects, addScan, cancelScan, getScan, getScans, getScanResults, pauseScan, restartScan };
+export { getScan, getScans, getScanResults, addScan, cancelScan, pauseScan, restartScan };
--- client/src/redux/reducers/tests/scansReducer.test.js
+++ client/src/redux/reducers/tests/scansReducer.test.js
@@ -10,6 +10,33 @@ const initialState = {
     pending: false,
     fulfilled: false,
     scans: []
+  },
+
+  detail: {
+    error: false,
+    errorMessage: '',
+    pending: false,
+    fulfilled: false,
+    scan: {}
+  },
+
+  results: {
+    error: false,
+    errorMessage: '',
+    pending: false,
+    fulfilled: false,
+    results: []
+  },
+
+  action: {
+    error: false,
+    errorMessage: '',
+    pending: false,
+    fulfilled: false,
+    add: false,
+    cancel: false,
+    pause: false,
+    restart: false
   }
 };
 
@@ -38,7 +65,9 @@ describe('scansReducer', function() {
     expect(resultState.view.errorMessage).toEqual('GET ERROR');
 
     expect(resultState.persist).toEqual(initialState.persist);
-    expect(resultState.update).toEqual(initialState.update);
+    expect(resultState.detail).toEqual(initialState.detail);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
   });
 
   it('should handle GET_SCANS_PENDING', () => {
@@ -51,7 +80,9 @@ describe('scansReducer', function() {
     expect(resultState.view.pending).toBeTruthy();
 
     expect(resultState.persist).toEqual(initialState.persist);
-    expect(resultState.update).toEqual(initialState.update);
+    expect(resultState.detail).toEqual(initialState.detail);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
   });
 
   it('should handle GET_SCANS_FULFILLED', () => {
@@ -87,6 +118,455 @@ describe('scansReducer', function() {
     expect(resultState.view.scans).toHaveLength(4);
 
     expect(resultState.persist).toEqual(initialState.persist);
-    expect(resultState.update).toEqual(initialState.update);
+    expect(resultState.detail).toEqual(initialState.detail);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
+  });
+
+  it('should handle GET_SCAN_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'GET ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.detail.error).toBeTruthy();
+    expect(resultState.detail.errorMessage).toEqual('GET ERROR');
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
+  });
+
+  it('should handle GET_SCAN_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.detail.pending).toBeTruthy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
+  });
+
+  it('should handle GET_SCAN_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_FULFILLED,
+      payload: {
+        data: {
+          results: {
+            name: '1',
+            id: 1
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.detail.fulfilled).toBeTruthy();
+    expect(resultState.detail.scan.id).toEqual(1);
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.results).toEqual(initialState.results);
+  });
+
+  it('should handle GET_SCAN_RESULTS_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_RESULTS_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'GET RESULTS ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.results.error).toBeTruthy();
+    expect(resultState.results.errorMessage).toEqual('GET RESULTS ERROR');
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle GET_SCAN_RESULTS_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_RESULTS_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.results.pending).toBeTruthy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle GET_SCAN_RESULTS_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.GET_SCAN_RESULTS_FULFILLED,
+      payload: {
+        data: {
+          results: {
+            connection_results: [
+              {
+                task_results: [
+                  {
+                    source: {
+                      id: 0,
+                      name: 'task result 0',
+                      source_type: 'network'
+                    },
+                    systems: [
+                      {
+                        name: '0',
+                        credential: {
+                          id: 0,
+                          name: 'credential 0',
+                          cred_type: 'network'
+                        },
+                        status: 'success'
+                      }
+                    ]
+                  }
+                ]
+              }
+            ],
+            inspection_results: [
+              {
+                task_results: [
+                  {
+                    source: {
+                      id: 0,
+                      name: 'source 0',
+                      source_type: 'network'
+                    },
+                    systems: [
+                      {
+                        name: '0',
+                        facts: [
+                          {
+                            name: 'fact 0',
+                            value: 'value 0'
+                          }
+                        ],
+                        status: 'success'
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.results.fulfilled).toBeTruthy();
+    expect(resultState.results.results.connection_results[0].task_results[0].source.id).toEqual(0);
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.action).toEqual(initialState.action);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle ADD_SCAN_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.ADD_SCAN_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'ADD ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.error).toBeTruthy();
+    expect(resultState.action.errorMessage).toEqual('ADD ERROR');
+    expect(resultState.action.add).toBeTruthy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle ADD_SCAN_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.ADD_SCAN_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.pending).toBeTruthy();
+    expect(resultState.action.add).toBeTruthy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle ADD_SCAN_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.ADD_SCAN_FULFILLED,
+      payload: {}
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.fulfilled).toBeTruthy();
+    expect(resultState.action.add).toBeTruthy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle CANCEL_SCAN_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.CANCEL_SCAN_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'CANCEL ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.error).toBeTruthy();
+    expect(resultState.action.errorMessage).toEqual('CANCEL ERROR');
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeTruthy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle CANCEL_SCAN_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.CANCEL_SCAN_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.pending).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeTruthy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle CANCEL_SCAN_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.CANCEL_SCAN_FULFILLED,
+      payload: {}
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.fulfilled).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeTruthy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle PAUSE_SCAN_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.PAUSE_SCAN_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'PAUSE ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.error).toBeTruthy();
+    expect(resultState.action.errorMessage).toEqual('PAUSE ERROR');
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeTruthy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle PAUSE_SCAN_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.PAUSE_SCAN_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.pending).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeTruthy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle PAUSE_SCAN_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.PAUSE_SCAN_FULFILLED,
+      payload: {}
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.fulfilled).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeTruthy();
+    expect(resultState.action.restart).toBeFalsy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle RESTART_SCAN_REJECTED', () => {
+    let dispatched = {
+      type: scansTypes.RESTART_SCAN_REJECTED,
+      error: true,
+      payload: {
+        message: 'BACKUP MESSAGE',
+        response: {
+          request: {
+            responseText: 'RESTART ERROR'
+          }
+        }
+      }
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.error).toBeTruthy();
+    expect(resultState.action.errorMessage).toEqual('RESTART ERROR');
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeTruthy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle RESTART_SCAN_PENDING', () => {
+    let dispatched = {
+      type: scansTypes.RESTART_SCAN_PENDING
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.pending).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeTruthy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
+  });
+
+  it('should handle RESTART_SCAN_FULFILLED', () => {
+    let dispatched = {
+      type: scansTypes.RESTART_SCAN_FULFILLED,
+      payload: {}
+    };
+
+    let resultState = scansReducer(undefined, dispatched);
+
+    expect(resultState.action.fulfilled).toBeTruthy();
+    expect(resultState.action.add).toBeFalsy();
+    expect(resultState.action.cancel).toBeFalsy();
+    expect(resultState.action.pause).toBeFalsy();
+    expect(resultState.action.restart).toBeTruthy();
+
+    expect(resultState.persist).toEqual(initialState.persist);
+    expect(resultState.view).toEqual(initialState.view);
+    expect(resultState.results).toEqual(initialState.results);
+    expect(resultState.detail).toEqual(initialState.detail);
   });
 });
