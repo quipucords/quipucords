e6461f42637027df53aaff4138117fb94e86fbf6
Kevan Holdaway <akokdh15@gmail.com>
2018-02-22 15:19:26 -0500

    Random UI changes from git merge.  Revert to latest in master.

--- client/src/redux/actions/scansActions.js
+++ client/src/redux/actions/scansActions.js
@@ -2,15 +2,24 @@ import { scansTypes } from '../constants';
 import scansService from '../../services/scansService';
 
 const getScan = id => dispatch => {
-  return dispatch(dispatchObjects.getScan(id));
+  return dispatch({
+    type: scansTypes.GET_SCAN,
+    payload: scansService.getScan(id)
+  });
 };
 
 const getScans = (query = {}) => dispatch => {
-  return dispatch(dispatchObjects.getScans(query));
+  return dispatch({
+    type: scansTypes.GET_SCANS,
+    payload: scansService.getScans('', query)
+  });
 };
 
 const getScanResults = id => dispatch => {
-  return dispatch(dispatchObjects.getScanResults(id));
+  return dispatch({
+    type: scansTypes.GET_SCAN_RESULTS,
+    payload: scansService.getScanResults(id)
+  });
 };
 
 const addScan = data => dispatch => {
@@ -28,11 +37,17 @@ const cancelScan = id => dispatch => {
 };
 
 const pauseScan = id => dispatch => {
-  return dispatch(dispatchObjects.pauseScan(id));
+  return dispatch({
+    type: scansTypes.PAUSE_SCAN,
+    payload: scansService.pauseScan(id)
+  });
 };
 
 const restartScan = id => dispatch => {
-  return dispatch(dispatchObjects.restartScan(id));
+  return dispatch({
+    type: scansTypes.RESTART_SCAN,
+    payload: scansService.restartScan(id)
+  });
 };
 
 export { getScan, getScans, getScanResults, addScan, cancelScan, pauseScan, restartScan };
