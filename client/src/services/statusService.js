f6db6cf102541ecf0f8bde2b4c11634a62614617
Jeffrey Phillips <jephilli@redhat.com>
2018-03-08 06:46:52 -0500

    Update per review

--- client/src/services/statusService.js
+++ client/src/services/statusService.js
@@ -4,9 +4,7 @@ class StatusService {
   static getStatus() {
     return axios({
       method: 'get',
-      url: process.env.REACT_APP_STATUS_SERVICE,
-      xsrfCookieName: process.env.REACT_APP_AUTH_TOKEN,
-      xsrfHeaderName: process.env.REACT_APP_AUTH_HEADER
+      url: process.env.REACT_APP_STATUS_SERVICE
     });
   }
 }
