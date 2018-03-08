56c9ac5a37fe8c76fef0fb0caa17fe38c81cf412
Jeffrey Phillips <jephilli@redhat.com>
2018-03-08 12:40:53 -0500

    UI: remove unsupported sort and filter fields

--- client/src/components/sources/sourceConstants.js
+++ client/src/components/sources/sourceConstants.js
@@ -1,13 +1,7 @@
 import { helpers } from '../../common/helpers';
 const SourceFilterFields = [
   {
-    id: 'name',
-    title: 'Name',
-    placeholder: 'Filter by Name',
-    filterType: 'text'
-  },
-  {
-    id: 'sourceType',
+    id: 'source_type',
     title: 'Source Type',
     placeholder: 'Filter by Source Type',
     filterType: 'select',
@@ -16,33 +10,6 @@ const SourceFilterFields = [
       { title: helpers.sourceTypeString('satellite'), id: 'satellite' },
       { title: helpers.sourceTypeString('vcenter'), id: 'vcenter' }
     ]
-  },
-  {
-    id: 'hosts',
-    title: 'IP Address',
-    placeholder: 'Filter by IP Address',
-    filterType: 'text'
-  },
-  {
-    id: 'status',
-    title: 'Connection Status',
-    placeholder: 'Filter by connection status',
-    filterType: 'select',
-    filterValues: [
-      { title: helpers.scanStatusString('completed'), id: 'completed' },
-      { title: helpers.scanStatusString('failed'), id: 'failed' },
-      { title: helpers.scanStatusString('created'), id: 'created' },
-      { title: helpers.scanStatusString('running'), id: 'running' },
-      { title: helpers.scanStatusString('paused'), id: 'paused' },
-      { title: helpers.scanStatusString('pending'), id: 'pending' },
-      { title: helpers.scanStatusString('canceled'), id: 'canceled' }
-    ]
-  },
-  {
-    id: 'credentials',
-    title: 'Credentials',
-    placeholder: 'Filter by Credentials',
-    filterType: 'text'
   }
 ];
 
@@ -53,34 +20,9 @@ const SourceSortFields = [
     isNumeric: false
   },
   {
-    id: 'sourceType',
+    id: 'source_type',
     title: 'Source Type',
     isNumeric: false
-  },
-  {
-    id: 'status',
-    title: 'Connection Status',
-    isNumeric: false
-  },
-  {
-    id: 'credentialsCount',
-    title: 'Credentials Count',
-    isNumeric: true
-  },
-  {
-    id: 'hostCount',
-    title: 'Systems Count',
-    isNumeric: true
-  },
-  {
-    id: 'successHostCount',
-    title: 'Successful Authentications',
-    isNumeric: true
-  },
-  {
-    id: 'failedHostCount',
-    title: 'Failed Authentications',
-    isNumeric: true
   }
 ];
 
