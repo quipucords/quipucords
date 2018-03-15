4d01ed637ef24f2afc8919d90fba9fe920ecb60b
Chris Hambridge <chambrid@redhat.com>
2018-03-15 14:17:44 -0400

    Add local pkg install. Add overrides and options to skip components. Added usage. (#1040)

--- install/install.sh
+++ install/install.sh
@@ -1,13 +1,54 @@
 #!/bin/bash
-
+#===============================================================================
 #
-# Description: Install Quipucords server and command-line.
+#          FILE:  install.sh
 #
+#         USAGE:  ./install.sh
+#
+#   DESCRIPTION:  Install Quipucords server and command-line components.
+#
+#       OPTIONS:  -h | --help   Obtain command USAGE
+#                 -e | --extra-vars  set additional variables as key=value
+#===============================================================================
 
 export PATH=$PATH:$ANSIBLE_HOME/bin
 PLAYBOOKFILE="qpc_playbook.yml"
 
-echo ansible-playbook $PLAYBOOKFILE $*
+
+usage() {
+    cat <<EOM
+    Install Quipucords server and command-line components.
+
+    Usage:
+    $(basename $0)
+
+    Options:
+    -h | --help   Obtain command USAGE
+    -e | --extra-vars  set additional variables as key=value
+
+
+    Extra Variables:
+    * Specify fully-qualified directory path for local install packages (defaults to `pwd`/packages/):
+         -e pkg_install_dir=/home/user/pkgs
+    * Skipping install for server:
+         -e install_server=false
+    * Skipping install for CLI:
+         -e install_cli=false
+    * Specify server port (defaults to 443):
+         -e server_port=8443
+    * Specify server container name (defaults to quipucords):
+         -e server_name=qpc_server
+EOM
+    exit 0
+}
+
+if [[ ($1 == "--help") ||  ($1 == "-h") ]]
+then
+  usage;
+fi
+
+
+echo ansible-playbook $PLAYBOOKFILE -v -K $*
 ansible-playbook $PLAYBOOKFILE -v -K $*
 
 if [ $? -eq 0 ]
