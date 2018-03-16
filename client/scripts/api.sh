02e65d0d1320fa8e95ac95cea07d1a5627e8c82b
CD Cabrera <ccabrera@redhat.com>
2018-03-16 16:43:10 -0400

    UI: Staging & prod data persistence
    * NPM API clean up script
    * Persist data across staging and prod containers

--- client/scripts/api.sh
+++ client/scripts/api.sh
@@ -21,22 +21,22 @@ checkContainerRunning()
   done
 
   if [ ! -z "$(docker ps | grep $CONTAINER)" ]; then
-    printf "\e[32mContainer SUCCESS"
-    printf "\n\n\e[39m"
+    printf "${GREEN}Container SUCCESS"
+    printf "\n\n${NOCOLOR}"
   else
     local HASH=$(git rev-list -1 --all --abbrev-commit $FILE)
     local COMMIT=$(git rev-list -1 --all --oneline $FILE)
     local BLAME=$(git blame $FILE | grep $HASH | sed 's/^/  /')
     local GITHUB="https://github.com/quipucords/quipucords/commit/$HASH"
 
-    echo "Last Commit:\n\n  $COMMIT\n\nVisit:\n\n  $GITHUB\n\nAssigning Blame:\n\n$BLAME"  > api-debug.txt
+    echo "Last Commit:\n\n  ${COMMIT}\n\nVisit:\n\n  ${GITHUB}\n\nAssigning Blame:\n\n${BLAME}"  > api-debug.txt
 
-    printf "\e[31mContainer ERROR"
-    printf "\n\e[31m  Review the Swagger doc for errors."
-    printf "\n\e[31m  Last commit: $COMMIT"
-    printf "\n\e[31m  See api-debug.txt for details."
-    printf "\n\e[31m  Visit: $GITHUB"
-    printf "\e[39m\n"
+    printf "${RED}Container ERROR"
+    printf "\n${RED}  Review the Swagger doc for errors."
+    printf "\n${RED}  Last commit: ${COMMIT}"
+    printf "\n${RED}  See api-debug.txt for details."
+    printf "\n${RED}  Visit: ${GITHUB}"
+    printf "${NOCOLOR}\n"
   fi
 }
 #
@@ -70,7 +70,7 @@ devApi()
   if [ ! -z "$(docker ps | grep $CONTAINER)" ] && [ "$UPDATE" = false ]; then
     echo "  Container: $(docker ps | grep $CONTAINER | cut -c 1-80)"
     echo "  Development API running: http://localhost:$PORT/"
-    printf "  To stop: $ \e[32mdocker stop $NAME\e[39m\n"
+    printf "  To stop: $ ${GREEN}docker stop ${NAME}${NOCOLOR}\n"
   fi
 }
 #
@@ -83,7 +83,11 @@ stageApi()
   local NAME="quipucords-stage"
   local PORT=$1
   local DIR=$2
-  local UPDATE=$3
+  local DATA=$3
+  local UPDATE=$4
+  local DATA_VOLUME="/var/data"
+  local CLIENT_VOLUME="/app/quipucords/client"
+  local TEMPLATE_VOLUME="/app/quipucords/quipucords/templates/client"
 
   docker stop -t 0 $NAME >/dev/null
 
@@ -95,14 +99,14 @@ stageApi()
 
   if [ -z "$(docker ps | grep $CONTAINER)" ] && [ "$UPDATE" = false ]; then
     echo "Starting staging API..."
-    docker run -d --rm -p $PORT:443 -v $DIR:/app/quipucords/client:cached -v $DIR:/app/quipucords/quipucords/templates/client:cached --name $NAME $CONTAINER >/dev/null
+    docker run -d --rm -p $PORT:443 -v $DATA:$DATA_VOLUME -v $DIR:$CLIENT_VOLUME:cached -v $DIR:$TEMPLATE_VOLUME:cached --name $NAME $CONTAINER >/dev/null
   fi
 
   if [ ! -z "$(docker ps | grep $CONTAINER)" ] && [ "$UPDATE" = false ]; then
     echo "  Container: $(docker ps | grep $CONTAINER | cut -c 1-80)"
-    echo "  Stage API running: https://localhost:$PORT/"
+    echo "  Stage API running: https://localhost:${PORT}/"
     echo "  Connected to local directory: $(basename $DIR | cut -c 1-80)"
-    printf "  To stop: $ \e[32mdocker stop $NAME\e[39m\n"
+    printf "  To stop: $ ${GREEN}docker stop ${NAME}${NOCOLOR}\n"
   fi
 }
 #
@@ -114,7 +118,9 @@ prodApi()
   local CONTAINER="quipucords-latest"
   local NAME="quipucords"
   local PORT=$1
-  local UPDATE=$2
+  local DATA=$2
+  local UPDATE=$3
+  local DATA_VOLUME="/var/data"
 
   docker stop -t 2 $NAME >/dev/null
 
@@ -126,13 +132,13 @@ prodApi()
 
   if [ -z "$(docker ps | grep $CONTAINER)" ] && [ "$UPDATE" = false ]; then
     echo "Starting production API..."
-    docker run -d --rm -p $PORT:443 --name $NAME $CONTAINER >/dev/null
+    docker run -d --rm -p $PORT:443 -v $DATA:$DATA_VOLUME --name $NAME $CONTAINER >/dev/null
   fi
 
   if [ ! -z "$(docker ps | grep $CONTAINER)" ] && [ "$UPDATE" = false ]; then
     echo "  Container: $(docker ps | grep $CONTAINER | cut -c 1-80)"
-    echo "  Production API running: https://localhost:$PORT/"
-    printf "  To stop: $ \e[32mdocker stop $NAME\e[39m\n"
+    echo "  Production API running: https://localhost:${PORT}/"
+    printf "  To stop: $ ${GREEN}docker stop ${NAME}${NOCOLOR}\n"
   fi
 }
 #
@@ -140,18 +146,25 @@ prodApi()
 # main()
 #
 {
+  RED="\e[31m"
+  GREEN="\e[32m"
+  NOCOLOR="\e[39m"
   PORT=8080
   FILE="$(pwd)/swagger.yaml"
   UPDATE=false
   DIR=""
+  DATA="$(pwd)/.container"
+  CLEAN=false
 
-  while getopts p:f:d:t:u option;
+  while getopts p:f:d:s:t:cu option;
     do
       case $option in
         p ) PORT=$OPTARG;;
         f ) FILE="$OPTARG";;
         d ) DIR="$OPTARG";;
+        s ) DATA="$OPTARG";;
         t ) TYPE="$OPTARG";;
+        c ) CLEAN=true;;
         u ) UPDATE=true;;
       esac
   done
@@ -160,11 +173,23 @@ prodApi()
     exit 1
   fi
 
+  if [ "$TYPE" != dev ] && [ "$CLEAN" = true ]; then
+    echo "Cleaning Docker and data..."
+    printf "${RED}\n"
+    docker system prune -f
+    printf "${GREEN}Docker cleaning success.${NOCOLOR}\n"
+    rm -rf $DATA
+  fi
+
+  if [ "$TYPE" != dev ]; then
+    mkdir -p $DATA
+  fi
+
   case $TYPE in
     prod )
-      prodApi $PORT $UPDATE;;
+      prodApi $PORT $DATA $UPDATE;;
     stage )
-      stageApi $PORT $DIR $UPDATE;;
+      stageApi $PORT $DIR $DATA $UPDATE;;
     dev )
       devApi $PORT "$FILE" $UPDATE;;
     * )
