51ff00babfc78f8d81e89967375f248f3bf3c9b2
Jeffrey Phillips <jephilli@redhat.com>
2018-04-06 13:07:50 -0400

    UI: Show source associated with system in scans results lists. (#1184)

--- client/src/components/scans/scanListItem.js
+++ client/src/components/scans/scanListItem.js
@@ -280,12 +280,17 @@ class ScanListItem extends React.Component {
 
   renderHostRow(host) {
     return (
-      <Grid.Col xs={12}>
-        <span>
-          <Icon type="pf" name={host.status === 'success' ? 'ok' : 'error-circle-o'} />
-          &nbsp; {host.name}
-        </span>
-      </Grid.Col>
+      <React.Fragment>
+        <Grid.Col xs={6} sm={4} md={3}>
+          <span>
+            <Icon type="pf" name={host.status === 'success' ? 'ok' : 'error-circle-o'} />
+            &nbsp; {host.name}
+          </span>
+        </Grid.Col>
+        <Grid.Col xs={6} sm={8} md={9}>
+          {_.get(host, 'source.name')}
+        </Grid.Col>
+      </React.Fragment>
     );
   }
 
