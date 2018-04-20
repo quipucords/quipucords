498996e0f129d0ce098984c2bd71be0e9a0b2d75
Kevan Holdaway <akokdh15@gmail.com>
2018-04-20 15:52:28 -0400

    Add reports/merge API

--- quipucords/api/report/tests_report.py
+++ quipucords/api/report/tests_report.py
@@ -14,6 +14,7 @@ import copy
 import json
 import uuid
 
+import api.messages as messages
 from api.models import (Credential,
                         FactCollection,
                         ServerInformation,
@@ -434,3 +435,154 @@ class DeploymentReportTest(TestCase):
         # pylint: disable=line-too-long
         expected = 'Report\r\n1\r\n\r\n\r\nReport:\r\nname\r\n1.2.3.4\r\n1.2.3.4\r\n1.2.3.4\r\n\r\n'  # noqa
         self.assertEqual(csv_result, expected)
+
+    ##############################################################
+    # Test Report Merge
+    ##############################################################
+    def test_merge_empty_body(self):
+        """Test merge with empty body."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        response = self.client.put(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_REQUIRED]})
+
+    def test_merge_empty_dict(self):
+        """Test merge with empty dict."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_REQUIRED]})
+
+    def test_merge_jobs_not_list(self):
+        """Test merge with not list."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {'reports': 5}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_NOT_LIST]})
+
+    def test_merge_jobs_list_too_short(self):
+        """Test merge with list too short."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {'reports': [5]}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_TOO_SHORT]})
+
+    def test_merge_jobs_list_contains_string(self):
+        """Test merge with containing str."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {'reports': [5, 'hello']}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_NOT_INT]})
+
+    def test_merge_jobs_list_contains_duplicates(self):
+        """Test merge with containing duplicates."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {'reports': [5, 5]}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports': [messages.REPORT_MERGE_NOT_UNIQUE]})
+
+    def test_merge_jobs_list_contains_invalid_job_ids(self):
+        """Test merge with containing duplicates."""
+        # pylint: disable=no-member
+        url = '/api/v1/reports/merge/'
+        data = {'reports': [5, 6]}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        json_response = response.json()
+        self.assertEqual(
+            json_response, {'reports':
+                            [messages.REPORT_MERGE_NOT_FOUND % '5, 6']})
+
+    def test_merge_jobs_success(self):
+        """Test merge jobs success."""
+        url = reverse('facts-list')
+        sources1 = [{'server_id': self.server_id,
+                     'source_name': self.net_source.name,
+                     'source_type': self.net_source.source_type,
+                     'facts': [{'key1': 'value1'}]}]
+        sources2 = [{'server_id': 'abc',
+                     'source_name': 'another_name',
+                     'source_type': 'network',
+                     'facts': [{'key2': 'value2'}]}]
+        request_json = {'sources': sources1}
+        response = self.client.post(url,
+                                    json.dumps(request_json),
+                                    'application/json')
+        if response.status_code != status.HTTP_201_CREATED:
+            print(response.json())
+        response_json = response.json()
+        self.assertEqual(
+            response_json['sources'],
+            sources1)
+        report1_id = response_json['id']
+
+        request_json = {'sources': sources2}
+        response = self.client.post(url,
+                                    json.dumps(request_json),
+                                    'application/json')
+        if response.status_code != status.HTTP_201_CREATED:
+            print(response.json())
+        response_json = response.json()
+        self.assertEqual(
+            response_json['sources'],
+            sources2)
+        report2_id = response_json['id']
+
+        url = '/api/v1/reports/merge/'
+        data = {'reports': [report1_id, report2_id]}
+        response = self.client.put(url, json.dumps(data),
+                                   content_type='application/json',
+                                   format='json')
+        if response.status_code != status.HTTP_201_CREATED:
+            print(response.json())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        json_response = response.json()
+        expected = {'id': 3,
+                    'sources': [
+                        {'server_id': 'abc',
+                         'source_name': 'another_name',
+                         'source_type': 'network',
+                         'facts': [{'key2': 'value2'}]},
+                        {'server_id': self.server_id,
+                         'source_name': 'test_source',
+                         'source_type': 'network',
+                         'facts': [{'key1': 'value1'}]}],
+                    'status': 'complete'}
+
+        self.assertEqual(
+            json_response, expected)
