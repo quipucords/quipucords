78e7bc09e02c0db3c4b811658c0c7a70c064a06e
Kevan Holdaway <32331547+kholdaway@users.noreply.github.com>
2018-06-06 08:31:03 -0400

    Update 0001_initial.py

--- quipucords/api/migrations/0001_initial.py
+++ quipucords/api/migrations/0001_initial.py
@@ -355,3 +355,4 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='entitlements', to='api.SystemFingerprint'),
         ),
     ]
+    
