b1f142932272053de27aa8cdb0675a306c158170
Kevan Holdaway <kholdawa@redhat.com>
2018-11-08 15:27:13 -0500

    Gather RHEL 8 system intent (#1691)
    * Gather RHEL 8 system intent

--- docs/source/command_line_reference.rst
+++ docs/source/command_line_reference.rst
@@ -1,3 +1,7 @@
 QPC command line reference
 ==========================
-qpc is the RPM command line tool for quipucords. Documentation for qpc is available through `readthedocs <https://qpc.readthedocs.io/en/latest/man.html>`_.
+qpc is the RPM command line tool for quipucords. Documentation for qpc is available through the |qpc documentation page|
+
+.. |qpc documentation page| raw:: html
+
+    <a href="https://qpc.readthedocs.io/en/latest/man.html" target="_blank">qpc documentation page</a>
--- quipucords/api/deployments_report/model.py
+++ quipucords/api/deployments_report/model.py
@@ -107,6 +107,14 @@ class SystemFingerprint(models.Model):
     system_creation_date = models.DateField(null=True)
     system_last_checkin_date = models.DateField(null=True)
 
+    system_intent_role = models.CharField(
+        max_length=128, unique=False, null=True)
+    system_intent_addons = models.TextField(unique=False, null=True)
+    system_intent_sla = models.CharField(
+        max_length=128, unique=False, null=True)
+    system_intent_usage_type = models.CharField(
+        max_length=128, unique=False, null=True)
+
     virtualized_type = models.CharField(max_length=64, unique=False, null=True)
 
     # VCenter scan facts
@@ -144,6 +152,10 @@ class SystemFingerprint(models.Model):
             'cpu_socket_count:{}, '\
             'cpu_core_count:{}, '\
             'system_creation_date:{}, '\
+            'system_intent_role:{}, '\
+            'system_intent_addons:{}, '\
+            'system_intent_sla:{}, '\
+            'system_intent_usage_type:{}, '\
             'infrastructure_type:{}, '\
             'virtualized_type:{}, '\
             'vm_state:{}, '\
@@ -172,6 +184,10 @@ class SystemFingerprint(models.Model):
                                   self.cpu_socket_count,
                                   self.cpu_core_count,
                                   self.system_creation_date,
+                                  self.system_intent_role,
+                                  self.system_intent_addons,
+                                  self.system_intent_sla,
+                                  self.system_intent_usage_type,
                                   self.infrastructure_type,
                                   self.virtualized_type,
                                   self.vm_state,
