f5f66c26a0d84a17e8fb1da4a614bee07a4c4a7a
Kevan Holdaway <kholdawa@redhat.com>
2018-11-09 17:57:30 -0500

    Rename system intent to system purpose

--- quipucords/scanner/network/processing/system_purpose.py
+++ quipucords/scanner/network/processing/system_purpose.py
@@ -7,7 +7,7 @@
 # along with this software; if not, see
 # https://www.gnu.org/licenses/gpl-3.0.txt.
 
-"""Initial processing of the system intent role."""
+"""Initial processing of the system purpose role."""
 
 import json
 import logging
@@ -19,21 +19,21 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 # pylint: disable=too-few-public-methods
 
 
-class ProcessSystemIntent(process.Processor):
-    """Process the system_intent_json fact."""
+class ProcessSystemPurpose(process.Processor):
+    """Process the system_purpose_json fact."""
 
-    KEY = 'system_intent_json'
+    KEY = 'system_purpose_json'
 
     @staticmethod
     def process(output, dependencies=None):
         """Pass the output back through."""
-        system_intent_str = output.get('stdout', None)
-        if system_intent_str:
+        system_purpose_str = output.get('stdout', None)
+        if system_purpose_str:
             try:
-                system_intent_dict = json.loads(system_intent_str)
-                return system_intent_dict
+                system_purpose_dict = json.loads(system_purpose_str)
+                return system_purpose_dict
             except json.decoder.JSONDecodeError as error:
                 logger.warning(
-                    'system_intent was not valid JSON.  Error: %s', error)
+                    'system_purpose was not valid JSON.  Error: %s', error)
                 return None
         return None
