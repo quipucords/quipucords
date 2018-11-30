76777a54452556ef4d82d52c6bb9a1ecdbe26e0d
Kevan Holdaway <kholdawa@redhat.com>
2018-11-30 11:04:00 -0500

    A X-Server-Version header

---
+++ quipucords/api/common/middleware.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2018 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+#
+
+"""Flow server version to clients."""
+
+from quipucords.environment import server_version
+
+
+class ServerVersionMiddle:
+    """Middleware class to flow server version to clients."""
+
+    # pylint: disable=too-few-public-methods
+    def __init__(self, get_response):
+        """Initialize middleware class."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Add server version header to response."""
+        response = self.get_response(request)
+        response['X-Server-Version'] = server_version()
+        return response
