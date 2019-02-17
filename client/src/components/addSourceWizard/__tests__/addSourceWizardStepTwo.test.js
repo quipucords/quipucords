8d553b0eb2ce6b298b2c7a8392cdcfc56dcaefe0
CD Cabrera <ccabrera@redhat.com>
2019-02-16 20:48:04 -0500

    DEV addSourceWizard components
    * build, eslint addition for labels
    * styling, scss updates for wizard, index.html parent class
    * component, addSourceWIzardField, removed in favor of formField
    * component, addSourceWizard cleanup how state handled
    * component, addSourceWizardStepOne update formState, selector
    * component, addSourceWizardStepTwo update formState, selector
    * component, addSourceWizardStepThree update selector

--- client/src/components/addSourceWizard/__tests__/addSourceWizardStepTwo.test.js
+++ client/src/components/addSourceWizard/__tests__/addSourceWizardStepTwo.test.js
@@ -4,38 +4,89 @@ import { AddSourceWizardStepTwo } from '../addSourceWizardStepTwo';
 
 describe('AddSourceWizardStepTwo Component', () => {
   it('should render a non-connected component', () => {
-    const props = {};
+    const props = {
+      type: 'network'
+    };
+
     const component = mount(<AddSourceWizardStepTwo {...props} />);
 
-    expect(component.render()).toMatchSnapshot('unconnected');
+    expect(component.render()).toMatchSnapshot('non-connected');
+  });
+
+  it('should display different forms for source types', () => {
+    const props = {
+      type: 'network'
+    };
+
+    let component = mount(<AddSourceWizardStepTwo {...props} />);
+    expect(component.render()).toMatchSnapshot('network');
+
+    props.type = 'vcenter';
+    component = mount(<AddSourceWizardStepTwo {...props} />);
+    expect(component.render()).toMatchSnapshot('vcenter');
+  });
+
+  it('should correctly validate and submit data for sources', () => {
+    const props = {
+      add: true,
+      type: 'network'
+    };
+
+    const spy = jest.spyOn(AddSourceWizardStepTwo.prototype, 'submitStep');
+    const component = mount(<AddSourceWizardStepTwo {...props} />);
+    const componentInstance = component.instance();
+
+    expect(
+      componentInstance.isStepValid({
+        values: {
+          credentials: [],
+          hosts: [],
+          name: null,
+          port: -1
+        }
+      })
+    ).toMatchSnapshot('step invalid');
+
+    expect(
+      componentInstance.isStepValid({
+        values: {
+          credentials: [1],
+          hosts: ['0.0.0.0'],
+          name: 'lorem',
+          port: 22
+        }
+      })
+    ).toMatchSnapshot('step valid');
+
+    expect(spy).toHaveBeenCalledTimes(1);
   });
 
   it('should correctly validate source hosts', () => {
-    expect(AddSourceWizardStepTwo.validateHosts(['l'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHost('l')).toBeNull();
+    expect(AddSourceWizardStepTwo.hostsValid(['l'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostValid('l')).toBe(true);
 
-    expect(AddSourceWizardStepTwo.validateHosts(['l.']).length).toBeGreaterThan(0);
-    expect(AddSourceWizardStepTwo.validateHost('l.').length).toBeGreaterThan(0);
+    expect(AddSourceWizardStepTwo.hostsValid(['l.'])).toBe(false);
+    expect(AddSourceWizardStepTwo.hostValid('l.')).toBe(false);
 
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.0:']).length).toBeGreaterThan(0);
-    expect(AddSourceWizardStepTwo.validateHost('0.0.0.0:').length).toBeGreaterThan(0);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.0:'])).toBe(false);
+    expect(AddSourceWizardStepTwo.hostValid('0.0.0.0:')).toBe(false);
 
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.0:22']).length).toBeGreaterThan(0);
-    expect(AddSourceWizardStepTwo.validateHost('0.0.0.0:22').length).toBeGreaterThan(0);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.0:22'])).toBe(false);
+    expect(AddSourceWizardStepTwo.hostValid('0.0.0.0:22')).toBe(false);
 
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.[12:24]'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.[12:a]']).length).toBeGreaterThan(0);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.[12:24]'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostValid(['0.0.0.[12:a]'])).toBe(false);
 
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/23']).length).toBeGreaterThan(0);
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/24'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/25'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/26'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/27'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/28'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/29'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/30'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/31'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/32'])).toBeNull();
-    expect(AddSourceWizardStepTwo.validateHosts(['0.0.0.12/33']).length).toBeGreaterThan(0);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/23'])).toBe(false);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/24'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/25'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/26'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/27'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/28'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/29'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/30'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/31'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/32'])).toBe(true);
+    expect(AddSourceWizardStepTwo.hostsValid(['0.0.0.12/33'])).toBe(false);
   });
 });
