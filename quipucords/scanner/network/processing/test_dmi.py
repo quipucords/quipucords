245372b3562afd1dfeab2c6532987fc34524b390
abaiken <aaiken@redhat.com>
2019-04-17 15:46:41 -0400

    handle multi line output of dmi system uuid command

---
+++ quipucords/scanner/network/processing/test_dmi.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2019 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+
+"""Unit tests for initial processing of dmi facts."""
+
+
+import unittest
+
+from scanner.network.processing import dmi
+from scanner.network.processing.util_for_test import ansible_result
+
+
+class TestProcessDmiSystemUuidr(unittest.TestCase):
+    """Test ProcessDmiSystemUuid."""
+
+    def test_success_case(self):
+        """Found cpu model ver."""
+        # stdout_lines looks like ['a', 'b', 'c']
+        dependencies = {'internal_dmi_system_uuid':
+                        ansible_result('a\nb\nc')}
+        self.assertEqual(
+            dmi.ProcessDmiSystemUuid.process(
+                'QPC_FORCE_POST_PROCESS', dependencies),
+            'a')
+        # stdout_lines looks like ['', 'b']
+        dependencies['internal_dmi_system_uuid'] = \
+            ansible_result('\nb\n')
+        self.assertEqual(
+            dmi.ProcessDmiSystemUuid.process(
+                'QPC_FORCE_POST_PROCESS', dependencies),
+            'b')
+        dependencies['internal_dmi_system_uuid'] = ansible_result('Failed', 1)
+        self.assertEqual(
+            dmi.ProcessDmiSystemUuid.process(
+                'QPC_FORCE_POST_PROCESS', dependencies),
+            '')
+
+    def test_not_found(self):
+        """Did not find dmi system uuid."""
+        dependencies = {}
+        self.assertEqual(
+            dmi.ProcessDmiSystemUuid.process(
+                'QPC_FORCE_POST_PROCESS', dependencies),
+            '')
