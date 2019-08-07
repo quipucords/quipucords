5de6e7070b2a3ca15360dad5c734bb59898a5c0c
Kevan Holdaway <kholdawa@redhat.com>
2019-08-07 15:07:57 -0400

    Add function to makefile again

--- deploy/server_run.sh
+++ deploy/server_run.sh
@@ -2,11 +2,7 @@
 
 source ~/venv/bin/activate
 
-make server-migrate -C /app
-
-python --version
-
-cat /app/deploy/setup_user.py | python /app/quipucords/manage.py shell --settings quipucords.settings -v 3
+make server-migrate server-set-superuser -C /app
 
 if [[ ${USE_SUPERVISORD,,} = "false" ]]; then
     cd /app/quipucords
