27f83f06f3667c8e6d2cfd9cc74f5f35ca172418
Nicole Aragao <nicolearagao@gmail.com>
2022-02-14 14:01:03 -0300

    adds modifications suggested by pylint

--- quipucords/quipucords/featureflag.py
+++ quipucords/quipucords/featureflag.py
@@ -1,39 +1,49 @@
-import os
+"""Filters feature flags from systen env variables."""
+
 import logging
+import os
 
 DEFAULT_FEATURE_FLAGS_VALUES = {
-    'OVERALL_STATUS': False}  # default values for feature flags that are already implemented
-VALID_VALUES_FOR_ENV_VARIABLES = ["0", "1"]
+    'OVERALL_STATUS': False}  # default values for feature flags that
+# are already implemented
+
+VALID_VALUES_FOR_ENV_VARIABLES = ['0', '1']
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
 def get_feature_flags_from_env():
-    """Filters feature flags from environment variables"""
-
+    """Filter feature flags from environment variables."""
     global DEFAULT_FEATURE_FLAGS_VALUES
     global VALID_VALUES_FOR_ENV_VARIABLES
     for key, value in os.environ.items():
-        if key.upper().startswith("QPC_FEATURE_"):
-            feature = key.upper().replace("QPC_FEATURE_", "")
+        if key.upper().startswith('QPC_FEATURE_'):
+            feature = key.upper().replace('QPC_FEATURE_', '')
             try:
                 if value in VALID_VALUES_FOR_ENV_VARIABLES:
                     DEFAULT_FEATURE_FLAGS_VALUES[feature] = bool(int(value))
             except ValueError:
-                logger.info(f"{value} can`t be converted to int, verify your environment variables.")
+                logger.info(' %s can`t be converted to int, verify your '
+                            'environment variables.', value)
                 continue
     return DEFAULT_FEATURE_FLAGS_VALUES
 
 
 class FeatureFlag:
+    """Handle environment variables and its status."""
 
     def __init__(self, initial_data):
+        """Attributes and values are generated dynamically.
+
+        Attributes keys-values are received in initial_data.
+        """
         for key, value in initial_data.items():
             setattr(self, key, value)
 
     def is_feature_active(self, feature_name):
+        """Return attribute value."""
         try:
             return getattr(self, feature_name)
         except AttributeError:
-            raise ValueError(f"{feature_name=} is not a valid input.")
+            raise ValueError(f'{feature_name=} is not a valid input.')
