cc3171b82cf7b3b0c9ab4525d484ee03993040c6
Bruno Ciconelle <bciconel@redhat.com>
2022-04-12 04:21:32 -0300

    Common infrastructure for integration tests

---
+++ quipucords/tests/constants.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2022  Red Hat, Inc.
+
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+
+"""Test constants."""
+
+from pathlib import Path
+
+PROJECT_ROOT_DIR = Path(__file__).absolute().parent.parent.parent
+
+POSTGRES_DB = "qpc-db"
+POSTGRES_PASSWORD = "qpc"
+POSTGRES_USER = "qpc"
+QPC_COMMIT = "test-commit"
+QPC_SERVER_PASSWORD = "test-password"
+QPC_SERVER_USERNAME = "test-username"
+READINESS_TIMEOUT_SECONDS = 60
+SCAN_TARGET_PASSWORD = "super-secret-password"
+SCAN_TARGET_SSH_PORT = "2222"
+SCAN_TARGET_USERNAME = "non-root-user"
---
+++ quipucords/tests/utils/container_wrappers.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2022  Red Hat, Inc.
+
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+
+"""Extends pytest-docker-tools wrappers with helpers for qpc tests."""
+
+from abc import ABCMeta, abstractmethod
+
+from pytest_docker_tools import wrappers
+
+from tests.constants import POSTGRES_USER
+from tests.utils.http import ApiClient
+
+SYSTEMCTL_ACTIVE_STATUS_STRING = "Active: active (running)"
+
+
+class ReadinessProbeMixin(metaclass=ABCMeta):
+    """Container wrapper mixin with a readiness_probe hook."""
+
+    @abstractmethod
+    def readiness_probe(self):
+        """
+        Return True if the application is ready.
+
+        Must be implemented on child classes.
+        """
+
+    def ready(self):
+        """Overload docker-py ready including readiness probe."""
+        if super().ready():
+            return self.readiness_probe()
+        return False
+
+
+class ScanTargetContainer(ReadinessProbeMixin, wrappers.Container):
+    """Container wrapper for scan target container."""
+
+    def readiness_probe(self):
+        """Return true if ssh daemon is up and running."""
+        ssh_daemon_status = self.exec_run("systemctl status sshd")
+        return SYSTEMCTL_ACTIVE_STATUS_STRING in ssh_daemon_status.output.decode()
+
+
+class PostgresContainer(ReadinessProbeMixin, wrappers.Container):
+    """Container wrapper for Postgres container."""
+
+    def readiness_probe(self):
+        """Return true when the database is ready to accept connections."""
+        pg_isready_command = self.exec_run(f"pg_isready -U {POSTGRES_USER}")
+        return pg_isready_command.exit_code == 0
+
+
+class QuipucordsContainer(ReadinessProbeMixin, wrappers.Container):
+    """Container wrapper for quipucords container."""
+
+    @property
+    def server_url(self):
+        """Quipucords public url."""
+        ip_address, port = self.get_addr("443/tcp")
+        return f"https://{ip_address}:{port}"
+
+    def readiness_probe(self):
+        """Return true if quipucords api is responsive."""
+        client = ApiClient(base_url=self.server_url)
+        response = client.get("api/v1/status/")
+        return response.ok
