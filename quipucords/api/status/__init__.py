81f4bccac256a605845e9c8b1f4ed25d4d60c607
Bruno Ciconelle <bciconel@redhat.com>
2022-07-21 09:04:28 -0300

    Add util to get and cache server_id

--- quipucords/api/status/__init__.py
+++ quipucords/api/status/__init__.py
@@ -1,1 +1,20 @@
-# noqa
+# Copyright (C) 2022  Red Hat, Inc.
+
+# This software is licensed to you under the GNU General Public License,
+# version 3 (GPLv3). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
+# along with this software; if not, see
+# https://www.gnu.org/licenses/gpl-3.0.txt.
+
+"""Status module."""
+
+from functools import lru_cache
+
+from .model import ServerInformation
+
+
+@lru_cache
+def get_server_id():
+    """Get server_id and cache it."""
+    return ServerInformation.create_or_retreive_server_id()
