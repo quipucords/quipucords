b2139ce30aa6d62975efa04722ab18ad37e4aed1
Bruno Ciconelle <bciconel@redhat.com>
2022-07-25 12:03:05 -0300

    Replace deprecated NullBooleanField
    >RemovedInDRF314Warning: The `NullBooleanField` is deprecated and will
    >be removed starting with 3.14. Instead use the `BooleanField` field and
    >set `allow_null=True` which does the same thing.
    
    Part of DISCOVERY-93

--- quipucords/api/source/serializer.py
+++ quipucords/api/source/serializer.py
@@ -17,9 +17,9 @@ import re
 from django.db import transaction
 from django.utils.translation import gettext as _
 from rest_framework.serializers import (
+    BooleanField,
     CharField,
     IntegerField,
-    NullBooleanField,
     PrimaryKeyRelatedField,
     ValidationError,
 )
@@ -63,9 +63,9 @@ class SourceOptionsSerializer(NotEmptySerializer):
 
     ssl_protocol = ValidStringChoiceField(
         required=False, choices=SourceOptions.SSL_PROTOCOL_CHOICES)
-    ssl_cert_verify = NullBooleanField(required=False)
-    disable_ssl = NullBooleanField(required=False)
-    use_paramiko = NullBooleanField(required=False)
+    ssl_cert_verify = BooleanField(allow_null=True, required=False)
+    disable_ssl = BooleanField(allow_null=True, required=False)
+    use_paramiko = BooleanField(allow_null=True, required=False)
 
     class Meta:
         """Metadata for serializer."""
