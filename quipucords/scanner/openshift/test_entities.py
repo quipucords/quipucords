12e7a3534330a3f81b37318eec405154201b2d5d
Bruno Ciconelle <bciconel@redhat.com>
2022-12-21 14:36:34 -0300

    feature(ocp): load entity function
    - function to load any dict as the equivalent OCP entity.
    
    Signed-off-by: Bruno Ciconelle <bciconel@redhat.com>

--- quipucords/scanner/openshift/test_entities.py
+++ quipucords/scanner/openshift/test_entities.py
@@ -9,9 +9,17 @@
 #
 """Test OCPEntities methods."""
 
+from copy import deepcopy
+
 import pytest
 
-from scanner.openshift.entities import OCPError
+from scanner.openshift.entities import (
+    OCPBaseEntity,
+    OCPDeployment,
+    OCPError,
+    OCPProject,
+    load_entity,
+)
 
 
 @pytest.mark.parametrize("reason", ["FOO", "BAR"])
@@ -33,3 +41,106 @@ def test_message_parsing(mocker, error_body, expected_message, status, reason):
     assert error.status == status
     assert error.reason == reason
     assert error.message == expected_message
+
+
+@pytest.fixture
+def deployment_data():
+    """Return data representing a Deployment."""
+    return dict(
+        kind="deployment",
+        name="deployment",
+        labels={"foo": "bar"},
+        container_images=["some-image"],
+        init_container_images=[],
+    )
+
+
+@pytest.fixture
+def project_data():
+    """Return data representing a Project."""
+    return dict(
+        name="some project",
+        kind="namespace",
+        labels={"foo": "bar"},
+        deployments=[],
+        errors={},
+    )
+
+
+@pytest.fixture
+def project_with_deployment(project_data, deployment_data):
+    """Return data representing a project + deployments."""
+    project_data["deployments"] = [deployment_data]
+    return project_data
+
+
+def test_load_nested(project_with_deployment):
+    """Test loading entity with other nested entities."""
+    entity = load_entity(project_with_deployment)
+    assert isinstance(entity, OCPProject)
+    assert isinstance(entity.deployments, list)
+    assert isinstance(entity.deployments[0], OCPDeployment)
+
+
+@pytest.mark.parametrize(
+    "data,expected_class",
+    [
+        (pytest.lazy_fixture("deployment_data"), OCPDeployment),
+        (pytest.lazy_fixture("project_data"), OCPProject),
+    ],
+)
+def test_load_entity(data, expected_class):
+    """Test load_entity function."""
+    # protect data for future comparison
+    expected_data = deepcopy(data)
+    entity = load_entity(data)
+    assert isinstance(entity, expected_class)
+    assert expected_data == entity.dict()
+
+
+class TestOCPBaseEntity:  # pylint: disable=missing-class-docstring, unused-variable
+    """Test OCPBaseEntity and its inheritance side effects."""
+
+    def test_kind_isnt_overridable(self):
+        """Test kind attribute ins't overriden at instance level."""
+
+        class TestEntity(OCPBaseEntity):
+            _kind = "not-overridable"
+            name: str
+
+        entity = TestEntity(kind="whatever", name="cool-name")
+        assert entity.kind == "not-overridable"
+
+    def test_missing_kind(self):
+        """Test OCP entity missing '_kind' attribute."""
+        with pytest.raises(
+            NotImplementedError, match="SomeClass MUST implement an attribute '_kind'"
+        ):
+
+            class SomeClass(OCPBaseEntity):
+                name: str
+
+    def test_reused_kind(self):
+        """Ensure kind can't be repeated."""
+
+        class TestClass1(OCPBaseEntity):
+            _kind = "test-entity"
+            name: str
+
+        with pytest.raises(
+            AssertionError, match="Entity with kind='test-entity' already registered."
+        ):
+
+            class TestClass2(OCPBaseEntity):
+                _kind = "test-entity"
+                name: str
+
+    def test_kind_is_available_to_subclass(self):
+        """Ensure kind is available on entity."""
+
+        class TestClass(OCPBaseEntity):
+            _kind = "test-available-to-subclass"
+            name: str
+
+        entity = TestClass(name="foo")
+        assert entity.kind is not None
