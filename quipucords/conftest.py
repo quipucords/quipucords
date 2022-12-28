b6df71c58951345389bc969632e6f9471c87ada0
Bruno Ciconelle <bciconel@redhat.com>
2022-12-28 16:21:07 -0300

    test(vcr): add vcr + its pytest plugin
    Co-authored-by: Nicole Aragão <nicolearagao@gmail.com>
    Signed-off-by: Bruno Ciconelle <bciconel@redhat.com>

--- quipucords/conftest.py
+++ quipucords/conftest.py
@@ -14,6 +14,7 @@ IMPORTANT: Don't import aplication code on toplevel as this can influence
 the proper patching.
 """
 # pylint: disable=import-outside-toplevel
+from functools import partial
 from urllib.parse import urljoin
 
 import pytest
@@ -43,3 +44,55 @@ def django_client(live_server):
         base_url=urljoin(live_server.url, "api/v1/"),
     )
     return client
+
+
+@pytest.fixture(scope="module")
+def vcr_config(_vcr_uri_map):
+    """
+    VCR configuration - should match the kwargs expected by VCR.use_cassettes.
+
+    https://github.com/kiwicom/pytest-recording#configuration
+    https://vcrpy.readthedocs.io/en/latest/configuration.html
+    """
+    replace_cassete_uri = partial(_replace_cassete_uri, uri_map=_vcr_uri_map)
+    return {
+        "filter_headers": [
+            ("authorization", "<AUTH_TOKEN>"),
+        ],
+        "before_record_request": replace_cassete_uri,
+    }
+
+
+@pytest.fixture(scope="module")
+def _vcr_uri_map():
+    """Return a dict mapping testing base URIs to its fallback values."""
+    from tests.constants import ConstantsFromEnv
+    from tests.env import BaseURI, EnvVar
+
+    uri_map = {}
+    for attr_name in dir(ConstantsFromEnv):
+        if attr_name.startswith("_"):
+            continue
+        attr_value = getattr(ConstantsFromEnv, attr_name)
+        if isinstance(attr_value, EnvVar) and attr_value.coercer == BaseURI:
+            assert (
+                attr_value.value not in uri_map
+            ), f"URI {attr_value.value} already in use."
+            uri_map[attr_value.value] = attr_value.fallback_value
+            uri_map[attr_value.fallback_value] = attr_value.fallback_value
+    return uri_map
+
+
+def _replace_cassete_uri(vcr_request, uri_map: dict):
+    """
+    Replace recorded request URI with its equivalent fallback URI.
+
+    Cassete recorded URIs are intended to be set on ConstantsFromEnv class
+    (see tests/constants.py module).
+    """
+    # https://vcrpy.readthedocs.io/en/latest/advanced.html#custom-request-filtering
+
+    vcr_base_uri = f"{vcr_request.scheme}://{vcr_request.host}:{vcr_request.port}"
+    fallback_base_uri = uri_map[vcr_base_uri]
+    vcr_request.uri = fallback_base_uri.replace_base_uri(vcr_request.uri)
+    return vcr_request
