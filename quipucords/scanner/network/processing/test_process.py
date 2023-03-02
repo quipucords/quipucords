36e5de18aa71863964eec409512b84db766fe7e5
Brad Smith <brasmith@redhat.com>
2023-03-02 13:37:21 -0500

    docs: clean up inconsistent copyright notices
    Unless otherwise stated, all source code files in this project are copyright Red Hat, Inc. and are licensed according to the terms of the LICENSE file.

--- quipucords/scanner/network/processing/test_process.py
+++ quipucords/scanner/network/processing/test_process.py
@@ -1,12 +1,3 @@
-# Copyright (c) 2018 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-
 """Unit tests for the process module."""
 from django.test import TestCase
 
