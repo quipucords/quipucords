27c0b76f757d3949b163d08e75607a0e690711c3
Brad Smith <brasmith@redhat.com>
2023-03-02 13:37:21 -0500

    docs: clean up inconsistent copyright notices
    Unless otherwise stated, all source code files in this project are copyright Red Hat, Inc. and are licensed according to the terms of the LICENSE file.

--- conftest.py
+++ conftest.py
@@ -1,12 +1,3 @@
-# Copyright (C) 2022  Red Hat, Inc.
-
-# This software is licensed to you under the GNU General Public License,
-# version 3 (GPLv3). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
-# along with this software; if not, see
-# https://www.gnu.org/licenses/gpl-3.0.txt.
-
 """
 pytest config for quipucords.
 
--- quipucords/scanner/openshift/__init__.py
+++ quipucords/scanner/openshift/__init__.py
@@ -1,12 +1,3 @@
-#
-# Copyright (c) 2022 Red Hat, Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 3 (GPLv3). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv3
-# along with this software; if not, see
-# https://www.gnu.org/licenses/gpl-3.0.txt.
 """Package for import organization."""
 
 from scanner.openshift.connect import ConnectTaskRunner
