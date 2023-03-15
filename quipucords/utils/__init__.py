280493a4792d265f3627de058bc92dc54ff515fa
Bruno Ciconelle <bciconel@redhat.com>
2023-03-15 19:26:37 -0300

    refactor: replace get_choice_ids with enum values

--- quipucords/utils/__init__.py
+++ quipucords/utils/__init__.py
@@ -4,9 +4,8 @@ Common utils for quipucords.
 All utils should be imported here so the internal api for importing is just
 "from utils import some_util_func".
 """
-import json
 
 from .deepget import deepget
 from .default_getter import default_getter
 from .get_from_object_or_dict import get_from_object_or_dict
-from .misc import get_choice_ids, load_json_from_tarball
+from .misc import load_json_from_tarball
