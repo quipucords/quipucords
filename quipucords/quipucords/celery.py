8ff278047abf9cd8bacf9e9e53b69d6fc1632c0b
Brad Smith <brasmith@redhat.com>
2023-03-30 17:00:42 -0400

    feat: add QPC_DISABLE_CELERY_LOGGING_HIJACK to bypass Celery logging hijack

--- quipucords/quipucords/celery.py
+++ quipucords/quipucords/celery.py
@@ -2,11 +2,13 @@
 
 import os
 
-from celery import Celery
+import environ
+from celery import Celery, signals
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quipucords.settings")
 
+env = environ.Env()
 app = Celery("quipucords")
 
 # Using a string here means the worker doesn't have to serialize
@@ -17,3 +19,18 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+if env.bool("QPC_DISABLE_CELERY_LOGGING_HIJACK", True):
+
+    @signals.setup_logging.connect
+    def on_celery_setup_logging(**kwargs):
+        """
+        Stop Celery from overriding default logging setup.
+
+        By default, celery hijacks the root logger. The configuration setting
+        CELERY_WORKER_HIJACK_ROOT_LOGGER only stops Celery from updating the handler;
+        celery still updates the formatter, and we can lose filters.
+
+        Since the formatter we want to use is the configured Django one,
+        we can just configure Celery not to touch logging.
+        """
