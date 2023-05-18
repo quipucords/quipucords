08fa077744913da9ee2c731c303da1e2b4b864b4
Bruno Ciconelle <bciconel@redhat.com>
2023-05-18 12:16:21 -0300

    test: helper to authenticate a test user
    Mixin intented for use with Django based unittests.

---
+++ quipucords/tests/mixins.py
@@ -0,0 +1,22 @@
+"""Mixins to help testing quipucords."""
+
+from django.contrib.auth import get_user_model
+from faker import Faker
+
+fake = Faker()
+
+
+class LoggedUserMixin:
+    """Mixin that logs a qpc user to TestCases."""
+
+    @classmethod
+    def _create_user(cls):
+        user = get_user_model()(username=fake.user_name())
+        user.set_password(fake.password())
+        user.save()
+        return user
+
+    def setUp(self):  # pylint: disable=invalid-name
+        """Set up data for test case."""
+        self.user = self._create_user()
+        self.client.force_login(self.user)
