687ed2e4bd711637022d61ef86e4db93ca8d5fba
Alberto Bellotti <abellott@redhat.com>
2023-06-07 19:04:39 -0400

    feat: ssh_keyvalue support - adding new ssh_keyvalue to credential model.
    - Adding the 64K encrypted ssh_keyvalue field to the Credential model.
    
      Background on the 64K limit:
    
      ssh-keygen with 16384 bit RSA key generated as 12.7KB private key file.
      Ansible vault encryption seems to be about a 1-4 size increase upon
      encrypting the data, so we're safely setting an upper limit of 64K
      for the ssh_keyvalue.

--- quipucords/api/credential/model.py
+++ quipucords/api/credential/model.py
@@ -39,6 +39,7 @@ class Credential(models.Model):
     password = models.CharField(max_length=1024, null=True)
     auth_token = models.CharField(max_length=1024, null=True)
     ssh_keyfile = models.CharField(max_length=1024, null=True)
+    ssh_keyvalue = models.CharField(max_length=65536, null=True)
     ssh_passphrase = models.CharField(max_length=1024, null=True)
     become_method = models.CharField(
         max_length=6, choices=BECOME_METHOD_CHOICES, null=True
@@ -49,6 +50,7 @@ class Credential(models.Model):
     ENCRYPTED_FIELDS = [
         "password",
         "ssh_passphrase",
+        "ssh_keyvalue",
         "become_password",
         "auth_token",
     ]
