046118e67ea3a6aebf039b1de6cdd7f79a6fad7d
Nicole Aragao <nicolearagao@gmail.com>
2023-07-04 18:58:35 -0300

    feature: create script to generate sudo cmds list
    This commit introduces a new script that analyzes Ansible playbooks to collect all commands
    requiring sudo permissions for execution. Additionally, it includes the following enhancements:
    
    Integration of a step in GitHub Actions to ensure the generated list is up to date.
    Addition of commands to the Makefile for generating the sudo command list and performing list comparison tests.

---
+++ scripts/generate_sudo_list.py
@@ -0,0 +1,72 @@
+"""Helper script to generate sudo list doc."""
+import argparse
+import filecmp
+import sys
+import tempfile
+from pathlib import Path
+
+import yaml
+
+
+def collect_sudo_commands(path=None):
+    """Collect all ansible cmds that require sudo."""
+    if not path:
+        path = Path("quipucords/scanner/network/runner")
+    list_cmds = []
+    for playbook in path.glob("roles/*/tasks/main.yml"):
+        for task in yaml.safe_load(playbook.open()):
+            if task.get("become", ""):
+                list_cmds.append(task.get("raw", ""))
+    list_cmds = [
+        cmd.strip() for cmd in list_cmds if isinstance(cmd, str) and cmd.strip()
+    ]
+    list_cmds = sorted(set(list_cmds))
+
+    return list_cmds
+
+
+def generate_sudo_cmds_document():
+    """Generate a document listing commands that require sudo."""
+    list_cmds = collect_sudo_commands()
+    document = ""
+    document += "\n".join(cmd for cmd in list_cmds if cmd.strip())
+    return document
+
+
+def create_document_file(file_path):
+    """Create a txt file containing a list of commands that require sudo."""
+    document = generate_sudo_cmds_document()
+    file = Path(file_path)
+    file.write_text(document)
+
+
+def compare_sudo_cmds_lists(file_path):
+    """Compare the most updated sudo list with an existing one."""
+    generated_doc = generate_sudo_cmds_document()
+
+    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+        temp_file.write(generated_doc.encode())
+        try:
+            if not filecmp.cmp(temp_file.name, file_path):
+                sys.stderr.write(
+                    "Error: The sudo command list document has been modified.\n"
+                )
+        except FileNotFoundError:
+            sys.stderr.write(f"Error: {file_path} has not been found.\n")
+        finally:
+            Path(temp_file.name).unlink()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "command", choices=["docs", "compare"], help="Command to execute"
+    )
+    parser.add_argument("file_path", help="Path to the file")
+
+    args = parser.parse_args()
+
+    if args.command == "docs":
+        create_document_file(args.file_path)
+    elif args.command == "compare":
+        compare_sudo_cmds_lists(args.file_path)
---
+++ scripts/test_generate_sudo_list.py
@@ -0,0 +1,179 @@
+"""Tests for generate sudo list script."""
+from unittest import mock
+
+import pytest
+
+from scripts.generate_sudo_list import (
+    collect_sudo_commands,
+    compare_sudo_cmds_lists,
+    create_document_file,
+    generate_sudo_cmds_document,
+)
+
+
+@pytest.fixture
+def playbook_content_with_sudo():
+    """Return playbook content with sudo cmds."""
+    playbook_content = """
+- name: Task 1
+  raw: sudo command1
+  become: yes
+
+- name: Task 2
+  raw: sudo command2
+  become: yes
+
+- name: Task 3
+  raw: command3
+"""
+    return playbook_content
+
+
+@pytest.fixture
+def playbook_content_without_sudo():
+    """Return playbook content with no cmds requiring sudo."""
+    playbook_content = """
+- name: Task 1
+  raw: command1
+  become: no
+
+- name: Task 2
+  raw: command2
+"""
+    return playbook_content
+
+
+@pytest.fixture
+def playbook_content_mixed_cmds():
+    """Return playbook content with a mix of sudo and non-sudo cmds."""
+    playbook_content = """
+- name: Task 1
+  raw: sudo command1
+  become: yes
+
+- name: Task 2
+  raw: command2
+"""
+    return playbook_content
+
+
+@pytest.fixture
+def return_playbook_path(
+    tmp_path,
+    playbook_content_with_sudo,
+    playbook_content_without_sudo,
+    playbook_content_mixed_cmds,
+    request,
+):
+    """Return the path to a temporary playbook file."""
+    test_name = request.node.name
+    if test_name.endswith("with_sudo"):
+        playbook = playbook_content_with_sudo
+    elif test_name.endswith("without_sudo"):
+        playbook = playbook_content_without_sudo
+    elif test_name.endswith("mixed_cmds"):
+        playbook = playbook_content_mixed_cmds
+
+    path = tmp_path / "quipucords" / "scanner" / "network" / "runner"
+    path.mkdir(parents=True, exist_ok=True)
+
+    role_dir = path / "roles" / "roles1" / "tasks"
+    role_dir.mkdir(parents=True, exist_ok=True)
+
+    main_yml_path = role_dir / "main.yml"
+    with open(main_yml_path, "w") as f:
+        f.write(playbook)
+
+    yield path
+
+
+@pytest.fixture
+def generated_document_mock():
+    """Return sudo cmds document."""
+    with mock.patch(
+        "scripts.generate_sudo_list.generate_sudo_cmds_document",
+        return_value="sudo command 1\nsudo command 2",
+    ) as mock_obj:
+        yield mock_obj
+
+
+def test_collect_commands_with_sudo(return_playbook_path):
+    """Verify if all the sudo raw commands are collected correctly."""
+    expected_cmds = [
+        "sudo command1",
+        "sudo command2",
+    ]
+    cmds = collect_sudo_commands(return_playbook_path)
+    assert cmds == expected_cmds
+
+
+def test_collect_commands_without_sudo(return_playbook_path):
+    """Verify that non-sudo raw commands are not collected."""
+    cmds = collect_sudo_commands(return_playbook_path)
+    assert cmds == []
+
+
+def test_collect_mixed_cmds(return_playbook_path):
+    """Verify that only sudo cmds are being collected."""
+    cmds = collect_sudo_commands(return_playbook_path)
+    assert cmds == ["sudo command1"]
+
+
+@mock.patch(
+    "scripts.generate_sudo_list.collect_sudo_commands",
+    return_value=["sudo command 1", "sudo command 2"],
+)
+def test_document_generation(list_cmds):
+    """Verify if sudo cmd document is being generated correctly."""
+    document = generate_sudo_cmds_document()
+    assert document == "sudo command 1\nsudo command 2"
+
+
+def test_file_creation(tmp_path, generated_document_mock):
+    """Verify if sudo file is being created with correct cmds."""
+    file_path = tmp_path / "sudo_list.txt"
+    create_document_file(file_path)
+    assert file_path.exists()
+    expected = ["sudo command 1\n", "sudo command 2"]
+
+    with open(file_path, "r") as f:
+        lines = f.readlines()
+    assert lines == expected
+    file_path.unlink()
+
+
+def test_compare_lists_equal_files(tmp_path, capsys):
+    """Verify expected output for equal sudo command lists."""
+    file_path = tmp_path / "sudo_list.txt"
+    with mock.patch("scripts.generate_sudo_list.filecmp.cmp", return_value=True):
+        compare_sudo_cmds_lists(file_path)
+    out, err = capsys.readouterr()
+
+    assert out == ""
+    assert err == ""
+
+
+def test_compare_lists_diff_files(tmp_path, capsys):
+    """Verify expected output for different sudo command lists."""
+    file_path = tmp_path / "sudo_list.txt"
+    with mock.patch("scripts.generate_sudo_list.filecmp.cmp", return_value=False):
+        compare_sudo_cmds_lists(file_path)
+    expected_stderr = "Error: The sudo command list document has been modified.\n"
+    out, err = capsys.readouterr()
+
+    assert err == expected_stderr
+    assert out == ""
+
+
+def test_compare_lists_nonexistent_file(tmp_path, capsys):
+    """Verify expected output for nonexistent sudo command file path."""
+    file_path = tmp_path / "sudo_list.txt"
+    with mock.patch(
+        "scripts.generate_sudo_list.filecmp.cmp", side_effect=FileNotFoundError()
+    ):
+        compare_sudo_cmds_lists(file_path)
+    expected_stderr = f"Error: {file_path} has not been found.\n"
+    out, err = capsys.readouterr()
+
+    assert err == expected_stderr
+    assert out == ""
