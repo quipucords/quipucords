c060358bae1d3dc983024ddcf189710675998617
Bruno FS Ciconelle <bciconel@redhat.com>
2023-10-02 15:57:07 -0300

    chore(BaseNormalizer): improve metadata handling
    Besides adding support for a new metadata (has_error), this also
    opens the door for normalizers to return NormalizedResult instead of
    just a normalized value. This will be important to define raw fact
    key used for fields with multiple dependencies/raw facts key dependencies.
    
    Relates to JIRA: DISCOVERY-226

--- quipucords/tests/scanner/test_normalizer.py
+++ quipucords/tests/scanner/test_normalizer.py
@@ -1,10 +1,11 @@
 """test normalizer 'framework'."""
 
 import json
+import logging
 
 import pytest
 
-from scanner.normalizer import BaseNormalizer, FactMapper
+from scanner.normalizer import BaseNormalizer, FactMapper, NormalizedResult
 
 
 def test_normalizer_creation():
@@ -38,14 +39,16 @@ def test_wrong_dependency_order():
 
 
 @pytest.mark.parametrize(
-    "raw_facts,expected_value",
+    "raw_facts, expected_value, expected_raw_facts, expected_has_error",
     [
-        ({"raw": "0"}, None),
-        ({"raw": "1"}, None),
-        ({"raw": "2"}, 2),
+        ({"raw": "0"}, None, None, True),
+        ({"raw": "1"}, None, None, True),
+        ({"raw": "2"}, 2, ["raw"], False),
     ],
 )
-def test_validators(raw_facts, expected_value, faker):
+def test_validators(
+    raw_facts, expected_value, expected_raw_facts, expected_has_error, faker
+):
     """Ensure validators are used (if provided)."""
 
     def _is_even(num):
@@ -63,9 +66,11 @@ def test_validators(raw_facts, expected_value, faker):
     norm = N(raw_facts=raw_facts, server_id=faker.uuid4())
     norm.normalize()
     assert norm.facts["even_fact"] == expected_value
+    assert norm.metadata["even_fact"]["has_error"] == expected_has_error
+    assert norm.metadata["even_fact"]["raw_fact_keys"] == expected_raw_facts
 
 
-def test_dependency(mocker):
+def test_dependency(faker):
     """Test normalization of facts with dependencies."""
     raw_facts = {"le_json": '{"final_answer": "42"}'}
 
@@ -78,31 +83,96 @@ def test_dependency(mocker):
             None, dependencies=["fact1"], normalizer_func=_normalize_fact2
         )
 
-    norm = N(raw_facts, mocker.ANY)
+    server_id = faker.uuid4()
+    norm = N(raw_facts, server_id)
     norm.normalize()
     assert norm.facts == {
         "fact1": {"final_answer": "42"},
         "fact2": 42,
     }
+    assert norm.metadata == {
+        "fact1": {
+            "has_error": False,
+            "raw_fact_keys": ["le_json"],
+            "server_id": server_id,
+            "source_type": None,
+        },
+        "fact2": {
+            "has_error": False,
+            "raw_fact_keys": ["le_json"],
+            "server_id": server_id,
+            "source_type": None,
+        },
+    }
     # ensure data that would break a normalizer only affect one field
-    norm2 = N({"le_json": '{"final_answer": "forty two"}'}, mocker.ANY)
+    server_id2 = faker.uuid4()
+    norm2 = N({"le_json": '{"final_answer": "forty two"}'}, server_id2)
     norm2.normalize()
     assert norm2.facts == {
         "fact1": {"final_answer": "forty two"},
         "fact2": None,
     }
+    assert norm2.metadata == {
+        "fact1": {
+            "has_error": False,
+            "raw_fact_keys": ["le_json"],
+            "server_id": server_id2,
+            "source_type": None,
+        },
+        "fact2": {
+            "has_error": True,
+            "raw_fact_keys": None,
+            "server_id": server_id2,
+            "source_type": None,
+        },
+    }
 
 
-def test_multiple_raw_facts(mocker):
-    """Test normalization of facts that depend on multiple raw facts directly."""
-    raw_facts = {"raw1": 2, "raw2": 3}
+class TestMultipleRawFacts:
+    """Test support for normalizers using multiple raw fact keys."""
 
-    def _normalizer(*, raw1, raw2):
+    @staticmethod
+    def _normalizer_simple(*, raw1, raw2):
         return raw1 + raw2
 
-    class N(BaseNormalizer):
-        fact = FactMapper(["raw1", "raw2"], _normalizer)
+    @staticmethod
+    def _normalizer_custom_result(*, raw1, raw2):
+        value = raw1 + raw2
+        return NormalizedResult(value=value, raw_fact_keys=["raw1", "raw2"])
 
-    norm = N(raw_facts, mocker.ANY)
-    norm.normalize()
-    assert norm.facts == {"fact": 5}
+    @staticmethod
+    def _normalizer_unknown_raw_fact(*, raw1, raw2):
+        value = raw1 + raw2
+        return NormalizedResult(value=value, raw_fact_keys=["raw3"])
+
+    @pytest.mark.parametrize(
+        "normalizer",
+        [
+            _normalizer_simple,
+            _normalizer_custom_result,
+        ],
+    )
+    def test_multiple_raw_facts(self, mocker, normalizer):
+        """Test normalization of facts that depend on multiple raw facts directly."""
+        raw_facts = {"raw1": 2, "raw2": 3}
+
+        class N(BaseNormalizer):
+            fact = FactMapper(["raw1", "raw2"], normalizer)
+
+        norm = N(raw_facts, mocker.ANY)
+        norm.normalize()
+        assert norm.facts == {"fact": 5}
+
+    def test_normalizer_with_unknown_raw_fact_key(self, mocker, caplog):
+        """Test a normalizer that says it depends on unexpected keys."""
+        raw_facts = {"raw1": 2, "raw2": 3}
+        caplog.set_level(logging.ERROR)
+
+        class N(BaseNormalizer):
+            fact = FactMapper(["raw1", "raw2"], self._normalizer_unknown_raw_fact)
+
+        norm = N(raw_facts, mocker.ANY)
+        with pytest.raises(
+            AssertionError, match="Unexpected raw facts used for fact 'fact': {'raw3'}"
+        ):
+            norm.normalize()
