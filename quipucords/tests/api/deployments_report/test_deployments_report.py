eb224e761017a24def0aef25688fc54422402ec5
Brad Smith <brasmith@redhat.com>
2023-10-03 16:17:28 -0400

    refactor: rewrite DeploymentReportTest class as pytest functions

--- quipucords/tests/api/deployments_report/test_deployments_report.py
+++ quipucords/tests/api/deployments_report/test_deployments_report.py
@@ -1,262 +1,205 @@
-"""Test the report API."""
+"""
+Test the deployments report API (/api/v1/reports/{id}/deployments/).
 
-import json
-import tarfile
-import uuid
+This module confirms that the CSV, JSON, and tarball (JSON inside .tar.gz) responses
+from the deployments report API are structured correctly. This test does not
+exhaustively test all possible fields since the shape of the outputs can change based
+on the types and contents of the collected facts and system fingerprints.
+"""
+import csv
 
-from django.core import management
-from django.test import TestCase
-from django.urls import reverse
+import pytest
 from rest_framework import status
 
-from api.common.common_report import create_report_version
-from api.common.report_json_gzip_renderer import ReportJsonGzipRenderer
-from api.deployments_report.csv_renderer import DeploymentCSVRenderer
+from api.common.common_report import REPORT_TYPE_DEPLOYMENT
+from api.deployments_report.model import DeploymentsReport, SystemFingerprint
 from api.deployments_report.util import sanitize_row
-from api.models import Credential, ServerInformation, Source
 from constants import DataSources
-from tests.api.details_report.test_details_report import MockRequest
-from tests.mixins import LoggedUserMixin
+from tests.factories import DeploymentReportFactory
+from tests.report_utils import extract_tarball_from_response
+from tests.utils import fake_semver
 
-EXPECTED_NUMBER_OF_FINGERPRINTS = 39
+DEPLOYMENTS_API_PATH = "/api/v1/reports/{0}/deployments/"
 
 
-class DeploymentReportTest(LoggedUserMixin, TestCase):
-    """Tests against the Deployment reports function."""
-
-    def setUp(self):
-        """Create test case setup."""
-        management.call_command("flush", "--no-input")
-        super().setUp()
-        self.net_source = Source.objects.create(
-            name="test_source", source_type=DataSources.NETWORK
-        )
-
-        self.net_cred = Credential.objects.create(
-            name="net_cred1",
-            cred_type=DataSources.NETWORK,
-            username="username",
-            password="password",
-            become_password=None,
-            ssh_keyfile=None,
-        )
-        self.net_source.credentials.add(self.net_cred)
-
-        self.net_source.hosts = ["1.2.3.4"]
-        self.net_source.save()
-        self.server_id = ServerInformation.create_or_retrieve_server_id()
-        self.report_version = create_report_version()
-        self.mock_request = MockRequest()
-        self.mock_renderer_context = {"request": self.mock_request}
-
-    def create_details_report(self, data):
-        """Call the create endpoint."""
-        url = reverse("reports-list")
-        return self.client.post(url, json.dumps(data), "application/json")
-
-    def create_details_report_expect_201(self, data):
-        """Create a source, return the response as a dict."""
-        response = self.create_details_report(data)
-        if response.status_code != status.HTTP_201_CREATED:
-            print("Failure cause: ")
-            print(response.json())
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        return response.json()
-
-    def create_details_report_expect_400(self, data):
-        """Create a source, return the response as a dict."""
-        response = self.create_details_report(data)
-        if response.status_code != status.HTTP_400_BAD_REQUEST:
-            print("Failure cause: ")
-            print(response.json())
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        return response.json()
-
-    def generate_fingerprints(self, os_name="RHEL", os_versions=None):
-        """Create a Report and DeploymentsReport for test."""
-        facts = []
-        fc_json = {
-            "report_type": "details",
-            "sources": [
-                {
-                    "server_id": self.server_id,
-                    "report_version": create_report_version(),
-                    "source_name": self.net_source.name,
-                    "source_type": self.net_source.source_type,
-                    "facts": facts,
-                }
-            ],
+@pytest.fixture
+def deployments_report(faker):
+    """Return a DeploymentsReport for one network source with two fingerprints."""
+    sources = [
+        {
+            "server_id": faker.uuid4(),
+            "source_type": DataSources.NETWORK,
+            "source_name": faker.slug(),
+            "report_version": f"{fake_semver()}+{faker.sha1()}",
+            "facts": [{"hello": "world", "goodnight": "moon"}],
         }
+    ]
+    deployments_report = DeploymentReportFactory(
+        report__sources=sources, number_of_fingerprints=2
+    )
+    return deployments_report
 
-        if os_versions is None:
-            os_versions = ["7.3", "7.4"]
 
-        for version in os_versions:
-            release = f"{os_name} {version}"
-            fact_json = {
-                "connection_host": "1.2.3.4",
-                "connection_port": 22,
-                "connection_uuid": str(uuid.uuid4()),
-                "cpu_count": 2,
-                "cpu_core_per_socket": 1,
-                "cpu_siblings": 1,
-                "cpu_hyperthreading": False,
-                "cpu_socket_count": 2,
-                "cpu_core_count": 2,
-                "date_anaconda_log": "2017-07-18",
-                "date_yum_history": "2017-07-18",
-                "etc_release_name": os_name,
-                "etc_release_version": version,
-                "etc_release_release": release,
-                "uname_hostname": "1.2.3.4",
-                "virt_virt": "virt-guest",
-                "virt_type": "vmware",
-                "virt_num_guests": 1,
-                "virt_num_running_guests": 1,
-                "virt_what_type": "vt",
-            }
-            facts.append(fact_json)
-        report = self.create_details_report_expect_201(fc_json)
-        return report
+def test_sanitize_row():
+    """Test sanitize_row function."""
+    assert sanitize_row(["data", None, "data,data"]) == ["data", None, "data;data"]
 
-    def test_get_details_report_group_report(self):
-        """Get a specific group count report."""
-        url = "/api/v1/reports/1/deployments/"
 
-        # Create a system fingerprint via collection receiver
-        self.generate_fingerprints(os_versions=["7.4", "7.4", "7.5"])
+def assert_response_dict_roughly_matches_actual_fingerprint(
+    system_fingerprint: SystemFingerprint, response_dict: dict
+):
+    """
+    Spot-check a few individual values from the reported fingerprint.
 
-        # Query API
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        report = response.json()
-        self.assertIsInstance(report, dict)
-        self.assertEqual(
-            len(report["system_fingerprints"][0].keys()),
-            EXPECTED_NUMBER_OF_FINGERPRINTS,
+    Full disclosure: These fields were picked because they are consistently populated
+    by SystemFingerprintFactory that this test module uses to generate data.
+    """
+    assert response_dict["architecture"] == system_fingerprint.architecture
+    assert response_dict["bios_uuid"] == system_fingerprint.bios_uuid
+    assert response_dict["name"] == system_fingerprint.name
+    assert response_dict["os_release"] == system_fingerprint.os_release
+    # The following two attributes are formatted differently in CSV and JSON.
+    # The CSV gets mangled string representations of list and dict objects.
+    # So, simply assert truthiness if they are strings, else assert actual equality.
+    if isinstance(response_dict["installed_products"], str):
+        assert bool(response_dict["installed_products"]) == bool(
+            system_fingerprint.installed_products
         )
-
-    def test_get_deployments_report_404(self):
-        """Fail to get a report for missing collection."""
-        url = "/api/v1/reports/2/deployments/"
-
-        # Create a system fingerprint via collection receiver
-        self.generate_fingerprints(os_versions=["7.4", "7.4", "7.5"])
-
-        # Query API
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    def test_get_bad_deployment_report(self):
-        """Test case where Report exists but no fingerprint."""
-        url = "/api/v1/reports/1/deployments/"
-
-        # Create a system fingerprint via collection receiver
-        facts = []
-        fc_json = {
-            "report_type": "details",
-            "sources": [
-                {
-                    "server_id": self.server_id,
-                    "report_version": create_report_version(),
-                    "source_name": self.net_source.name,
-                    "source_type": self.net_source.source_type,
-                    "facts": facts,
-                }
-            ],
-        }
-
-        fact_json = {
-            "cpu_core_count": "cat",
-        }
-        facts.append(fact_json)
-        self.create_details_report_expect_400(fc_json)
-
-        # Query API
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_424_FAILED_DEPENDENCY)
-
-    def test_get_details_report_bad_id(self):
-        """Fail to get a report for missing collection."""
-        url = "/api/v1/reports/string/deployments/"
-
-        # Query API
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
-    ##############################################################
-    # Test CSV Renderer
-    ##############################################################
-
-    def test_sanitize_row(self):
-        """Test sanitize_row function."""
-        self.assertEqual(
-            sanitize_row(["data", None, "data,data"]), ["data", None, "data;data"]
+    else:
+        assert (
+            response_dict["installed_products"] == system_fingerprint.installed_products
         )
-
-    def test_csv_renderer(self):
-        """Test DeploymentCSVRenderer."""
-        renderer = DeploymentCSVRenderer()
-        # Test no FC id
-        test_json = {}
-        value = renderer.render(test_json, renderer_context=self.mock_renderer_context)
-        self.assertIsNone(value)
-
-        # Test doesn't exist
-        test_json = {"id": 42}
-        value = renderer.render(test_json, renderer_context=self.mock_renderer_context)
-        self.assertIsNone(value)
-
-        # Create a system fingerprint via collection receiver
-        self.generate_fingerprints(os_versions=["7.4", "7.4", "7.5"])
-        url = "/api/v1/reports/1/deployments/"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        report = response.json()
-
-        csv_result = renderer.render(
-            report, renderer_context=self.mock_renderer_context
+    if isinstance(response_dict["ip_addresses"], str):
+        assert bool(response_dict["ip_addresses"]) == bool(
+            system_fingerprint.ip_addresses
         )
-        # skip csv headers and last line
-        csv_lines = csv_result.splitlines()[5:-1]
+    else:
+        assert response_dict["ip_addresses"] == system_fingerprint.ip_addresses
 
-        data_rows = [
-            "architecture,bios_uuid,cloud_provider,cpu_core_count,cpu_count,cpu_hyperthreading,cpu_socket_count,detection-acs,detection-ansible,detection-network,detection-openshift,detection-satellite,detection-vcenter,entitlements,etc_machine_id,infrastructure_type,insights_client_id,installed_products,ip_addresses,is_redhat,jboss brms,jboss eap,jboss fuse,jboss web server,mac_addresses,name,os_name,os_release,os_version,redhat_certs,redhat_package_count,sources,subscription_manager_id,system_addons,system_creation_date,system_last_checkin_date,system_memory_bytes,system_role,system_service_level_agreement,system_usage_type,system_user_count,user_login_history,virtual_host_name,virtual_host_uuid,virtualized_type,vm_cluster,vm_datacenter,vm_dns_name,vm_host_core_count,vm_host_socket_count,vm_state,vm_uuid",  # noqa: E501
-            ",,,2,2,,2,False,False,True,False,False,False,,,virtualized,,,,,absent,absent,absent,absent,,1.2.3.4,RHEL,RHEL 7.4,7.4,,,[test_source],,,2017-07-18,,,,,,,,,,vmware,,,,,,,",  # noqa: E501
-            ",,,2,2,False,2,False,False,True,False,False,False,,,virtualized,,,,,absent,absent,absent,absent,,1.2.3.4,RHEL,RHEL 7.4,7.4,,,[test_source],,,2017-07-18,,,,,,,,,,vmware,,,,,,,",  # noqa: E501
-            ",,,2,2,False,2,False,False,True,False,False,False,,,virtualized,,,,,absent,absent,absent,absent,,1.2.3.4,RHEL,RHEL 7.5,7.5,,,[test_source],,,2017-07-18,,,,,,,,,,vmware,,,,,,,",  # noqa: E501
-        ]
-        assert len(csv_lines) == len(data_rows)
-        for expected_row, csv_row in zip(data_rows, csv_lines):
-            assert expected_row.split(",") == csv_row.split(",")
 
-    ##############################################################
-    # Test Json Gzip Render
-    ##############################################################
-    def test_json_gzip_renderer(self):
-        """Test ReportJsonGzipRenderer for deployments."""
-        renderer = ReportJsonGzipRenderer()
-        # Test no FC id
-        test_json = {}
-        value = renderer.render(test_json)
-        self.assertIsNone(value)
+def test_get_deployments_report_as_csv(django_client, deployments_report):
+    """
+    Test deployments report rendering as CSV.
 
-        # Test doesn't exist
-        test_json = {"id": 42}
-        value = renderer.render(test_json)
-        self.assertIsNone(value)
+    This effectively tests DeploymentCSVRenderer.
+    """
+    response = django_client.get(
+        DEPLOYMENTS_API_PATH.format(deployments_report.report.id),
+        headers={"Accept": "text/csv"},
+    )
+    assert response.ok, f"response was not ok; status {response.status_code}"
 
-        # Create a system fingerprint via collection receiver
-        self.generate_fingerprints(os_versions=["7.4", "7.4", "7.5"])
-        url = "/api/v1/reports/1/deployments/"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        report_dict = response.json()
+    # Sanity-check the basic structure of the tarball deployments.csv directly.
+    deployments_csv_data = list(csv.reader(response.text.splitlines()))
+    # We expect a table header, one line in the first table, two blank lines, a section
+    # marker, another table header, one line per system fingerprint in the second table,
+    # and another blank line at the end. (1 + 1 + 2 + 1 + 1 + n + 1, or 7 + n)
+    # Since our fixture creates two fingerprints, the total should be 9.
+    # This CSV is a mess. Please put it out of its misery. >:(
+    assert deployments_report.system_fingerprints.count() == 2
+    assert len(deployments_csv_data) == 9
 
-        # Test that the data in the subfile equals the report_dict
-        tar_gz_result = renderer.render(report_dict)
-        with tarfile.open(fileobj=tar_gz_result) as tar:
-            json_file = tar.getmembers()[0]
-            tar_info = tar.extractfile(json_file)
-            tar_dict_data = json.loads(tar_info.read().decode())
-            self.assertEqual(tar_dict_data, report_dict)
+    # Directly check the CSV contents up to but not including the second table.
+    expected_first_few_rows = [
+        [
+            "Report ID",
+            "Report Type",
+            "Report Version",
+            "Report Platform ID",
+        ],
+        [
+            str(deployments_report.report.id),
+            REPORT_TYPE_DEPLOYMENT,
+            "REPORT_VERSION",
+            str(deployments_report.report_platform_id),
+        ],
+        [],
+        [],
+        ["System Fingerprints:"],
+    ]
+    assert (
+        deployments_csv_data[: len(expected_first_few_rows)] == expected_first_few_rows
+    )
+    # Spot-check a couple individual values in the reported fingerprints.
+    expected_first_fingerprint = deployments_report.system_fingerprints.first()
+    reported_first_fingerprint = dict(
+        zip(deployments_csv_data[5], deployments_csv_data[6])
+    )
+    assert_response_dict_roughly_matches_actual_fingerprint(
+        expected_first_fingerprint, reported_first_fingerprint
+    )
+
+
+def test_get_deployments_report_as_json(django_client, deployments_report):
+    """Test deployments report rendering as JSON."""
+    response = django_client.get(
+        DEPLOYMENTS_API_PATH.format(deployments_report.report.id),
+        headers={"Accept": "application/json"},
+    )
+    assert response.ok, f"response was not ok; status {response.status_code}"
+
+    deployments_json = response.json()
+    deployments_json_fingerprints = deployments_json.pop("system_fingerprints")
+
+    expected_json_without_fingerprints = {
+        "report_id": deployments_report.report.id,
+        "status": DeploymentsReport.STATUS_COMPLETE,
+        "report_type": REPORT_TYPE_DEPLOYMENT,
+        "report_version": "REPORT_VERSION",
+        "report_platform_id": str(deployments_report.report_platform_id),
+    }
+    assert deployments_json == expected_json_without_fingerprints
+
+    expected_fingerprints = deployments_report.system_fingerprints
+    assert len(deployments_json_fingerprints) == expected_fingerprints.count()
+    expected_first_fingerprint = expected_fingerprints.first()
+    reported_first_fingerprint = deployments_json_fingerprints[0]
+    assert_response_dict_roughly_matches_actual_fingerprint(
+        expected_first_fingerprint, reported_first_fingerprint
+    )
+
+
+def test_get_deployment_report_as_tarball(django_client, deployments_report):
+    """
+    Test deployments report rendering as a JSON file inside a tarball (tar.gz).
+
+    This effectively tests ReportJsonGzipRenderer by extracting its response and
+    comparing it to the response generated from the regular JSON API.
+    """
+    path = DEPLOYMENTS_API_PATH.format(deployments_report.report.id)
+    gzip_response = django_client.get(path, headers={"Accept": "application/json+gzip"})
+    assert (
+        gzip_response.ok
+    ), f"gzip response was not ok; status {gzip_response.status_code}"
+    json_response = django_client.get(path, headers={"Accept": "application/json"})
+    assert (
+        json_response.ok
+    ), f"json response was not ok; status {json_response.status_code}"
+
+    extracted_files = extract_tarball_from_response(gzip_response, decode_json=True)
+    assert len(extracted_files) == 1
+    extracted_file = list(extracted_files.values())[0]
+    assert extracted_file == json_response.json()
+
+
+def test_get_deployment_report_invalid_id_not_found(django_client):
+    """Test getting a report for an invalid report ID responds with 404."""
+    response = django_client.get(DEPLOYMENTS_API_PATH.format("invalid"))
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_get_deployment_report_unknown_id_not_found(django_client):
+    """Test getting a report for a report ID that does not exist responds with 404."""
+    response = django_client.get(DEPLOYMENTS_API_PATH.format("1"))
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_get_deployments_report_not_complete_424_failed(django_client):
+    """Test getting a report with incomplete status responds with 424."""
+    deployments_report = DeploymentReportFactory(
+        number_of_fingerprints=0, status=DeploymentsReport.STATUS_PENDING
+    )
+    response = django_client.get(
+        DEPLOYMENTS_API_PATH.format(deployments_report.report.id)
+    )
+    assert response.status_code == status.HTTP_424_FAILED_DEPENDENCY
