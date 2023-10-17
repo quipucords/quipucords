04efe3b5965b124535579142f7f981f02c448440
Alberto Bellotti <abellott@redhat.com>
2023-10-17 15:53:27 -0400

    feature(passwords): make sure we do the password validation
    For the server password specified, let's make sure we pass
    our password validations. If password validation fails, we display
    the failed validation error and do a failure exit.

--- deploy/setup_user.py
+++ deploy/setup_user.py
@@ -1,8 +1,10 @@
 """Setup admin user for quipucords server."""
 
 import os
+import sys
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation
+from django.core.exceptions import ValidationError
 
 from quipucords.user import create_random_password
 
@@ -13,12 +15,28 @@ QPC_SERVER_USER_EMAIL = os.environ.get("QPC_SERVER_USER_EMAIL", "admin@example.c
 QPC_SERVER_PASSWORD = os.environ.get("QPC_SERVER_PASSWORD")
 
 
+def validate_server_password(password):
+    """Validate the server password specified."""
+    # We want to make sure the server password specified passes our password validators.
+    # If validation fails, let's display the errors for the failed validations
+    # and trigger a failure exit.
+    try:
+        password_validation.validate_password(password)
+    except ValidationError as error:
+        print("Invalid server password specified:", file=sys.stderr)
+        for err in error:
+            print(f"- {err}", file=sys.stderr)
+        sys.exit(1)
+
+
 ADMIN_NOT_PRESENT = User.objects.filter(username=QPC_SERVER_USERNAME).count() == 0
 
 if ADMIN_NOT_PRESENT:
     use_random_password = not QPC_SERVER_PASSWORD
     if use_random_password:
         QPC_SERVER_PASSWORD = create_random_password()
+    else:
+        validate_server_password(QPC_SERVER_PASSWORD)
     User.objects.create_superuser(
         QPC_SERVER_USERNAME, QPC_SERVER_USER_EMAIL, QPC_SERVER_PASSWORD
     )
@@ -33,6 +51,7 @@ elif QPC_SERVER_PASSWORD:
     # Note: If the QPC_SERVER_PASSWORD is specified, that may be coming to us
     # from an OpenShift or Kubernetes secret, we need to update the admin password
     # accordingly.
+    validate_server_password(QPC_SERVER_PASSWORD)
     user = User.objects.get(username=QPC_SERVER_USERNAME)
     user.set_password(QPC_SERVER_PASSWORD)
     user.save()
