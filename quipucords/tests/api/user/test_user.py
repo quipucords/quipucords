6833ff244b7bc5c3b0cd766bf017ddf9b6f28937
Bruno FS Ciconelle <bciconel@redhat.com>
2024-01-18 19:36:46 -0300

    tests: pytestfy quipucords/tests/api/user/test_user.py

--- quipucords/tests/api/user/test_user.py
+++ quipucords/tests/api/user/test_user.py
@@ -1,28 +1,18 @@
 """Test the API application."""
 
 
-from django.contrib.auth.models import User
-from django.test import TestCase
-from django.urls import reverse
+import pytest
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate
-
-from api.views import UserViewSet
+from rest_framework.reverse import reverse
 
 
-class UserTest(TestCase):
+@pytest.mark.django_db
+class TestUser:
     """Test the basic user APIs."""
 
-    def setUp(self):
-        """Create test case setup."""
-        self.user = User.objects.create_superuser("test", "test@example.com", "pass")
-
-    def test_current(self):
+    def test_current(self, qpc_user, django_client):
         """Test the current API endpoint."""
         url = reverse("users-current")
-        factory = APIRequestFactory()
-        request = factory.get(url, content_type="application/json")
-        force_authenticate(request, user=self.user)
-        user_current = UserViewSet.as_view({"get": "current"})
-        response = user_current(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = django_client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {"username": qpc_user.username}
