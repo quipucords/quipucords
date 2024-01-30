c1c64eb3cc678d21dd50dab9651f8443d11e39e2
Brad Smith <brasmith@redhat.com>
2024-01-30 13:58:32 -0500

    fix: sanitize raw ansible values before storing them to the database
    Relates to JIRA: DISCOVERY-529
    https://issues.redhat.com/browse/DISCOVERY-529

---
+++ quipucords/tests/utils/test_sanitize_for_utf8_compatibility.py
@@ -0,0 +1,23 @@
+"""Tests for `utils.sanitize_for_utf8_compatibility`."""
+import pytest
+
+from utils import sanitize_for_utf8_compatibility
+
+
+@pytest.mark.parametrize(
+    "original,expected",
+    [
+        ("potato", "potato"),
+        ("potato\udcc0", "potato?"),
+        ("πø†åTØ🥔", "πø†åTØ🥔"),  # non-ASCII UTF-8 characters are okay
+        (["a", "b\udcc0"], ["a", "b?"]),
+        ({"a": "A\udcc0", "b\udcc0": "B"}, {"a": "A?", "b?": "B"}),
+        ({"a": ["A\udcc0"], "b\udcc0": "B"}, {"a": ["A?"], "b?": "B"}),
+        (("a", "b\udcc0"), ("a", "b?")),
+        (b"\udcc0", b"\udcc0"),  # we do not change raw bytes objects
+        (None, None),
+    ],
+)
+def test_sanitize_for_utf8_compatibility(original, expected):
+    """Test values are santized for utf-8 compatibility."""
+    assert sanitize_for_utf8_compatibility(original) == expected
--- quipucords/utils/misc.py
+++ quipucords/utils/misc.py
@@ -5,3 +5,31 @@ import json
 def load_json_from_tarball(json_filename, tarball):
     """Extract a json as dict from given TarFile interface."""
     return json.loads(tarball.extractfile(json_filename).read())
+
+
+def sanitize_for_utf8_compatibility(value):
+    """
+    Sanitize the given value of non-UTF-8 bytes.
+
+    Ansible's raw facts may contain unexpected bytes in their `str` representations
+    that cannot be encoded as UTF-8. We need to ensure the strings are UTF-8 safe
+    before we store them in the database. This means we may lose some bytes from the
+    original values, but by using `errors="replace"`, any unexpected bytes will be
+    replaced with the `?` character.
+
+    Those original values could be strings, dicts... who knows?! So, we try a few
+    different approaches here to cover most cases. We are invoking recursive calls,
+    but I expect the objects we sanitize are shallow enough not to be a problem.
+    """
+    if isinstance(value, str):
+        return value.encode("utf-8", errors="replace").decode()
+    if isinstance(value, dict):
+        return {
+            sanitize_for_utf8_compatibility(key): sanitize_for_utf8_compatibility(value)
+            for key, value in value.items()
+        }
+    if isinstance(value, list):
+        return [sanitize_for_utf8_compatibility(value) for value in value]
+    if isinstance(value, tuple):
+        return tuple(sanitize_for_utf8_compatibility(value) for value in value)
+    return value
