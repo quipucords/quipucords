8008010162247e1ded5689d66b2475d3c34496e0
Brad Smith <brasmith@redhat.com>
2024-02-13 13:16:51 -0500

    feat: set Clear-Site-Data header on logout response
    Relates to JIRA: DISCOVERY-468
    https://issues.redhat.com/browse/DISCOVERY-468

--- quipucords/tests/api/user/test_user.py
+++ quipucords/tests/api/user/test_user.py
@@ -1,18 +1,25 @@
 """Test the API application."""
 
-
 import pytest
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.reverse import reverse
 
+pytestmark = pytest.mark.django_db  # all user tests require the database
 
-@pytest.mark.django_db
-class TestUser:
-    """Test the basic user APIs."""
 
-    def test_current(self, qpc_user, django_client):
-        """Test the current API endpoint."""
-        url = reverse("v1:users-current")
-        response = django_client.get(url)
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == {"username": qpc_user.username}
+def test_current(qpc_user, django_client):
+    """Test the current API endpoint."""
+    url = reverse("v1:users-current")
+    response = django_client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"username": qpc_user.username}
+
+
+def test_logout_sends_clear_header(qpc_user, django_client):
+    """Test that user logout sends the Clear-Site-Data header."""
+    logout_url = reverse("v1:users-logout")
+    response = django_client.put(logout_url)
+    assert response.status_code == status.HTTP_200_OK
+    assert "Clear-Site-Data" in response.headers
+    assert response.text == ""
