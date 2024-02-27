27cb05e582d91d37a65f59fa9208d14ada0a3fa6
Bruno FS Ciconelle <bciconel@redhat.com>
2024-02-27 15:37:50 -0300

    refactor(ScanJob): bind InspectResult to ScanTask

--- quipucords/api/inspectresult/model.py
+++ quipucords/api/inspectresult/model.py
@@ -51,6 +51,7 @@ class InspectResult(models.Model):
     task_inspection_result = models.ForeignKey(
         TaskInspectionResult, on_delete=models.CASCADE, related_name="systems"
     )
+    tasks = models.ManyToManyField("ScanTask", related_name="inspect_results")
 
     class Meta:
         """Metadata for model."""
