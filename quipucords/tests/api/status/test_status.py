588f25361210f62b6b91f61aacfc5e46dc88075c
Alberto Bellotti <abellott@redhat.com>
2024-04-03 04:16:02 -0400

    feature(ping): Adding a public ping endpoint
    - adding a public /api/v1/ping/ endpoint
      text/plain pong response
      can be used for liveness probes

--- quipucords/tests/api/status/test_status.py
+++ quipucords/tests/api/status/test_status.py
@@ -6,10 +6,10 @@ from rest_framework.reverse import reverse
 from quipucords.environment import server_version
 
 
-@pytest.mark.django_db
 class TestStatus:
     """Tests the status view."""
 
+    @pytest.mark.django_db
     def test_status_endpoint(self, client_logged_in):
         """Test the status endpoint."""
         url = reverse("v1:server-status")
@@ -17,3 +17,10 @@ class TestStatus:
         assert response.headers.get("X-Server-Version")
         assert response.headers["X-Server-Version"] == server_version()
         assert response.json()["api_version"] == 1
+
+    def test_ping_endpoint(self, client_logged_out):
+        """Test the public ping endpoint."""
+        url = reverse("v1:server-ping")
+        response = client_logged_out.get(url)
+        assert response.headers.get("Content-type") == "text/plain"
+        assert response.text == "pong"
