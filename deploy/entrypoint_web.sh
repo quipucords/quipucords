4be98ea61705a7f0683e8400d94cc94c6d5c03a9
Bruno FS Ciconelle <bciconel@redhat.com>
2024-04-05 14:54:37 -0300

    fix(ssl): replace hardcoded certs with on-the-fly generated certs
    Users users to provide their own certificates when using a folder mounted to /etc/ssl/qpc/.
    Otherwise, certificates are generated on-the-fly to avoid the original antipattern.
    
    Relates to JIRA: DISCOVERY-361

--- deploy/entrypoint_web.sh
+++ deploy/entrypoint_web.sh
@@ -1,8 +1,36 @@
 #!/usr/bin/env bash
 
+set -e
+
+handle_certificates () {
+    # verify if user provided certificates exist or create a self signed certificate.
+    CERTS_PATH="/etc/ssl/qpc"
+    mkdir -p ${CERTS_PATH}
+    if [ -f "${CERTS_PATH}/server.key" ] && [ -f "${CERTS_PATH}/server.crt" ]; then
+        echo "Using user provided certificates..."
+        openssl rsa -in "${CERTS_PATH}/server.key" -check
+        openssl x509 -in "${CERTS_PATH}/server.crt" -text -noout
+    elif [ ! -f "${CERTS_PATH}/server.key" ] && [ ! -f "${CERTS_PATH}/server.crt" ]; then
+        echo "No certificates provided. Creating them..."
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -keyout "${CERTS_PATH}/server.key" \
+            -out "${CERTS_PATH}/server.crt" \
+            -subj "/C=US/ST=Raleigh/L=Raleigh/O=IT/OU=IT Department/CN=example.com"
+    else
+        echo "Either key or certificate is missing."
+        echo "Please provide both named as server.key and server.crt."
+        echo "Tip: this container expects these files at ${CERTS_PATH}/"
+        exit 1
+    fi
+}
+
 if [[ "${QPC_ENABLE_CELERY_SCAN_MANAGER:-0}" == "0" ]]; then
     # ssh-agent is required for thread-based scan manager
     eval `ssh-agent -s`
+    # handling certificates is only required for our legacy gunicorn configuration.
+    # in the future discovery won't require any of this and a nginx proxy will
+    # handle this (DISCOVERY-522)
+    handle_certificates
     GUNICORN_CONF="/deploy/legacy_gunicorn_conf.py"
 else
     # our gunicorn config is a relic from the past - gunicorn standards are good enough
