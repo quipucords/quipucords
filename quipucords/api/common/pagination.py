e7a1c7d03b13bea4ab751af51e6e90e14425d2cf
Alberto Bellotti <abellott@redhat.com>
2024-04-05 04:40:43 -0400

    chore(pagination): Upping the default paging size to 100
    - While the UI may have restriction to not show more than 10 items
      at a time, the default paging size for an API back-end should
      be larger than 10 items.
    
      we have the enforced paging size limit of 1000, let's up the default
      paging size to 100.
    
      UI was calling the requests with the appropriate ?page_size=10 so this
      should have no effect there.

--- quipucords/api/common/pagination.py
+++ quipucords/api/common/pagination.py
@@ -6,6 +6,6 @@ from rest_framework.pagination import PageNumberPagination
 class StandardResultsSetPagination(PageNumberPagination):
     """Create standard paginiation class with page size."""
 
-    page_size = 10
+    page_size = 100
     page_size_query_param = "page_size"
     max_page_size = 1000
