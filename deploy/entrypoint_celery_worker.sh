db712a8f8be9b8f3ecc593e80d9994b0e8f164b4
Brad Smith <brasmith@redhat.com>
2024-04-07 15:48:59 -0400

    feat: check psql liveness before starting celery workers

--- deploy/entrypoint_celery_worker.sh
+++ deploy/entrypoint_celery_worker.sh
@@ -2,4 +2,8 @@
 # shellcheck disable=SC2312
 eval "$(ssh-agent -s)"
 
+SCRIPT_PATH=$(realpath "${BASH_SOURCE[0]}")
+SCRIPT_DIR=$(dirname "${SCRIPT_PATH}")
+"${SCRIPT_DIR}"/check_related_services_liveness.sh
+
 make celery-worker 2>&1 | tee -a /var/log/celery_worker.log
