79ff2db1f23509781ddd697b9335bafb05aba56d
Brad Smith <brasmith@redhat.com>
2024-05-31 17:23:03 -0400

    refactor: rewrite deploy/setup_user.py as a Django management command "create_or_update_user" so we can test and update its behavior
    Relates to JIRA: DISCOVERY-683

---
+++ quipucords/api/management/commands/create_or_update_user.py
@@ -0,0 +1,67 @@
+"""Create or update user credentials."""
+
+from argparse import RawTextHelpFormatter
+from os import environ
+from textwrap import dedent
+
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand, CommandError
+
+from quipucords.user import InvalidPasswordError, create_or_update_user
+
+User = get_user_model()
+
+DEFAULT_USERNAME = "admin"
+DEFAULT_EMAIL = "admin@example.com"
+
+
+class Command(BaseCommand):
+    """
+    Management command for (re)setting user credentials.
+
+    Creates or updates the user based on the following environment variables:
+
+    * QPC_SERVER_USERNAME (default: "{DEFAULT_USERNAME}")
+    * QPC_SERVER_EMAIL (default: "{DEFAULT_EMAIL}")
+    * QPC_SERVER_PASSWORD
+    """
+
+    help = dedent(
+        __doc__.format(DEFAULT_USERNAME=DEFAULT_USERNAME, DEFAULT_EMAIL=DEFAULT_EMAIL)
+    )
+    requires_migrations_checks = True
+
+    def create_parser(self, *args, **kwargs):
+        """Reconfigure the parser to allow line breaks in the help text."""
+        parser = super(Command, self).create_parser(*args, **kwargs)
+        parser.formatter_class = RawTextHelpFormatter
+        return parser
+
+    def handle(self, *args, **options):
+        """Execute the logic for this command."""
+        username = environ.get("QPC_SERVER_USERNAME", DEFAULT_USERNAME)
+        email = environ.get("QPC_SERVER_USER_EMAIL", DEFAULT_EMAIL)
+        password = environ.get("QPC_SERVER_PASSWORD")
+
+        try:
+            created, updated, generated_password = create_or_update_user(
+                username, email, password
+            )
+        except InvalidPasswordError as error:
+            error_message = "Invalid server password specified."
+            for error_detail in error:
+                error_message += f"\n- {error_detail}"
+            raise CommandError(error_message)
+
+        if created or updated:
+            verb = "Created" if created else "Updated"
+            password_description = (
+                f"random password: {generated_password}"
+                if generated_password
+                else "password from QPC_SERVER_PASSWORD"
+            )
+            message = f"{verb} user '{username}' with {password_description}"
+            self.stdout.write(self.style.SUCCESS(message))
+        else:
+            message = f"User '{username}' already exists and was not updated."
+            self.stdout.write(self.style.WARNING(message))
--- quipucords/quipucords/user.py
+++ quipucords/quipucords/user.py
@@ -1,9 +1,69 @@
 """Utilities for User management."""
 
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model, password_validation
+from django.core.exceptions import ValidationError
+from django.db import transaction
+
+User = get_user_model()
 
 
-def create_random_password():
+class InvalidPasswordError(ValidationError):
+    """Custom exception for invalid passwords."""
+
+
+def make_random_password():
     """Create a random password for a User."""
     return User.objects.make_random_password(settings.QPC_MINIMUM_PASSWORD_LENGTH)
+
+
+def validate_password(password: str, user: User | None = None):
+    """Validate the given password."""
+    try:
+        password_validation.validate_password(password, user)
+    except ValidationError as error:
+        raise InvalidPasswordError(error)
+
+
+def update_password(user: User, password: str):
+    """Update the password for a User."""
+    validate_password(password, user)
+    user.set_password(password)
+    user.save()
+
+
+@transaction.atomic
+def create_or_update_user(
+    username: str, email: str, password: str | None
+) -> tuple[bool, bool, str | None]:
+    """
+    Create or update a User, optionally with a random password.
+
+    If a User does not exist, create a new User.
+    If a User does not exist and no password is given, use a random password.
+    If a User already exists and a password is given, update the User.
+    If a User already exits and no password is given, make no changes.
+
+    Note: To preserve legacy behavior, this function does not update the User's
+    email address if an existing user is found with the given username but the
+    email address does not match. At the time of this writing, nothing in the
+    server uses the email address; so, this omission probably doesn't matter.
+
+    Returns a tuple containing:
+    - "created" bool True if a new User was created, False otherwise.
+    - "updated" bool True if an existing User was updated, False otherwise.
+    - "password" str contains the new password only if a new random password was set.
+    """
+    if password:
+        validate_password(password)
+    user, created = User.objects.get_or_create(
+        username=username, defaults={"email": email}
+    )
+    updated = not created and password is not None
+    generate_password = created and password is None
+    if generate_password:
+        password = make_random_password()
+    if created or updated:
+        update_password(user, password)
+
+    return created, updated, password if generate_password else None
