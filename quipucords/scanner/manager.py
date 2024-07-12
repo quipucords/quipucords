quipucords/quipucords

commit 5bae95cd182c5bc2816ffbfccb0ca46b0cc4ed85

feature(Celery): Implement Scan Job cancel
- Use the redis cache for storing the scan job to Celery task id
- Use the AsyncResult revoke method to signal the worker that
  the celery task was revoked
- Update the celery worker to use the celery inspector to check
  if the scan job celery task id was revoked.
- Update celery shared tasks to check for revoke status and
  skip running the tasks.

quipucords/scanner/manager.py
@@ -8,7 +8,6 @@ from time import sleep
 
 from celery.result import AsyncResult
 from django.conf import settings
-from django.core.cache import caches
 from django.db.models import Q
 
 from api.models import ScanJob, ScanTask
@@ -17,14 +16,9 @@ from scanner.job import (
     ProcessBasedScanJobRunner,
     ScanJobRunner,
 )
+from scanner.tasks import get_scan_job_celery_task_id, set_scan_job_celery_task_id
 
 logger = logging.getLogger(__name__)
-redis_cache = caches["redis"]
-
-
-def scan_job_celery_task_id_key(scan_job_id):
-    """Return the key to store the celery task id for a given scan job id."""
-    return f"scan-job-{scan_job_id}-celery-task-id"
 
 
 class CeleryScanManager:
@@ -34,6 +28,7 @@ class CeleryScanManager:
 
     def __init__(self):
         """Log a warning about this scan manager being incomplete."""
+        logger.info("%s: Celery Scan manager instance created.", self.log_prefix)
         logger.warning("%s is not yet fully functional.", __class__.__name__)
 
     def is_alive(self):
@@ -48,18 +43,26 @@ class CeleryScanManager:
         :returns: True if killed, False otherwise.
         """
         scan_job_id = job.id
-        celery_task_id = redis_cache.get(scan_job_celery_task_id_key(scan_job_id))
+        job.status = ScanJob.JOB_TERMINATE_CANCEL
+        job.status_cancel()
+        for scan_task in job.tasks.values():
+            scan_task_id = scan_task["id"]
+            task = ScanTask.objects.get(id=scan_task_id)
+            task.status = ScanTask.CANCELED
+        celery_task_id = get_scan_job_celery_task_id(scan_job_id)
         if celery_task_id is None:
             logger.warning(
                 f"{self.log_prefix}: Could not kill the scan job {scan_job_id},"
                 " no related Celery Task found"
             )
             return False
+
         logger.info(
             f"{self.log_prefix}: Canceling the Celery Task {celery_task_id}"
             f" for scan job {scan_job_id}"
         )
-        AsyncResult(str(celery_task_id)).revoke(terminate=True, signal="SIGKILL")
+        celery_task = AsyncResult(str(celery_task_id))
+        celery_task.revoke()
         return True
 
     def start(self):
@@ -74,11 +77,7 @@ class CeleryScanManager:
             )
         celery_task_id = scan_job_runner.run()
         scan_job_id = scan_job_runner.scan_job.id
-        redis_cache.set(
-            scan_job_celery_task_id_key(scan_job_id),
-            celery_task_id,
-            timeout=settings.QPC_SCAN_JOB_TTL,
-        )
+        set_scan_job_celery_task_id(scan_job_id, str(celery_task_id))
         logger.info(
             f"{self.log_prefix}: Started the Celery Task {celery_task_id}"
             f" for scan job {scan_job_id}"

quipucords/scanner/tasks.py
@@ -3,15 +3,56 @@
 import logging
 
 import celery
+from django.conf import settings
+from django.core.cache import caches
 
 from api.scanjob.model import ScanJob
 from api.scantask.model import ScanTask
 from fingerprinter.runner import FingerprintTaskRunner
+from quipucords.celery import app as celery_app
 from scanner.get_scanner import get_scanner
 from scanner.job import create_report_for_scan_job, run_task_runner
 from scanner.runner import ScanTaskRunner
 
 logger = logging.getLogger(__name__)
+celery_inspect = celery_app.control.inspect()
+redis_cache = caches["redis"]
+
+
+def scan_job_celery_task_id_key(scan_job_id: int):
+    """Return the key to store the celery task id for a given scan job id."""
+    return f"scan-job-{scan_job_id}-celery-task-id"
+
+
+def get_scan_job_celery_task_id(scan_job_id: int):
+    """Return the celery task id for a given scan job id."""
+    return redis_cache.get(scan_job_celery_task_id_key(scan_job_id))
+
+
+def set_scan_job_celery_task_id(
+    scan_job_id: int, celery_task_id: str, key_timeout=None
+):
+    """Set the celery task id for the scan job id."""
+    if key_timeout is None:
+        key_timeout = settings.QPC_SCAN_JOB_TTL
+    key = scan_job_celery_task_id_key(scan_job_id)
+    redis_cache.set(key, celery_task_id, key_timeout)
+
+
+def celery_task_is_revoked(task: celery.Task, celery_task_id: str):
+    """Return a boolean if the celery task is specified has been revoked."""
+    worker_hostname = task.request.hostname
+    revoked_tasks = celery_inspect.revoked()[worker_hostname]
+    is_revoked = celery_task_id in revoked_tasks
+    return is_revoked
+
+
+def scan_job_is_canceled(task: celery.Task, scan_job_id: int):
+    """Return a boolean indicating if the scan job is cancelled."""
+    celery_task_id = get_scan_job_celery_task_id(scan_job_id)
+    if celery_task_id is None:
+        return False
+    return celery_task_is_revoked(task, celery_task_id)
 
 
 def get_task_runner_class(source_type, scan_type) -> type[ScanTaskRunner]:
@@ -47,20 +88,29 @@ def _celery_run_task_runner(
     scan_task = ScanTask.objects.get(id=scan_task_id)
     runner_class = get_task_runner_class(source_type, scan_type)
     runner: ScanTaskRunner = runner_class(scan_task.job, scan_task)
-    success = (task_status := run_task_runner(runner)) == ScanTask.COMPLETED
+    scan_job_id = scan_task.job.id
+
+    if scan_job_is_canceled(task_instance, scan_job_id):
+        logger.info(
+            f"Scan Job {scan_job_id} canceled, skipping scan task {scan_task_id}."
+        )
+        success = False
+        task_status = ScanTask.CANCELED
+    else:
+        success = (task_status := run_task_runner(runner)) == ScanTask.COMPLETED
     if not success:
         # If this task failed, we do not want subsequent tasks in its chain to run.
         task_instance.request.chain = None
     return success, scan_task_id, task_status
 
 
-@celery.shared_task
-def fingerprint(scan_task_id: int) -> tuple[bool, int, str]:
+@celery.shared_task(bind=True)
+def fingerprint(self: celery.Task, scan_task_id: int) -> tuple[bool, int, str]:
     """Wrap _fingerprint to call it as an async Celery task."""
-    return _fingerprint(scan_task_id)
+    return _fingerprint(self, scan_task_id)
 
 
-def _fingerprint(scan_task_id: int) -> tuple[bool, int, str]:
+def _fingerprint(self: celery.Task, scan_task_id: int) -> tuple[bool, int, str]:
     """Create and assign the Report, and update the related ScanJob status.
 
     This task should run once after all inspection tasks have collected and stored
@@ -72,6 +122,11 @@ def _fingerprint(scan_task_id: int) -> tuple[bool, int, str]:
     # It is safe to assume one will always exist at this point.
     scan_task = ScanTask.objects.get(id=scan_task_id)
     scan_job = scan_task.job
+
+    if scan_job_is_canceled(self, scan_job.id):
+        logger.info(f"Scan Job {scan_job.id} canceled, skipping fingerprint")
+        scan_job.status_fail("Scan Job was Canceled")
+        return False, scan_task_id, ScanTask.CANCELED
 
     if not (report := scan_job.report):
         report, error_message = create_report_for_scan_job(scan_job)
@@ -96,18 +151,23 @@ def _fingerprint(scan_task_id: int) -> tuple[bool, int, str]:
     return success, scan_task_id, task_status
 
 
-@celery.shared_task
-def finalize_scan(scan_job_id: int):
+@celery.shared_task(bind=True)
+def finalize_scan(self: celery.Task, scan_job_id: int):
     """Wrap _finalize_scan to call it as an async Celery task."""
-    return _finalize_scan(scan_job_id)
+    return _finalize_scan(self, scan_job_id)
 
 
-def _finalize_scan(scan_job_id: int):
+def _finalize_scan(self: celery.Task, scan_job_id: int):
     """Set ScanJob status and log failures after running all other ScanJob tasks.
 
     This logic follows the basic pattern established in SyncScanJobRunner.run.
     """
     scan_job = ScanJob.objects.get(id=scan_job_id)
+    if scan_job_is_canceled(self, scan_job.id):
+        logger.info(f"Scan Job {scan_job_id} canceled, skipping finalize_scan")
+        scan_job.status_cancel()
+        return
+
     failed_tasks = (
         ScanTask.objects.filter(job_id=scan_job_id)
         .exclude(status=ScanTask.COMPLETED)

commit 5c06c010c323e5df3469609e7bda151eea151408

fix: remove erroneous ScanJob.status assignment
JOB_TERMINATE_CANCEL is not normally a valid/expected value for ScanJob.status.
That pseudo-constant is intended to be used as a multiprocessing signal value
to be sent when cancelling jobs in the old thread-based manager, not to be
stored as a persistent status. When JOB_TERMINATE_CANCEL was set as the status,
the UI would incorrectly show the "working" spinner graphic and still show the
"stop" cancel button, implying that the job was actually still running. The
backend server would also emit this seemingly nonsensical warning:

    Cannot change job state to canceled when it is 2

This bug was introduced in 5bae95cd1 and only affects the Celery-based scan manager.

quipucords/scanner/manager.py
@@ -42,7 +42,6 @@ class CeleryScanManager:
         :returns: True if killed, False otherwise.
         """
         scan_job_id = job.id
-        job.status = ScanJob.JOB_TERMINATE_CANCEL
         job.status_cancel()
         if job.tasks is not None:
             for scan_task in job.tasks.all():