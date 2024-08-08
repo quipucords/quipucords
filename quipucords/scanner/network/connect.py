quipucords/quipucords

commit bff7ee948bfc009a8aaa74667045d4d60bc8c0a3

fix: reduce individual connect timeouts, multiply ansible job timeout by number of hosts
Relates to JIRA: DISCOVERY-723

quipucords/scanner/network/connect.py
@@ -263,8 +263,13 @@ def _connect(  # noqa: PLR0913, PLR0912, PLR0915
             result_store, credential, scan_task.source, manager_interrupt
         )
 
-        # Create parameters for ansible runner
-        runner_settings = {"job_timeout": int(settings.NETWORK_CONNECT_JOB_TIMEOUT)}
+        # Create parameters for ansible runner. For more info, see:
+        # https://ansible.readthedocs.io/projects/runner/en/stable/intro/#env-settings-settings-for-runner-itself
+        job_timeout = int(settings.NETWORK_CONNECT_JOB_TIMEOUT) * len(group_names)
+        runner_settings = {
+            "idle_timeout": job_timeout,  # Ansible default = 600 sec
+            "job_timeout": job_timeout,  # Ansible default = 3600 sec
+        }
         extra_vars_dict = {
             "variable_host": group_name,
             "ansible_ssh_timeout": settings.QPC_SSH_CONNECT_TIMEOUT,

quipucords/quipucords/settings.py
@@ -88,11 +88,12 @@ def app_secret_key_and_path():
     return django_secret_key, django_secret_path
 
 
-QPC_SSH_CONNECT_TIMEOUT = env.int("QPC_SSH_CONNECT_TIMEOUT", 60)
+QPC_SSH_CONNECT_TIMEOUT = env.int("QPC_SSH_CONNECT_TIMEOUT", 10)
 QPC_SSH_INSPECT_TIMEOUT = env.int("QPC_SSH_INSPECT_TIMEOUT", 120)
 
+# Note: NETWORK_*_JOB_TIMEOUT are **per host**. They may not need to be huge.
 NETWORK_INSPECT_JOB_TIMEOUT = env.int("NETWORK_INSPECT_JOB_TIMEOUT", 10800)  # 3 hours
-NETWORK_CONNECT_JOB_TIMEOUT = env.int("NETWORK_CONNECT_JOB_TIMEOUT", 600)  # 10 minutes
+NETWORK_CONNECT_JOB_TIMEOUT = env.int("NETWORK_CONNECT_JOB_TIMEOUT", 10)  # 10 sec
 
 QPC_CONNECT_TASK_TIMEOUT = env.int("QPC_CONNECT_TASK_TIMEOUT", 30)
 QPC_INSPECT_TASK_TIMEOUT = env.int("QPC_INSPECT_TASK_TIMEOUT", 600)

commit 2276a7d54caa5e15d156a282e53fa506061626f6

fix: network connect timeout should multiply by number of ips in the current group, not the number of groups
Caused by bff7ee94

Relates to JIRA: DISCOVERY-723

quipucords/scanner/network/connect.py
@@ -265,7 +265,7 @@ def _connect(  # noqa: PLR0913, PLR0912, PLR0915
 
         # Create parameters for ansible runner. For more info, see:
         # https://ansible.readthedocs.io/projects/runner/en/stable/intro/#env-settings-settings-for-runner-itself
-        job_timeout = int(settings.NETWORK_CONNECT_JOB_TIMEOUT) * len(group_names)
+        job_timeout = int(settings.NETWORK_CONNECT_JOB_TIMEOUT) * len(group_ips)
         runner_settings = {
             "idle_timeout": job_timeout,  # Ansible default = 600 sec
             "job_timeout": job_timeout,  # Ansible default = 3600 sec