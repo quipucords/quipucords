121f530004fa802cdd33f6a19e314150be491914
Bruno FS Ciconelle <bciconel@redhat.com>
2024-08-15 15:44:49 -0300

    chore: deprecate setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
-#!/usr/bin/env python
-"""quipucords setup."""
+"""quipucords mock setup."""
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name="quipucords",
     version="0.0.0",  # just a placeholder version (cachito requires it)
-    packages=find_packages(include=["quipucords"]),
-    include_package_data=True,
-    classifiers=[
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.11",
-    ],
-    license="GPLv3",
-    python_requires=">=3.11",
+)
+
+raise NotImplementedError(
+    "Quipucords SHOULD NOT be installed from setup.py. This file only exists to please "
+    "an archaic downstream build system."
 )
