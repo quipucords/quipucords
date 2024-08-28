936a3ddd17b4ef6941a72bdba99820483324437e
Bruno FS Ciconelle <bciconel@redhat.com>
2024-08-28 15:16:08 -0300

    chore: remove option to run server with https

--- deploy/gunicorn_conf.py
+++ deploy/gunicorn_conf.py
@@ -9,7 +9,6 @@ from utils.debugger import start_debugger_if_required
 env = environ.Env()
 
 QPC_SERVER_PORT = env.int("QPC_SERVER_PORT", 8000)
-QUIPUCORDS_HTTPS_ON = env.bool("QUIPUCORDS_HTTPS_ON", True)
 
 bind = f"0.0.0.0:{QPC_SERVER_PORT}"
 workers = multiprocessing.cpu_count() * 2 + 1
@@ -17,10 +16,5 @@ workers = multiprocessing.cpu_count() * 2 + 1
 errorlog = "-"
 loglevel = "info"
 accesslog = "-"
-
-# SSL configuration
-if QUIPUCORDS_HTTPS_ON:
-    keyfile = "/etc/ssl/qpc/server.key"
-    certfile = "/etc/ssl/qpc/server.crt"
 
 start_debugger_if_required()
