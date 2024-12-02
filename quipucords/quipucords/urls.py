056310997b73a3ec10c5115b94b9494e563c95af
Bruno FS Ciconelle <bciconel@redhat.com>
2024-12-02 16:02:10 -0300

    chore: get rid of the deprecated legacy UI views
    With the new and standalone UI released we can finally get rid of UI
    views, templates, js, css and other static files from that UI.
    
    With this change, Django development server can FINALLY run in true
    standalone mode, without the argument --nostatic.
    
    Please remember to remove these unneeded files from local storage:
    ```
    rm -rf quipucords/client
    rm -rf quipucords/quipucords/templates
    rm -rf quipucords/staticfiles
    ```
    
    Relates to JIRA: DISCOVERY-777

--- quipucords/quipucords/urls.py
+++ quipucords/quipucords/urls.py
@@ -17,38 +17,9 @@ Including another URLconf
 
 from django.conf.urls import include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from django.urls import path, re_path
-from django.views.generic import RedirectView
-from django.views.generic.base import TemplateView
+from django.urls import path
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("", RedirectView.as_view(url="/login", permanent=False), name="home"),
-    # ui routing
-    re_path(
-        r"^(client/(sources|scans|credentials|)(/|)(index.html|))$",
-        TemplateView.as_view(template_name="client/index.html"),
-        name="client",
-    ),
-    # docs files
-    re_path(
-        r"^client/docs(/|)(index.html|use.html|)$",
-        RedirectView.as_view(url="/client/docs/use.html", permanent=False),
-        name="docs",
-    ),
-    re_path(
-        r"^client/docs(/|)(install.html|)$",
-        RedirectView.as_view(url="/client/docs/install.html", permanent=False),
-        name="docs",
-    ),
-    # static files (*.css, *.js, *.jpg etc.)
-    re_path(
-        r"^(?!/?client/)(?P<path>.*\..*)$",
-        RedirectView.as_view(url="/client/%(path)s", permanent=False),
-        name="client",
-    ),
 ]
