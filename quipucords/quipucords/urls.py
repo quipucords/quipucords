0b5dcff3af03598fb54e28b2295dcf67635bf3b2
Bruno FS Ciconelle <bciconel@redhat.com>
2024-12-02 17:49:12 -0300

    feat: add autogenerated openapi schema
    Relates to JIRA: DISCOVERY-726

--- quipucords/quipucords/urls.py
+++ quipucords/quipucords/urls.py
@@ -18,8 +18,20 @@ Including another URLconf
 from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "",
+        RedirectView.as_view(url="/api/schema/swagger-ui/?version=v2", permanent=False),
+    ),
 ]
