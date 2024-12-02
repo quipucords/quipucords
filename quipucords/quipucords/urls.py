31dba140829ca4a94e0b0ba7cb60348682dcc4f4
Bruno FS Ciconelle <bciconel@redhat.com>
2024-12-02 17:51:03 -0300

    chore: remove admin urls

--- quipucords/quipucords/urls.py
+++ quipucords/quipucords/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 
 from django.conf.urls import include
-from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
