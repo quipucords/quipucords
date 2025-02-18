b4999fde72c35b1d0712b529d2f6b92ac0ff12bf
Brad Smith <brasmith@redhat.com>
2025-02-18 11:31:43 -0500

    refactor: remove connect jobs and tasks from satellite scanner
    This commit introduces many TODO comments because it is one step in a
    much larger refactor operation to completely remove the connect-type
    scan jobs and scan tasks. Some code cannot be removed until we complete
    the work for scanners of all types.
    
    Relates to JIRA: DISCOVERY-784

--- quipucords/scanner/satellite/api.py
+++ quipucords/scanner/satellite/api.py
@@ -13,7 +13,6 @@ from api.models import (
     InspectResult,
     RawFact,
     Scan,
-    ScanTask,
     SystemConnectionResult,
 )
 from api.status.misc import get_server_id
@@ -44,12 +43,7 @@ class SatelliteInterface(ABC):
         else:
             self.max_concurrency = scan_job.options.get(Scan.MAX_CONCURRENCY)
 
-        if scan_task.scan_type == ScanTask.SCAN_TYPE_CONNECT:
-            self.connect_scan_task = scan_task
-            self.inspect_scan_task = None
-        else:
-            self.connect_scan_task = scan_task.prerequisites.first()
-            self.inspect_scan_task = scan_task
+        self.inspect_scan_task = scan_task
         self.source = scan_task.source
 
     @transaction.atomic
@@ -64,11 +58,11 @@ class SatelliteInterface(ABC):
             source=self.source,
             credential=credential,
             status=SystemConnectionResult.SUCCESS,
-            task_connection_result=self.connect_scan_task.connection_result,
+            task_connection_result=self.inspect_scan_task.connection_result,
         )
         sys_result.save()
 
-        self.connect_scan_task.increment_stats(name, increment_sys_scanned=True)
+        self.inspect_scan_task.increment_stats(name, increment_sys_scanned=True)
 
     @cached_property
     def _inspect_group(self):
