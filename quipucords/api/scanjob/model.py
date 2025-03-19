quipucords/quipucords

commit b4999fde72c35b1d0712b529d2f6b92ac0ff12bf

refactor: remove connect jobs and tasks from satellite scanner
This commit introduces many TODO comments because it is one step in a
much larger refactor operation to completely remove the connect-type
scan jobs and scan tasks. Some code cannot be removed until we complete
the work for scanners of all types.

Relates to JIRA: DISCOVERY-784

quipucords/api/scanjob/model.py
@@ -20,6 +20,7 @@ from api.scan.model import Scan
 from api.scanjob.queryset import ScanJobQuerySet
 from api.scantask.model import ScanTask
 from api.source.model import Source
+from constants import DataSources
 
 logger = logging.getLogger(__name__)
 
@@ -92,6 +93,20 @@ class ScanJob(BaseModel):
             self.scan.options = value
             self.scan.save()
 
+    @property
+    def no_connect_tasks(self):
+        """
+        Determine if we should use modernized code for no connect-type jobs/tasks.
+
+        This is a temporary crutch until we have refactored all source types to remove
+        their use of connect-type jobs and tasks.
+
+        TODO Remove this function once we stop using connect-type jobs/tasks.
+        """
+        if self.tasks.filter(scan_type=ScanTask.SCAN_TYPE_CONNECT).exists():
+            return False
+        return self.sources.filter(source_type__in=[DataSources.SATELLITE]).exists()
+
     def get_extra_vars(self):
         """Return the extra vars from the related Scan."""
         if self.scan:
@@ -154,6 +169,7 @@ class ScanJob(BaseModel):
             connection_systems_unreachable,
         ) = self._calculate_counts(ScanTask.SCAN_TYPE_CONNECT)
         if self.scan_type == ScanTask.SCAN_TYPE_CONNECT or connect_only:
+            # TODO Remove SCAN_TYPE_CONNECT logic.
             systems_count = connection_systems_count
             systems_scanned = connection_systems_scanned
             systems_failed = connection_systems_failed
@@ -281,7 +297,10 @@ class ScanJob(BaseModel):
         self.log_current_status()
 
     def _create_pending_tasks(self):
-        conn_tasks = self._create_connection_tasks()
+        # TODO Remove conn_tasks.
+        conn_tasks = (
+            self._create_connection_tasks() if not self.no_connect_tasks else []
+        )
         inspect_tasks = self._create_inspection_tasks(conn_tasks)
         self._create_fingerprint_task(conn_tasks, inspect_tasks)
 
@@ -304,6 +323,9 @@ class ScanJob(BaseModel):
 
         :return: list of connection_tasks
         """
+        # TODO Remove this function when we stop using connection tasks.
+        if self.no_connect_tasks:
+            raise Exception("This should never happen.")
         conn_tasks = []
         if self.scan_type in [ScanTask.SCAN_TYPE_CONNECT, ScanTask.SCAN_TYPE_INSPECT]:
             count = 1
@@ -340,7 +362,31 @@ class ScanJob(BaseModel):
         :return: list of inspection_tasks
         """
         inspect_tasks = []
-        if conn_tasks and self.scan_type == ScanTask.SCAN_TYPE_INSPECT:
+        if (
+            not conn_tasks
+            and self.no_connect_tasks
+            and self.scan_type == ScanTask.SCAN_TYPE_INSPECT
+        ):
+            # Eventually all logic in this function routes here.
+            # The complex condition is temporary while we migrate
+            # all the different source types.
+            for index, source in enumerate(self.sources.all(), start=1):
+                inspect_task = ScanTask.objects.create(
+                    job=self,
+                    source=source,
+                    scan_type=ScanTask.SCAN_TYPE_INSPECT,
+                    status=ScanTask.PENDING,
+                    status_message=_(messages.ST_STATUS_MSG_PENDING),
+                    sequence_number=index,
+                    connection_result=TaskConnectionResult.objects.create(
+                        job_connection_result=self.connection_results
+                    ),
+                )
+
+                self.tasks.add(inspect_task)
+                inspect_tasks.append(inspect_task)
+        elif conn_tasks and self.scan_type == ScanTask.SCAN_TYPE_INSPECT:
+            # TODO Remove this.
             count = len(conn_tasks) + 1
             for conn_task in conn_tasks:
                 # Create inspect tasks

quipucords/scanner/job.py
@@ -63,12 +63,14 @@ def create_report_for_scan_job(scan_job: ScanJob):
     :returns: tuple[Report,str] with the created Report (if successful)
         and error string (if not successful)
     """
-    conn_query = ScanTask.objects.filter(
-        job=scan_job, scan_type=ScanTask.SCAN_TYPE_CONNECT
-    ).aggregate(successful_connections=Sum("systems_scanned"))
+    if not scan_job.no_connect_tasks:
+        # TODO Remove this when we have removed all connect scan jobs/tasks.
+        conn_query = ScanTask.objects.filter(
+            job=scan_job, scan_type=ScanTask.SCAN_TYPE_CONNECT
+        ).aggregate(successful_connections=Sum("systems_scanned"))
 
-    if not conn_query["successful_connections"]:
-        return None, "No connection results found."
+        if not conn_query["successful_connections"]:
+            return None, "No connection results found."
 
     inspect_query = ScanTask.objects.filter(
         job=scan_job, scan_type=ScanTask.SCAN_TYPE_INSPECT

quipucords/scanner/satellite/api.py
@@ -13,7 +13,6 @@ from api.models import (
     InspectResult,
     RawFact,
     Scan,
-    ScanTask,
     SystemConnectionResult,
 )
 from api.status.misc import get_server_id
@@ -44,12 +43,7 @@ class SatelliteInterface(ABC):
         else:
             self.max_concurrency = scan_job.options.get(Scan.MAX_CONCURRENCY)
 
-        if scan_task.scan_type == ScanTask.SCAN_TYPE_CONNECT:
-            self.connect_scan_task = scan_task
-            self.inspect_scan_task = None
-        else:
-            self.connect_scan_task = scan_task.prerequisites.first()
-            self.inspect_scan_task = scan_task
+        self.inspect_scan_task = scan_task
         self.source = scan_task.source
 
     @transaction.atomic
@@ -64,11 +58,11 @@ class SatelliteInterface(ABC):
             source=self.source,
             credential=credential,
             status=SystemConnectionResult.SUCCESS,
-            task_connection_result=self.connect_scan_task.connection_result,
+            task_connection_result=self.inspect_scan_task.connection_result,
         )
         sys_result.save()
 
-        self.connect_scan_task.increment_stats(name, increment_sys_scanned=True)
+        self.inspect_scan_task.increment_stats(name, increment_sys_scanned=True)
 
     @cached_property
     def _inspect_group(self):

quipucords/scanner/satellite/six.py
@@ -466,7 +466,7 @@ class SatelliteSix(SatelliteInterface, metaclass=ABCMeta):
     def _request_and_record_hosts(self, credential, org_id=None):
         """Request and record hosts for the given credential and optional org filter."""
         hosts = []
-        for result in request_results(self.connect_scan_task, self.HOSTS_URL, org_id):
+        for result in request_results(self.inspect_scan_task, self.HOSTS_URL, org_id):
             host_name = result.get(NAME)
             host_id = result.get(ID)
 
@@ -482,9 +482,10 @@ class SatelliteSix(SatelliteInterface, metaclass=ABCMeta):
 
     def hosts_facts(self):
         """Obtain the managed hosts detail raw facts."""
-        systems_count = len(self.connect_scan_task.connection_result.systems.all())
+        systems_count = len(self.inspect_scan_task.connection_result.systems.all())
         if self.inspect_scan_task is None:
             raise SatelliteError("hosts_facts cannot be called for a connection scan")
+        self.inspect_scan_task.reset_stats()
         self.inspect_scan_task.update_stats(
             "INITIAL SATELLITE STATS", sys_count=systems_count
         )
@@ -528,7 +529,7 @@ class SatelliteSixV1(SatelliteSix):
 
         self.orgs = [
             result.get(ID)
-            for result in request_results(self.connect_scan_task, ORGS_V1_URL)
+            for result in request_results(self.inspect_scan_task, ORGS_V1_URL)
             if result.get(ID) is not None
         ]
         return self.orgs
@@ -540,7 +541,7 @@ class SatelliteSixV1(SatelliteSix):
         for org_id in orgs:
             params = {PAGE: 1, PER_PAGE: 100, THIN: 1}
             response, url = utils.execute_request(
-                self.connect_scan_task,
+                self.inspect_scan_task,
                 url=HOSTS_V1_URL,
                 org_id=org_id,
                 query_params=params,
@@ -550,14 +551,14 @@ class SatelliteSixV1(SatelliteSix):
                     f"Invalid response code {response.status_code} for url: {url}"
                 )
             systems_count += response.json().get("total", 0)
-            self.connect_scan_task.update_stats(
+            self.inspect_scan_task.update_stats(
                 "INITIAL SATELLITE STATS", sys_count=systems_count
             )
             return systems_count
 
     def hosts(self):
         """Obtain the managed hosts."""
-        credential = utils.get_credential(self.connect_scan_task)
+        credential = utils.get_credential(self.inspect_scan_task)
 
         hosts = list(
             itertools.chain.from_iterable(
@@ -590,21 +591,21 @@ class SatelliteSixV2(SatelliteSix):
         """Obtain the count of managed hosts."""
         params = {PAGE: 1, PER_PAGE: 10, THIN: 1}
         response, url = utils.execute_request(
-            self.connect_scan_task, url=HOSTS_V2_URL, query_params=params
+            self.inspect_scan_task, url=HOSTS_V2_URL, query_params=params
         )
         if response.status_code != requests.codes.ok:
             raise SatelliteError(
                 f"Invalid response code {response.status_code} for url: {url}"
             )
         systems_count = response.json().get("total", 0)
-        self.connect_scan_task.update_stats(
+        self.inspect_scan_task.update_stats(
             "INITIAL SATELLITE STATS", sys_count=systems_count
         )
         return systems_count
 
     def hosts(self):
         """Obtain the managed hosts."""
-        credential = utils.get_credential(self.connect_scan_task)
+        credential = utils.get_credential(self.inspect_scan_task)
         return self._request_and_record_hosts(credential)
 
     def _requests_hosts_unique(self):

quipucords/tests/scanner/satellite/test_sat_six.py
@@ -63,10 +63,10 @@ class TestSatelliteSixV1:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        self.api.connect_scan_task.connection_result = connection_results
-        self.api.connect_scan_task.connection_result.save()
+        self.api.inspect_scan_task.connection_result = connection_results
+        self.api.inspect_scan_task.connection_result.save()
 
-        conn_result = self.api.connect_scan_task.connection_result
+        conn_result = self.api.inspect_scan_task.connection_result
         sys_result = SystemConnectionResult(
             name="sys1",
             status=InspectResult.SUCCESS,
@@ -85,7 +85,7 @@ class TestSatelliteSixV1:
             task_connection_result=conn_result,
         )
         sys_result.save()
-        self.api.connect_scan_task.save()
+        self.api.inspect_scan_task.save()
 
     @pytest.mark.django_db
     def test_get_orgs(self):
@@ -584,17 +584,17 @@ class TestSatelliteSixV2:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        self.api.connect_scan_task.connection_result = connection_results
-        self.api.connect_scan_task.connection_result.save()
+        self.api.inspect_scan_task.connection_result = connection_results
+        self.api.inspect_scan_task.connection_result.save()
 
-        conn_result = self.api.connect_scan_task.connection_result
+        conn_result = self.api.inspect_scan_task.connection_result
         sys_result = SystemConnectionResult(
             name="sys1_1",
             status=InspectResult.SUCCESS,
             task_connection_result=conn_result,
         )
         sys_result.save()
-        self.api.connect_scan_task.save()
+        self.api.inspect_scan_task.save()
 
     @pytest.mark.django_db
     def test_host_count(self):
@@ -1069,16 +1069,16 @@ class TestSatelliteSixV2:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        api.connect_scan_task.connection_result = connection_results
-        api.connect_scan_task.connection_result.save()
+        api.inspect_scan_task.connection_result = connection_results
+        api.inspect_scan_task.connection_result.save()
 
         sys_result = SystemConnectionResult(
             name="sys1_1",
             status=InspectResult.SUCCESS,
-            task_connection_result=api.connect_scan_task.connection_result,
+            task_connection_result=api.inspect_scan_task.connection_result,
         )
         sys_result.save()
-        api.connect_scan_task.save()
+        api.inspect_scan_task.save()
         hosts_url = "https://{sat_host}:{port}/api/v2/hosts"
 
         _request_host_details = [

quipucords/api/scan/model.py
@@ -28,7 +28,7 @@ class Scan(BaseModel):
     """Configuration for the scan jobs that will run."""
 
     SCAN_TYPE_CHOICES = (
-        (ScanTask.SCAN_TYPE_CONNECT, ScanTask.SCAN_TYPE_CONNECT),
+        (ScanTask.SCAN_TYPE_CONNECT, ScanTask.SCAN_TYPE_CONNECT),  # TODO Remove this.
         (ScanTask.SCAN_TYPE_INSPECT, ScanTask.SCAN_TYPE_INSPECT),
     )
 

quipucords/api/scantask/model.py
@@ -23,11 +23,11 @@ logger = logging.getLogger(__name__)
 class ScanTask(BaseModel):
     """The scan task captures a single source for a scan."""
 
-    SCAN_TYPE_CONNECT = "connect"
+    SCAN_TYPE_CONNECT = "connect"  # TODO Remove SCAN_TYPE_CONNECT.
     SCAN_TYPE_INSPECT = "inspect"
     SCAN_TYPE_FINGERPRINT = "fingerprint"
     SCANTASK_TYPE_CHOICES = (
-        (SCAN_TYPE_CONNECT, SCAN_TYPE_CONNECT),
+        (SCAN_TYPE_CONNECT, SCAN_TYPE_CONNECT),  # TODO Remove SCAN_TYPE_CONNECT choice.
         (SCAN_TYPE_INSPECT, SCAN_TYPE_INSPECT),
         (SCAN_TYPE_FINGERPRINT, SCAN_TYPE_FINGERPRINT),
     )
@@ -455,6 +455,7 @@ class ScanTask(BaseModel):
                 inspect_group__in=self.inspect_groups.all()
             ).all()
         elif self.scan_type == ScanTask.SCAN_TYPE_CONNECT:
+            # TODO Remove SCAN_TYPE_CONNECT logic.
             return self.connection_result
         elif self.scan_type == ScanTask.SCAN_TYPE_FINGERPRINT:
             return self.job.report

quipucords/scanner/satellite/connect.py
@@ -1,16 +1,29 @@
 """ScanTask used for satellite connection task."""
+# TODO Remove this module.
 
+import warnings
+
+from api.scantask.model import ScanTask
 from scanner.satellite.runner import SatelliteTaskRunner
 
 
 class ConnectTaskRunner(SatelliteTaskRunner):
-    """ConnectTaskRunner satellite connection capabilities.
-
-    Attempts connections to a source using a credential
-    and gathers the set of available systems.
     """
+    ConnectTaskRunner legacy interface does nothing for Satellite sources.
+
+    TODO Remove this class when all source types stop using connect tasks.
+    """
+
+    def execute_task(self):
+        """Scan Satellite for system connection data."""
+        warnings.warn(
+            "ConnectTaskRunner.execute_task does nothing.", DeprecationWarning
+        )
+
+        return None, ScanTask.COMPLETED
 
     def handle_api_calls(self, api):
         """Handle api calls for connetion phase."""
-        api.host_count()
-        api.hosts()
+        warnings.warn(
+            "ConnectTaskRunner.handle_api_calls does nothing.", DeprecationWarning
+        )

commit 6a28356124ca2e54fd5af030b0c6daa4c4b7bbb2

fix: restore connect task for satellite to fix broken multi-type scans
Before this change, standalone scans for each type (Satellite, network, etc.)
all worked fine in isolation. However, if you created a scan with multiple
types of sources, and one of those types included Satellite which recently
stopped using connect scan tasks, the overall scan would fail because the
task models had an inconsistent API across different source types. That problem
was introduced in b4999fde72c35b1d0712b529d2f6b92ac0ff12bf via
<https://github.com/quipucords/quipucords/pull/2845>.

This change restores the use of connect scan tasks for Satellite to preserve
the old internal interface as still used by other source types. The restored
use of the connect scan task is only superficial, though, and all "connect"
logic for Satellite has still been moved out of its old connect task runner
and into its inspect task runner. After we have performed similar refactoring
work to the other source types, we should be able to fully remove the connect
type scan task from *all* of the different source type task runners.

Relates to JIRA: DISCOVERY-784

quipucords/api/scanjob/model.py
@@ -20,7 +20,6 @@ from api.scan.model import Scan
 from api.scanjob.queryset import ScanJobQuerySet
 from api.scantask.model import ScanTask
 from api.source.model import Source
-from constants import DataSources
 
 logger = logging.getLogger(__name__)
 
@@ -92,20 +91,6 @@ class ScanJob(BaseModel):
         if self.scan:
             self.scan.options = value
             self.scan.save()
-
-    @property
-    def no_connect_tasks(self):
-        """
-        Determine if we should use modernized code for no connect-type jobs/tasks.
-
-        This is a temporary crutch until we have refactored all source types to remove
-        their use of connect-type jobs and tasks.
-
-        TODO Remove this function once we stop using connect-type jobs/tasks.
-        """
-        if self.tasks.filter(scan_type=ScanTask.SCAN_TYPE_CONNECT).exists():
-            return False
-        return self.sources.filter(source_type__in=[DataSources.SATELLITE]).exists()
 
     def get_extra_vars(self):
         """Return the extra vars from the related Scan."""
@@ -298,9 +283,7 @@ class ScanJob(BaseModel):
 
     def _create_pending_tasks(self):
         # TODO Remove conn_tasks.
-        conn_tasks = (
-            self._create_connection_tasks() if not self.no_connect_tasks else []
-        )
+        conn_tasks = self._create_connection_tasks()
         inspect_tasks = self._create_inspection_tasks(conn_tasks)
         self._create_fingerprint_task(conn_tasks, inspect_tasks)
 
@@ -324,8 +307,6 @@ class ScanJob(BaseModel):
         :return: list of connection_tasks
         """
         # TODO Remove this function when we stop using connection tasks.
-        if self.no_connect_tasks:
-            raise Exception("This should never happen.")
         conn_tasks = []
         if self.scan_type in [ScanTask.SCAN_TYPE_CONNECT, ScanTask.SCAN_TYPE_INSPECT]:
             count = 1
@@ -362,31 +343,7 @@ class ScanJob(BaseModel):
         :return: list of inspection_tasks
         """
         inspect_tasks = []
-        if (
-            not conn_tasks
-            and self.no_connect_tasks
-            and self.scan_type == ScanTask.SCAN_TYPE_INSPECT
-        ):
-            # Eventually all logic in this function routes here.
-            # The complex condition is temporary while we migrate
-            # all the different source types.
-            for index, source in enumerate(self.sources.all(), start=1):
-                inspect_task = ScanTask.objects.create(
-                    job=self,
-                    source=source,
-                    scan_type=ScanTask.SCAN_TYPE_INSPECT,
-                    status=ScanTask.PENDING,
-                    status_message=_(messages.ST_STATUS_MSG_PENDING),
-                    sequence_number=index,
-                    connection_result=TaskConnectionResult.objects.create(
-                        job_connection_result=self.connection_results
-                    ),
-                )
-
-                self.tasks.add(inspect_task)
-                inspect_tasks.append(inspect_task)
-        elif conn_tasks and self.scan_type == ScanTask.SCAN_TYPE_INSPECT:
-            # TODO Remove this.
+        if conn_tasks and self.scan_type == ScanTask.SCAN_TYPE_INSPECT:
             count = len(conn_tasks) + 1
             for conn_task in conn_tasks:
                 # Create inspect tasks

quipucords/scanner/job.py
@@ -63,15 +63,6 @@ def create_report_for_scan_job(scan_job: ScanJob):
     :returns: tuple[Report,str] with the created Report (if successful)
         and error string (if not successful)
     """
-    if not scan_job.no_connect_tasks:
-        # TODO Remove this when we have removed all connect scan jobs/tasks.
-        conn_query = ScanTask.objects.filter(
-            job=scan_job, scan_type=ScanTask.SCAN_TYPE_CONNECT
-        ).aggregate(successful_connections=Sum("systems_scanned"))
-
-        if not conn_query["successful_connections"]:
-            return None, "No connection results found."
-
     inspect_query = ScanTask.objects.filter(
         job=scan_job, scan_type=ScanTask.SCAN_TYPE_INSPECT
     ).aggregate(successful_connections=Sum("systems_scanned"))

quipucords/scanner/satellite/api.py
@@ -43,6 +43,9 @@ class SatelliteInterface(ABC):
         else:
             self.max_concurrency = scan_job.options.get(Scan.MAX_CONCURRENCY)
 
+        # Next line assumes scan_task has type 'inspect'.
+        # TODO Delete next line when we stop using connect scan tasks.
+        self.connect_scan_task = scan_task.prerequisites.first()
         self.inspect_scan_task = scan_task
         self.source = scan_task.source
 
@@ -58,11 +61,11 @@ class SatelliteInterface(ABC):
             source=self.source,
             credential=credential,
             status=SystemConnectionResult.SUCCESS,
-            task_connection_result=self.inspect_scan_task.connection_result,
+            task_connection_result=self.connect_scan_task.connection_result,
         )
         sys_result.save()
 
-        self.inspect_scan_task.increment_stats(name, increment_sys_scanned=True)
+        self.connect_scan_task.increment_stats(name, increment_sys_scanned=True)
 
     @cached_property
     def _inspect_group(self):

quipucords/scanner/satellite/six.py
@@ -466,7 +466,7 @@ class SatelliteSix(SatelliteInterface, metaclass=ABCMeta):
     def _request_and_record_hosts(self, credential, org_id=None):
         """Request and record hosts for the given credential and optional org filter."""
         hosts = []
-        for result in request_results(self.inspect_scan_task, self.HOSTS_URL, org_id):
+        for result in request_results(self.connect_scan_task, self.HOSTS_URL, org_id):
             host_name = result.get(NAME)
             host_id = result.get(ID)
 
@@ -482,7 +482,7 @@ class SatelliteSix(SatelliteInterface, metaclass=ABCMeta):
 
     def hosts_facts(self):
         """Obtain the managed hosts detail raw facts."""
-        systems_count = len(self.inspect_scan_task.connection_result.systems.all())
+        systems_count = len(self.connect_scan_task.connection_result.systems.all())
         if self.inspect_scan_task is None:
             raise SatelliteError("hosts_facts cannot be called for a connection scan")
         self.inspect_scan_task.reset_stats()
@@ -529,7 +529,7 @@ class SatelliteSixV1(SatelliteSix):
 
         self.orgs = [
             result.get(ID)
-            for result in request_results(self.inspect_scan_task, ORGS_V1_URL)
+            for result in request_results(self.connect_scan_task, ORGS_V1_URL)
             if result.get(ID) is not None
         ]
         return self.orgs
@@ -551,14 +551,14 @@ class SatelliteSixV1(SatelliteSix):
                     f"Invalid response code {response.status_code} for url: {url}"
                 )
             systems_count += response.json().get("total", 0)
-            self.inspect_scan_task.update_stats(
+            self.connect_scan_task.update_stats(
                 "INITIAL SATELLITE STATS", sys_count=systems_count
             )
             return systems_count
 
     def hosts(self):
         """Obtain the managed hosts."""
-        credential = utils.get_credential(self.inspect_scan_task)
+        credential = utils.get_credential(self.connect_scan_task)
 
         hosts = list(
             itertools.chain.from_iterable(
@@ -591,21 +591,21 @@ class SatelliteSixV2(SatelliteSix):
         """Obtain the count of managed hosts."""
         params = {PAGE: 1, PER_PAGE: 10, THIN: 1}
         response, url = utils.execute_request(
-            self.inspect_scan_task, url=HOSTS_V2_URL, query_params=params
+            self.connect_scan_task, url=HOSTS_V2_URL, query_params=params
         )
         if response.status_code != requests.codes.ok:
             raise SatelliteError(
                 f"Invalid response code {response.status_code} for url: {url}"
             )
         systems_count = response.json().get("total", 0)
-        self.inspect_scan_task.update_stats(
+        self.connect_scan_task.update_stats(
             "INITIAL SATELLITE STATS", sys_count=systems_count
         )
         return systems_count
 
     def hosts(self):
         """Obtain the managed hosts."""
-        credential = utils.get_credential(self.inspect_scan_task)
+        credential = utils.get_credential(self.connect_scan_task)
         return self._request_and_record_hosts(credential)
 
     def _requests_hosts_unique(self):

quipucords/tests/scanner/satellite/test_sat_six.py
@@ -63,10 +63,10 @@ class TestSatelliteSixV1:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        self.api.inspect_scan_task.connection_result = connection_results
-        self.api.inspect_scan_task.connection_result.save()
+        self.api.connect_scan_task.connection_result = connection_results
+        self.api.connect_scan_task.connection_result.save()
 
-        conn_result = self.api.inspect_scan_task.connection_result
+        conn_result = self.api.connect_scan_task.connection_result
         sys_result = SystemConnectionResult(
             name="sys1",
             status=InspectResult.SUCCESS,
@@ -85,7 +85,7 @@ class TestSatelliteSixV1:
             task_connection_result=conn_result,
         )
         sys_result.save()
-        self.api.inspect_scan_task.save()
+        self.api.connect_scan_task.save()
 
     @pytest.mark.django_db
     def test_get_orgs(self):
@@ -584,17 +584,17 @@ class TestSatelliteSixV2:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        self.api.inspect_scan_task.connection_result = connection_results
-        self.api.inspect_scan_task.connection_result.save()
+        self.api.connect_scan_task.connection_result = connection_results
+        self.api.connect_scan_task.connection_result.save()
 
-        conn_result = self.api.inspect_scan_task.connection_result
+        conn_result = self.api.connect_scan_task.connection_result
         sys_result = SystemConnectionResult(
             name="sys1_1",
             status=InspectResult.SUCCESS,
             task_connection_result=conn_result,
         )
         sys_result.save()
-        self.api.inspect_scan_task.save()
+        self.api.connect_scan_task.save()
 
     @pytest.mark.django_db
     def test_host_count(self):
@@ -1069,16 +1069,16 @@ class TestSatelliteSixV2:
         job_conn_result.save()
         connection_results = TaskConnectionResult(job_connection_result=job_conn_result)
         connection_results.save()
-        api.inspect_scan_task.connection_result = connection_results
-        api.inspect_scan_task.connection_result.save()
+        api.connect_scan_task.connection_result = connection_results
+        api.connect_scan_task.connection_result.save()
 
         sys_result = SystemConnectionResult(
             name="sys1_1",
             status=InspectResult.SUCCESS,
-            task_connection_result=api.inspect_scan_task.connection_result,
+            task_connection_result=api.connect_scan_task.connection_result,
         )
         sys_result.save()
-        api.inspect_scan_task.save()
+        api.connect_scan_task.save()
         hosts_url = "https://{sat_host}:{port}/api/v2/hosts"
 
         _request_host_details = [