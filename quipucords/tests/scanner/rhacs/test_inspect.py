bfa7c445390f575c5a98f61de6be3677fbbced98
Brad Smith <brasmith@redhat.com>
2025-03-24 16:55:00 -0400

    refactor: move RHACS connect task runner logic to inspect task runner
    Relates to JIRA: DISCOVERY-784

--- quipucords/tests/scanner/rhacs/test_inspect.py
+++ quipucords/tests/scanner/rhacs/test_inspect.py
@@ -9,7 +9,7 @@ from urllib3.exceptions import MaxRetryError
 
 from api.models import ScanTask
 from constants import DataSources
-from scanner.rhacs.connect import ConnectTaskRunner
+from scanner.rhacs.inspect import InspectTaskRunner
 from scanner.rhacs.runner import RHACSTaskRunner
 from tests.factories import ScanTaskFactory
 
@@ -17,12 +17,21 @@ from tests.factories import ScanTaskFactory
 @pytest.fixture
 def scan_task():
     """Return a ScanTask for testing."""
-    return ScanTaskFactory(
+    connect_task = ScanTaskFactory(
         source__source_type=DataSources.RHACS,
         source__hosts=["1.2.3.4"],
         source__port=4321,
         scan_type=ScanTask.SCAN_TYPE_CONNECT,
+        status=ScanTask.COMPLETED,
+        sequence_number=1,
     )
+    inspect_task = ScanTaskFactory(
+        source=connect_task.source,
+        job=connect_task.job,
+        scan_type=ScanTask.SCAN_TYPE_INSPECT,
+    )
+    inspect_task.prerequisites.add(connect_task)
+    return inspect_task
 
 
 @pytest.fixture
@@ -32,17 +41,23 @@ def mock_client(mocker):
 
 
 @pytest.mark.django_db
-def test_connect_successfully(mock_client, scan_task: ScanTask):
+def test_connect_successfully(mocker, mock_client, scan_task: ScanTask):
     """Test connecting to RHACS host with success."""
-    runner = ConnectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
+    runner = InspectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
     mock_client.return_value.get.return_value.status_code = 200
+    mocker.patch.object(
+        runner, "inspect", return_value=[runner.success_message, ScanTask.COMPLETED]
+    )
     message, status = runner.execute_task()
     assert message == runner.success_message
     assert status == ScanTask.COMPLETED
-    assert scan_task.systems_count == 1
-    assert scan_task.systems_scanned == 1
-    assert scan_task.systems_failed == 0
-    assert scan_task.systems_unreachable == 0
+    # Next line assumes self.scan_task has type 'inspect'.
+    # TODO Delete connect_scan_task when we stop using connect scan tasks.
+    connect_scan_task = scan_task.prerequisites.first()
+    assert connect_scan_task.systems_count == 1
+    assert connect_scan_task.systems_scanned == 1
+    assert connect_scan_task.systems_failed == 0
+    assert connect_scan_task.systems_unreachable == 0
 
 
 @pytest.mark.django_db
@@ -58,15 +73,18 @@ def test_connect_with_error(
 ):
     """Test connecting to RHACS host with failure."""
     caplog.set_level(logging.ERROR)
-    runner = ConnectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
+    runner = InspectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
     mock_client.return_value.get.return_value.status_code = err_status
     message, status = runner.execute_task()
     assert message == runner.failure_message
     assert status == ScanTask.FAILED
-    assert scan_task.systems_count == 1
-    assert scan_task.systems_scanned == 0
-    assert scan_task.systems_failed == 1
-    assert scan_task.systems_unreachable == 0
+    # Next line assumes self.scan_task has type 'inspect'.
+    # TODO Delete connect_scan_task when we stop using connect scan tasks.
+    connect_scan_task = scan_task.prerequisites.first()
+    assert connect_scan_task.systems_count == 1
+    assert connect_scan_task.systems_scanned == 0
+    assert connect_scan_task.systems_failed == 1
+    assert connect_scan_task.systems_unreachable == 0
     assert err_message in caplog.text
 
 
@@ -111,13 +129,16 @@ def test_handling_connection_errors(  # noqa: PLR0913
 ):
     """Test handling RHACS connection exceptions."""
     caplog.set_level(logging.ERROR)
-    runner = ConnectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
+    runner = InspectTaskRunner(scan_task=scan_task, scan_job=scan_task.job)
     mock_client.return_value.get.side_effect = exception
     message, status = runner.execute_task()
     assert message == runner.failure_message
     assert status == ScanTask.FAILED
-    assert scan_task.systems_count == 1
-    assert scan_task.systems_scanned == 0
-    assert scan_task.systems_failed == failed
-    assert scan_task.systems_unreachable == unreachable
+    # Next line assumes self.scan_task has type 'inspect'.
+    # TODO Delete connect_scan_task when we stop using connect scan tasks.
+    connect_scan_task = scan_task.prerequisites.first()
+    assert connect_scan_task.systems_count == 1
+    assert connect_scan_task.systems_scanned == 0
+    assert connect_scan_task.systems_failed == failed
+    assert connect_scan_task.systems_unreachable == unreachable
     assert err_message in caplog.text
