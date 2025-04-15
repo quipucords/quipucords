18e0246d61bad9fb7c487bd939568942f4636f43
Brad Smith <brasmith@redhat.com>
2025-04-15 18:14:24 -0400

    refactor: simplify and consolidate ScanSerializer.validate_search_directories tests with parametrize
    Relates to JIRA: DISCOVERY-784

--- quipucords/tests/api/scan/test_scan_serializer.py
+++ quipucords/tests/api/scan/test_scan_serializer.py
@@ -6,24 +6,22 @@ from rest_framework.exceptions import ValidationError
 from api.scan.serializer import ScanSerializer
 
 
-def test_get_extra_vars_missing_search_directories_empty():
-    """Tests the get_extra_vars with search_directories empty."""
-    search_directories = []
-    ScanSerializer.validate_search_directories(search_directories)
+@pytest.mark.parametrize("search_directories", [None, [], ["/a"], ["/a", "/b"]])
+def test_validate_search_directories_with_acceptable_inputs(search_directories):
+    """Tests the get_extra_vars with search_directories with acceptable values."""
+    result = ScanSerializer.validate_search_directories(search_directories)
+    assert result == search_directories  # yes, output==input means success here.
 
 
-def test_get_extra_vars_missing_search_directories_w_int():
-    """Tests the get_extra_vars with search_directories contains int."""
+@pytest.mark.parametrize(
+    "search_directories",
+    [
+        "/a",  # is not a list
+        [1],  # is not a list of strings
+        ["a"],  # is not a list of absolute path like strings
+    ],
+)
+def test_validate_search_directories_with_bad_inputs(search_directories):
+    """Tests the get_extra_vars with search_directories contains bad values."""
     with pytest.raises(ValidationError):
-        ScanSerializer.validate_search_directories([1])
-
-
-def test_get_extra_vars_missing_search_directories_w_not_path():
-    """Tests the get_extra_vars with search_directories no path."""
-    with pytest.raises(ValidationError):
-        ScanSerializer.validate_search_directories(["a"])
-
-
-def test_get_extra_vars_missing_search_directories_w_path():
-    """Tests the get_extra_vars with search_directories no path."""
-    ScanSerializer.validate_search_directories(["/a"])
+        ScanSerializer.validate_search_directories(search_directories)
