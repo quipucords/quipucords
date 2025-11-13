cdeefb9702b7635c6dbeaae4529b18e78768e8c2
abellotti <abellott@redhat.com>
2025-11-13 01:29:42 -0500

    feature(vault): adding HashiVault class
    - Adding the HashiVault class for instantiating a class
      that talks to Hashi Vault and providing helper methods
      for encrypting and decryption sensitive data.

---
+++ quipucords/api/hashivault.py
@@ -0,0 +1,54 @@
+"""HashiVault is used to read and write data securely using the HashiCorp Vault."""
+
+import base64
+
+import hvac
+from django.conf import settings
+
+
+class VaultClient:
+    """Instantiate a HashiVault client for encryption and decryption of data."""
+
+    def __init__(self):
+        if settings.QUIPUCORDS_VAULT_ENABLED:
+            self.vault_client = hvac.Client(
+                url=settings.QUIPUCORDS_VAULT_ADDR,
+                token=settings.QUIPUCORDS_VAULT_TOKEN,
+                verify=settings.QUIPUCORDS_VAULT_SSL_VERIFY,
+            )
+        else:
+            self.vault_client = None
+
+    def encrypt(self, key, plaintext):
+        """Encrypt plain text using Vault."""
+        plaintext_bytes = (
+            plaintext.encode("utf8") if isinstance(plaintext, str) else plaintext
+        )
+        plaintext_b64 = base64.urlsafe_b64encode(plaintext_bytes).decode("ascii")
+        encrypted_response = self.vault_client.secrets.transit.encrypt_data(
+            mount_point=settings.QUIPUCORDS_VAULT_MOUNT_POINT,
+            name=key,
+            plaintext=plaintext_b64,
+        )
+        self.vault_client.secrets.transit.update_key_configuration(
+            mount_point=settings.QUIPUCORDS_VAULT_MOUNT_POINT,
+            name=key,
+            deletion_allowed=True,
+        )
+        return encrypted_response["data"]["ciphertext"]
+
+    def decrypt(self, key, ciphertext):
+        """Decrypt encrypted key using Vault."""
+        decrypted_response = self.vault_client.secrets.transit.decrypt_data(
+            mount_point=settings.QUIPUCORDS_VAULT_MOUNT_POINT,
+            name=key,
+            ciphertext=ciphertext,
+        )
+        decrypted_response_b64 = decrypted_response["data"]["plaintext"]
+        return base64.urlsafe_b64decode(decrypted_response_b64).decode("utf-8")
+
+    def delete(self, key):
+        """Delete key from vault."""
+        self.vault_client.secrets.transit.delete_key(
+            mount_point=settings.QUIPUCORDS_VAULT_MOUNT_POINT, name=key
+        )
