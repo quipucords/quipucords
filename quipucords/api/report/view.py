5c3027ba6c6e84fb4c4f44a3df0890e9eac561e2
abellotti <abellott@redhat.com>
2026-02-17 17:57:26 -0500

    feat: adding the ?report_type=insights to the downloads endpoint
    - Adding the report_type=insights to /api/v2/reports/<report_id>/download
      to complement the /api/v1/reports/<report_id/insights endpoint.

--- quipucords/api/report/view.py
+++ quipucords/api/report/view.py
@@ -13,7 +13,9 @@ from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from api.aggregate_report.view import get_serialized_aggregate_report
 from api.deployments_report.view import get_deployments_report
-from api.models import InspectResult, Report
+from api.insights_report.serializers import YupanaPayloadSerializer
+from api.insights_report.view import get_report, validate_deployment_report_status
+from api.models import DeploymentsReport, InspectResult, Report
 from api.report.reports_gzip_renderer import ReportsGzipRenderer
 from api.report.serializer import InspectResultSerializer, ReportSerializer
 from api.report.view_v1 import get_reports_report
@@ -66,34 +68,42 @@ class ReportViewSet(ReadOnlyModelViewSet):
     )
 )
 def download_report(request, report_id):
-    """View for downloading a report (v2)."""
+    """View for downloading a report."""
     report_type = request.GET.get("report_type", "default")
 
+    response_report = None
+    response_status = status.HTTP_200_OK
+
     match report_type:
-        case "default":  # Default tar.gz, same as v1 reports/<report_id>/
-            reports_report, response_status = get_reports_report(report_id)
+        case "default":  # Default tar.gz, same as /api/v1/reports/<report_id>/
+            response_report, response_status = get_reports_report(report_id)
             if response_status == status.HTTP_200_OK:
                 renderer = ReportsGzipRenderer()
                 request.accepted_renderer = renderer
                 request.accepted_media_type = renderer.media_type
-            return Response(reports_report, status=response_status)
-        case "aggregate":  # same as v1 reports/<report_id>/aggregate
-            aggregate = get_serialized_aggregate_report(report_id)
-            if aggregate:
-                return Response(aggregate)
-            return None
-        case "deployments":  # same as v1 reports/<report_id>/deployments
-            deployments_report, response_status = get_deployments_report(report_id)
-            return Response(deployments_report, status=response_status)
-        case "details":  # same as v1 reports/<report_id>/details
+        case "aggregate":  # same as /api/v1/reports/<report_id>/aggregate
+            response_report = get_serialized_aggregate_report(report_id)
+            if not response_report:
+                response_report = dict()
+        case "deployments":  # same as /api/v1/reports/<report_id>/deployments
+            response_report, response_status = get_deployments_report(report_id)
+        case "details":  # same as /api/v1/reports/<report_id>/details
             detail_data = get_object_or_404(Report.objects.all(), id=report_id)
             serializer = DetailsReportSerializer(detail_data)
-            details_report = serializer.data
-            details_report.pop("cached_csv", None)
-            return Response(details_report)
-
+            response_report = serializer.data
+            response_report.pop("cached_csv", None)
+        case "insights":  # same as /api/v1/reports/<report_id>/insights
+            deployment_report = get_object_or_404(
+                DeploymentsReport.objects.only("id", "status"), report__id=report_id
+            )
+            validate_deployment_report_status(deployment_report)
+            report = get_report(deployment_report)
+            serializer = YupanaPayloadSerializer(report)
+            response_report = serializer.data
         case _:
             return Response(
                 {"detail": f"Unsupported report_type {report_type} specified"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+    return Response(response_report, status=response_status)
