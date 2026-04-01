64562a40b062545c260bd77e2c13cdbb1ec11d49
abellotti <abellott@redhat.com>
2026-03-31 20:40:19 -0400

    feat: OpenAPI schema example for /api/v2/auth/hashicorp-vault
    - Adding examples for the CRUD operations on
      the /api/v2/auth/hashicorp-vault singleton endpoint.

--- quipucords/api/auth/view.py
+++ quipucords/api/auth/view.py
@@ -7,6 +7,7 @@ from django.db import transaction
 from django.utils.translation import gettext as _
 from drf_spectacular.utils import (
     OpenApiExample,
+    OpenApiRequest,
     OpenApiResponse,
     extend_schema,
 )
@@ -28,6 +29,7 @@ from api.auth.auth_lightspeed import (
 )
 from api.auth.serializer import (
     FailedAuthRequestResponseSerializer,
+    HashiCorpVaultResponseSerializer,
     HashiCorpVaultSerializer,
     LightspeedAuthLoginResponseSerializer,
     LightspeedAuthLogoutResponseSerializer,
@@ -47,6 +49,51 @@ def unauthorized_auth_response() -> OpenApiResponse:
                 "Unauthorized auth request response",
                 description="Unauthorized auth request response",
                 value={"detail": "Authentication credentials were not provided."},
+            )
+        ],
+    )
+
+
+def hashicorp_vault_not_defined() -> OpenApiResponse:
+    """Return an OpenApiResponse if the HashiCorp Vault is not defined (404)."""
+    return OpenApiResponse(
+        description="HashiCorp Vault server not found response.",
+        response=FailedAuthRequestResponseSerializer(),
+        examples=[
+            OpenApiExample(
+                "HashiCorp Vault server not found response.",
+                description="HashiCorp Vault server is not defined",
+                value={"detail": "HashiCorp Vault server is not defined"},
+            )
+        ],
+    )
+
+
+def hashicorp_vault_missing_client_key() -> OpenApiResponse:
+    """Return an OpenApiResponse if the HashiCorp Vault is missing client_key."""
+    return OpenApiResponse(
+        description="HashiCorp Vault server definition is missing client_key.",
+        response=FailedAuthRequestResponseSerializer(),
+        examples=[
+            OpenApiExample(
+                "HashiCorp Vault server is missing client_key.",
+                description="HashiCorp Vault server is missing client_key defined",
+                value={"client_key": ["This field is required."]},
+            )
+        ],
+    )
+
+
+def hashicorp_vault_port_not_integer() -> OpenApiResponse:
+    """Return an OpenApiResponse if the HashiCorp Vault port is not an integer."""
+    return OpenApiResponse(
+        description="HashiCorp Vault server port is not an integer.",
+        response=FailedAuthRequestResponseSerializer(),
+        examples=[
+            OpenApiExample(
+                "HashiCorp Vault server port is not an integer.",
+                description="HashiCorp Vault server port is not an integer.",
+                value={"port": ["A valid integer is required."]},
             )
         ],
     )
@@ -188,6 +235,26 @@ class HashiCorpVaultViewSet(viewsets.GenericViewSet):
 
     serializer_class = HashiCorpVaultSerializer
 
+    @extend_schema(
+        responses={
+            (200, "application/json"): OpenApiResponse(
+                description="Successful HashiCorp Vault server retrieved",
+                response=HashiCorpVaultResponseSerializer(),
+                examples=[
+                    OpenApiExample(
+                        "HashiCorp Vault Server definition",
+                        value={
+                            "address": "vault.example.com",
+                            "port": 8200,
+                            "ssl_verify": False,
+                        },
+                    )
+                ],
+            ),
+            (401, "application/json"): unauthorized_auth_response(),
+            (404, "application/json"): hashicorp_vault_not_defined(),
+        },
+    )
     def retrieve(self, request, *args, **kwargs):
         """Get the HashiCorp Vault server definition."""
         hashicorp_vault_token = get_hashicorp_vault_token()
@@ -200,6 +267,58 @@ class HashiCorpVaultViewSet(viewsets.GenericViewSet):
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+        request=OpenApiRequest(
+            HashiCorpVaultSerializer(),
+            examples=[
+                OpenApiExample(
+                    "Request to define a HashiCorp Vault server",
+                    description="HashiCorp Vault server definition with a client"
+                    " certificate and key. client_cert and client_key are single line"
+                    " base64 encodings of the certificate pem files.",
+                    value={
+                        "address": "vault.example.com",
+                        "port": 8200,
+                        "ssl_verify": False,
+                        "client_cert": "....",
+                        "client_key": "....",
+                    },
+                    request_only=True,
+                ),
+            ],
+        ),
+        responses={
+            (200, "application/json"): OpenApiResponse(
+                description="Successful HashiCorp Vault server created",
+                response=HashiCorpVaultResponseSerializer(),
+                examples=[
+                    OpenApiExample(
+                        "HashiCorp Vault Server definition",
+                        value={
+                            "address": "vault.example.com",
+                            "port": 8200,
+                            "ssl_verify": False,
+                        },
+                    )
+                ],
+            ),
+            (401, "application/json"): unauthorized_auth_response(),
+            (400, "application/json"): hashicorp_vault_missing_client_key(),
+            (409, "application/json"): OpenApiResponse(
+                description="HashiCorp Vault server already exists.",
+                response=FailedAuthRequestResponseSerializer(),
+                examples=[
+                    OpenApiExample(
+                        "HashiCorp Vault server already exists.",
+                        description="HashiCorp Vault server definition already exists",
+                        value={
+                            "detail": "HashiCorp Vault server definition already exists"
+                        },
+                    )
+                ],
+            ),
+        },
+    )
     def create(self, request, *args, **kwargs) -> Response:
         """Create a HashiCorp Vault server definition."""
         if get_hashicorp_vault_token():
@@ -217,6 +336,48 @@ class HashiCorpVaultViewSet(viewsets.GenericViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @extend_schema(
+        request=OpenApiRequest(
+            HashiCorpVaultSerializer(),
+            examples=[
+                OpenApiExample(
+                    "Request to replace the HashiCorp Vault server definition",
+                    description="HashiCorp Vault server definition with a client"
+                    " certificate, key and CA cert. client_cert, client_key and"
+                    " ca_cert are single line base64 encoded of the certificate"
+                    " pem files.",
+                    value={
+                        "address": "newvault.example.com",
+                        "port": 8210,
+                        "ssl_verify": True,
+                        "client_cert": "....",
+                        "client_key": "....",
+                        "ca_cert": "....",
+                    },
+                    request_only=True,
+                ),
+            ],
+        ),
+        responses={
+            (200, "application/json"): OpenApiResponse(
+                description="Successful update of the HashiCorp Vault server",
+                response=HashiCorpVaultResponseSerializer(),
+                examples=[
+                    OpenApiExample(
+                        "Updated HashiCorp Vault server definition",
+                        value={
+                            "address": "newvault.example.com",
+                            "port": 8210,
+                            "ssl_verify": True,
+                        },
+                    )
+                ],
+            ),
+            (401, "application/json"): unauthorized_auth_response(),
+            (400, "application/json"): hashicorp_vault_missing_client_key(),
+            (404, "application/json"): hashicorp_vault_not_defined(),
+        },
+    )
     def update(self, request, *args, **kwargs) -> Response:
         """Update a HashiCorp Vault server definition."""
         partial = kwargs.pop("partial", False)
@@ -243,6 +404,40 @@ class HashiCorpVaultViewSet(viewsets.GenericViewSet):
         serializer.save()
         return Response(serializer.data)
 
+    @extend_schema(
+        request=OpenApiRequest(
+            HashiCorpVaultSerializer(),
+            examples=[
+                OpenApiExample(
+                    "Request to update the port of a HashiCorp Vault server",
+                    description="Updated HashiCorp Vault server port.",
+                    value={
+                        "port": 8230,
+                    },
+                    request_only=True,
+                ),
+            ],
+        ),
+        responses={
+            (200, "application/json"): OpenApiResponse(
+                description="Successful HashiCorp Vault server updated",
+                response=HashiCorpVaultResponseSerializer(),
+                examples=[
+                    OpenApiExample(
+                        "HashiCorp Vault Server definition",
+                        value={
+                            "address": "newvault.example.com",
+                            "port": 8230,
+                            "ssl_verify": True,
+                        },
+                    )
+                ],
+            ),
+            (401, "application/json"): unauthorized_auth_response(),
+            (400, "application/json"): hashicorp_vault_port_not_integer(),
+            (404, "application/json"): hashicorp_vault_not_defined(),
+        },
+    )
     def partial_update(self, request, *args, **kwargs) -> Response:
         """Do a partial update (PATCH) of the HashiCorp Vault server definition."""
         return self.update(request, partial=True)
